98b4898c8f4e36a7415ce2d7605453fb0fc8b6d1
Seth Schoen <schoen@eff.org>
2012-11-18 17:58:00 -0800

    remove dead code

--- server-ca/payment.py
+++ server-ca/payment.py
@@ -9,7 +9,6 @@ import web, redis
 
 urls = (
      '/([a-f0-9]{10})', 'shortform',
-     '/([a-f0-9]{64})', 'form',
      '/submit=([a-f0-9]{64})', 'payment'
 )
 
@@ -23,31 +22,6 @@ class shortform(object):
               return "<html><h1>Unknown session ID</h1></html>"
           with open("index.html","r") as f:
               return f.read() % expanded
-
-class form(object):
-      def GET(self, what):
-          web.header("Content-type", "text/html")
-          return """
-          <html>
-          <h1>Payment required</h1>
-          Due to certificate authority policy, issuing this certificate requires a payment.
-          <p>
-          <hr width="70%%" />
-          <p>
-          A payment of <b>17.00 simoleons</b> is due now.
-          <p>
-          In order to process this payment, please pretend to enter a 16-digit credit-card
-          number below, and then click the Submit Payment button.
-          <p>
-          <form action="/payment.py/submit=%s" method="GET">
-          <i>Credit Card Type</i> <select name=""><option>Vista</option><option>MisterCard</option><option>Discovery</option></select> <br />
-          <i>Credit Card Number</i> <input type="text" name="" style="font-family:monospace" autocomplete="off" /><br />
-          <input type="submit" value="Submit Payment">
-          </form>
-          This payment will appear on your
-          credit card statement as TRUSTIFIABLE CERTIFICATE SERVICES.
-          </html>
-          """ % what
 
 def hexdigit(s):
     return s in "0123456789abcdef"
