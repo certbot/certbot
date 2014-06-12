499f6c2fad62fe4d793cfee727576f399012a1b2
Jacob Hoffman-Andrews <jsha@eff.org>
2014-06-12 11:53:02 -0400

    Add comment to ProcessgoogleSTARTTLSDomains.py

--- ProcessGoogleSTARTTLSDomains.py
+++ ProcessGoogleSTARTTLSDomains.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
+"""
+Process Google's TLS delivery data from
+https://www.google.com/transparencyreport/saferemail/data/?hl=en
+to look for outbound domains that can negotiate an encrypted
+connection >99% of the time.
+
+Usage:
+  ./ProcessGoogleSTARTTLSDomains.py google-starttls-domains.csv
+"""
 import csv
 import codecs
 import sys
@@ -14,5 +23,5 @@ for (address_suffix, hostname_suffix, direction, region, fraction_encrypted) in 
       pass
 
 for address_suffix, fraction_encrypted in d.iteritems():
-  if min(fraction_encrypted) >= 0.50:
+  if min(fraction_encrypted) >= 0.99:
     print min(fraction_encrypted), address_suffix
