f72836ba14afbb556789de4bc133812d5c3d4357
James Kasten <jdkasten@umich.edu>
2014-11-11 11:52:53 -0800

    Add validator class to be used after configuration to guarantee correctness

---
+++ trustify/client/validator.py
@@ -0,0 +1,12 @@
+class Validator(object):
+    """
+    This Class will contain an API to validate configurations.
+    """
+    def redirect(name):
+        return
+    def ocsp_stapling(name):
+        return
+    def https(names):
+        return
+    def hsts(name):
+        return
