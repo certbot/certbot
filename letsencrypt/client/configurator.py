c238a6a36f91202d8983a344a36cd9bd65919133
James Kasten <jdkasten@umich.edu>
2014-11-20 02:21:56 -0800

    Add AugeasConfigurator class... move all appropriate functions to it

--- letsencrypt/client/configurator.py
+++ letsencrypt/client/configurator.py
@@ -1,61 +1,70 @@
-from abc import PluginBase
+import abc
 
-class Configurator(PluginBase):
+class Configurator(object):
     """
     Class represents all possible webservers and configuration editors
     This includes the generic webserver which wont have configuration
     files at all, but instead create a new process to handle the DVSNI
     and other challenges.
     """
+    __metaclass__ = abc.ABCMeta
 
+    @abc.abstractmethod
     def deploy_cert(self, vhost, cert, key , cert_chain=None):
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def choose_virtual_host(self, name):
         """
         Chooses a virtual host based on a given domain name
         """
-        raise Exception("Error: base Configurator class")
+        return
 
-
+    @abc.abstractmethod
     def get_all_names(self):
         """
         Returns all names found in the Configuration
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def enable_redirect(self, ssl_vhost):
         """
         Makes all traffic redirect to the given ssl_vhost
         ie. port 80 => 443
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def enable_hsts(self, ssl_vhost):
         """
         Enable HSTS on the given ssl_vhost
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def enable_ocsp_stapling(self, ssl_vhost):
         """
         Enable OCSP stapling on given ssl_vhost
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def get_all_certs_keys(self):
         """
         Retrieve all certs and keys set in configuration
         returns: list of tuples with form [(cert, key, path)]
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def enable_site(self, vhost):
         """
         Enable the site at the given vhost
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def save(self, title=None, temporary=False):
         """
         Saves all changes to the configuration files, both
@@ -70,34 +79,39 @@ class Configurator(PluginBase):
         temporary: boolean - Indicates whether the changes made will be
                              quickly reversed in the future (challenges)
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def revert_challenge_config(self):
         """
         This function should reload the users original configuration files
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def rollback_checkpoints(self, rollback = 1):
         """
         Revert `rollback` number of configuration checkpoints
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def display_checkpoints(self):
         """
         Display the saved configuration checkpoints
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def config_test(self):
         """
         Make sure the configuration is valid
         """
-        raise Exception("Error: base Configurator class")
+        return
 
+    @abc.abstractmethod
     def restart(self):
         """
         Restart or refresh the server content
         """
-        raise Exception("Error: base Configurator class")
+        return
