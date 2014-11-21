0ab0b185d394e3aa6fc75376eefb432104a66c01
Jakub Warmuz <jakub@warmuz.org>
2014-11-21 21:42:25 +0100

    Fix no-self-argument

--- letsencrypt/client/validator.py
+++ letsencrypt/client/validator.py
@@ -2,11 +2,11 @@ class Validator(object):
     """
     This Class will contain an API to validate configurations.
     """
-    def redirect(name):
+    def redirect(self, name):
         return
-    def ocsp_stapling(name):
+    def ocsp_stapling(self, name):
         return
-    def https(names):
+    def https(self, names):
         return
-    def hsts(name):
+    def hsts(self, name):
         return
