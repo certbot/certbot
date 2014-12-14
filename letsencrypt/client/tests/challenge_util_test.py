d53889b617cb8ac2f106d8a8f65f3759fd6896b7
James Kasten <jdkasten@umich.edu>
2014-12-14 02:51:17 -0800

    Fixed up remaining pylint errors

--- letsencrypt/client/tests/challenge_util_test.py
+++ letsencrypt/client/tests/challenge_util_test.py
@@ -11,13 +11,12 @@ from letsencrypt.client import client
 from letsencrypt.client import CONFIG
 from letsencrypt.client import le_util
 
-
+# pylint: disable=too-few-public-methods
 class DvnsiGenCertTest(unittest.TestCase):
     """Tests for letsencrypt.client.challenge_util.dvsni_gen_cert."""
 
     def test_standard(self):
         """Basic test for straightline code."""
-
         # This is a helper function that can be used for handling
         # open context managers more elegantly. It avoids dealing with
         # __enter__ and __exit__ calls.
@@ -46,6 +45,7 @@ class DvnsiGenCertTest(unittest.TestCase):
                 m_open().write.call_args[0][0], domain, nonce, ext)
 
     def _standard_check_cert(self, pem, domain, nonce, ext):
+        """Check the certificate fields."""
         dns_regex = r"DNS:([^, $]*)"
         cert = M2Crypto.X509.load_cert_string(pem)
         self.assertEqual(
@@ -58,6 +58,7 @@ class DvnsiGenCertTest(unittest.TestCase):
 
         self.assertEqual(exp_sans, act_sans)
 
+    # pylint: disable= no-self-use
     def _call(self, filepath, name, r_b64, nonce, key):
         from letsencrypt.client.challenge_util import dvsni_gen_cert
         return dvsni_gen_cert(filepath, name, r_b64, nonce, key)
