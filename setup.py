c43ecf924ce1128dfa6d08e6e53e683815b2e0af
Seth Schoen <schoen@eff.org>
2015-02-02 17:02:22 -0800

    Declare dependency on PyOpenSSL package

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ install_requires = [
     'python-augeas',
     'python2-pythondialog',
     'requests',
+    'PyOpenSSL',
     'zope.component',
     'zope.interface',
     # order of items in install_requires DOES matter and M2Crypto has
