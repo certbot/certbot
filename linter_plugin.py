0b8767f990e058a56bf2ebc2cece535df4f5cfc9
Jakub Warmuz <jakub@warmuz.org>
2015-02-12 08:47:17 +0000

    Catch-all __slots__ linter_plugin

--- linter_plugin.py
+++ linter_plugin.py
@@ -14,8 +14,10 @@ def _transform(cls):
     # fix the "no-member" error on instances of
     # letsencrypt.acme.util.ImmutableMap subclasses (instance
     # attributes are initialized dynamically based on __slots__)
-    if (('Message' in cls.basenames or 'ImmutableMap' in cls.basenames or
-         'util.ImmutableMap' in cls.basenames) and (cls.slots() is not None)):
+
+    # TODO: this is too broad and applies to any tested class...
+
+    if cls.slots() is not None:
         for slot in cls.slots():
             cls.locals[slot.value] = [nodes.EmptyNode()]
 
