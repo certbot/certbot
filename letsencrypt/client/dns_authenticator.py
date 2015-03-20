490d8bee42a19e94e5987617896d0f45048ba8ae
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-03-19 21:04:00 -0700

    100% test coverage

--- letsencrypt/client/dns_authenticator.py
+++ letsencrypt/client/dns_authenticator.py
@@ -17,116 +17,226 @@ from letsencrypt.client import interfaces
 # this should be provided by the user!
 
 def add_record(zone, token, keyring):
-	challenge_subdomain = "_acme-challenge.%s" % (zone)
+    """Create a `dns.message.Message` object asking the server to
+       create a TXT record for the challenge subdomain
+       (`_acme-challenge.example.com`) signed by the provided
+       TSIG keyring.
 
-	challenge_request = dns.update.Update(zone, keyring=keyring)
-	# check challenge_subdomain is absent
-	challenge_request.absent(challenge_subdomain)
-	# add challenge_subdomain TXT with token
-	challenge_request.add(challenge_subdomain, constants.DNS_CHALLENGE_TTL, "TXT", token)
+       :param str zone: Zone (domain) in which the record should
+           be provisioned.
+       :param str token: Token provided by ACME server.
+       :param dns.tsigkeyring keyring: TSIG keyring object containg TSIG
+           keypair valid for the provided zone.
 
-	# return req
-	return challenge_request
+       :returns Add record DNS message.
+       :rtype dns.message.Message
 
-def del_record(zone, token, keyring):
-	challenge_subdomain = "_acme-challenge.%s" % (zone)
+    """
+    challenge_subdomain = "_acme-challenge.%s" % (zone)
 
-	challenge_request = dns.update(zone, keyring=keyring)
-	# check challenge_subdomain is present
-	challenge_request.present(challenge_subdomain)
-	# delete challegen_subdomain TXT
-	challenge_request.delete(challenge_subdomain)
+    challenge_request = dns.update.Update(zone, keyring=keyring)
+    # check challenge_subdomain is absent
+    challenge_request.absent(challenge_subdomain)
+    # add challenge_subdomain TXT with token
+    challenge_request.add(
+        challenge_subdomain, constants.DNS_CHALLENGE_TTL, "TXT", token)
 
-	# return req
-	return challenge_request
+    # return req
+    return challenge_request
 
-def send_request(gen_record, zone, token, keyring, server, port, source_port, timeout):
-	dns_request = gen_record(zone, token, keyring)
+def del_record(zone, token, keyring): # pylint: disable=unused-argument
+    """Create a `dns.message.Message` object asking the server to
+       delete a TXT record for the challenge subdomain
+       (`_acme-challenge.example.com`) signed by the provided
+       TSIG keyring.
 
-	# FIXME: better keyring errors (that include that key that was used)
-	rcode_errors = {
-		1: 'Malformed DNS message',
-		2: 'Server failed',
-		3: 'Domain does not exist on DNS server (%s)' % (zone),
-		4: 'DNS server does not support that opcode',
-		5: ('DNS server refuses to perform the specified '
-		    'operation for policy or security reasons'),
-		6: 'Name exists when it should not (_acme-challenge.%s)' % (zone),
-		7: 'Records that should not exist do exist (_acme-challenge.%s)' % (zone),
-		8: 'Records that should exist do not exist (_acme-challenge.%s)' % (zone),
-		9: ('Server is not authorized or is using bad TSIG key to '
-		    'make updates to zone "%s"' % (zone)),
-		10: ('Zone "%s" does not exist' % (zone)),
-		16: ('Server is using bad TSIG key to '
+       :param str zone: Zone (domain) in which the record should exists.
+       :param str token: Not needed
+       :param dns.tsigkeyring keyring: TSIG keyring object containg TSIG
+           keypair valid for the provided zone.
+
+       :returns Delete record DNS message.
+       :rtype dns.message.Message
+
+    """
+    challenge_subdomain = "_acme-challenge.%s" % (zone)
+
+    challenge_request = dns.update.Update(zone, keyring=keyring)
+    # check challenge_subdomain is present
+    challenge_request.present(challenge_subdomain)
+    # delete challegen_subdomain TXT
+    challenge_request.delete(challenge_subdomain)
+
+    # return req
+    return challenge_request
+
+def send_request(
+        gen_request, zone, token, keyring, server, port):
+    """Generates a DNS message based on function passed as `gen_request`
+       (either `add_record` or `del_record`) and then sends it to the DNS
+       server specified by `server` at `port`.
+
+       If the DNS request fails for any reason `LetsEncryptDNSAuthError` will
+       be raised, otherwise `True` will be returned.
+
+       :param func gen_request: Function (either `add_record` or `del_record`)
+           to use to generate the relevant DNS message.
+       :param str zone: Zone (domain) in which things will happen.
+       :param str token: Token provided by ACME server.
+       :param dns.tsigkeyring keyring: TSIG keyring object containg TSIG
+           keypair valid for the provided zone.
+       :param str server: Hostname or IP address of DNS server to make
+           requests to.
+       :param int port: Port the DNS server listens on.
+
+       :returns `True` if challenge subdomain is successfully
+           provisioned.
+       :rtype bool
+
+    """
+    dns_request = gen_request(zone, token, keyring)
+
+    # FIXME: better keyring errors (that include that key that was used)
+    rcode_errors = {
+        1: 'Malformed DNS message',
+        2: 'Server failed',
+        3: 'Domain does not exist on DNS server (%s)' % (zone),
+        4: 'DNS server does not support that opcode',
+        5: ('DNS server refuses to perform the specified '
+            'operation for policy or security reasons'),
+        6: 'Name exists when it should not (_acme-challenge.%s)' % (zone),
+        7: ('Records that should not exist do exist (_acme-challenge.%s)'
+            % (zone)),
+        8: ('Records that should exist do not exist (_acme-challenge.%s)'
+            % (zone)),
+        9: ('Server is not authorized or is using bad TSIG key to '
+            'make updates to zone "%s"' % (zone)),
+        10: ('Zone "%s" does not exist' % (zone)),
+        16: ('Server is using bad TSIG key to '
                     'make updates to zone "%s"' % (zone)),
-		17: ('Server is using bad TSIG key to '
+        17: ('Server is using bad TSIG key to '
                     'make updates to zone "%s"' % (zone))
         }
 
-	try:
-		response = dns.query.tcp(dns_request, server, port=port, source_port=source_port, timeout=timeout)
+    try:
+        response = dns.query.tcp(
+            dns_request,
+            server, port=port,
+            source_port=constants.DNS_CHALLENGE_SOURCE_PORT,
+            timeout=constants.DNS_CHALLENGE_TIMEOUT
+        )
 
-		if response.rcode() == 0:
-			return True
-		else:
-			raise errors.LetsEncryptDNSAuthError(rcode_errors.get("DNS Error: %s" % (response.rcode())))
+        if response.rcode() == 0:
+            return True
+        else:
+            raise errors.LetsEncryptDNSAuthError(
+                rcode_errors.get("DNS Error: %s" % (response.rcode())))
 
-	except (NoAnswer, UnexpectedSource, BadResponse,
-                OSError) as err: # TimeoutError doesn't exist in 2.7 I dont think...
-		if isinstance(err, NoAnswer):
-			dns_error = "DNS Error: Did not recieve a response to DNS request!"
-		elif isinstance(err, UnexpectedSource):
-			dns_error = "DNS Error: Recieved response to DNS request from unexpected source!"
-		elif isinstance(err, BadResponse):
-			dns_error = "DNS Error: Recieved malformed response to DNS request!"
-		# elif isinstance(err, TimeoutError):
-		# 	dns_error = "DNS Error: DNS request timed out!"
-		elif isinstance(err, OSError):
-			dns_error = "DNS Error: I forgot what an OSError means in this context..."
-		raise errors.LetsEncryptDNSAuthError(dns_error)
+    except (NoAnswer, UnexpectedSource, BadResponse,
+                OSError) as err: # TimeoutError doesn't exist in 2.7 afaik
+        # elif isinstance(err, TimeoutError):
+        #     dns_error = "DNS Error: DNS request timed out!"
+        if isinstance(err, NoAnswer):
+            dns_error = "DNS Error: Did not recieve a response to DNS request!"
+        elif isinstance(err, UnexpectedSource):
+            dns_error = ("DNS Error: Recieved response to DNS request from "
+                         "unexpected source!")
+        elif isinstance(err, BadResponse):
+            dns_error = ("DNS Error: Recieved malformed response to DNS "
+                         "request!")
+        elif isinstance(err, OSError):
+            dns_error = ("DNS Error: I forgot what an OSError means in this"
+                         " context...")
+        raise errors.LetsEncryptDNSAuthError(dns_error)
 
 class DNSAuthenticator(object):
-	zope.interface.implements(interfaces.IAuthenticator)
-	
-	def __init__(self, config):
-		# super(DNSAuthenticator, self).__init__(config)
-		self.dns_server = config.dns_server
-		self.dns_server_port = config.dns_server_port
-		self.dns_tsig_keys = config.dns_tsig_keys
+    """DNS Authenticator
 
-	def get_chall_pref(self, unused_domain):
-		return [challenges.DNS]
+    This authenticator interacts with an already configured DNS
+    server in order to provision subdomains in response to DNS
+    challenges from the certificate authority.
 
-	def perform(self, achalls):
-		if not achalls or not isinstance(achalls, list):
-			raise ValueError(".perform() was called without challenge list")
-		responses = []
-		for achall in achalls:
-			if isinstance(achall, achallenges.DNS):
-				zone = achall.domain
-				tsig = [[t[0], t[1]] for t in self.dns_tsig_keys if zone in t[2]]
-				if not tsig:
-					raise errors.LetsEncryptDNSAuthError("No TSIG key provided for domain '%s'" % (zone))
-				tsig_keyring = dns.tsigkeyring.from_text({tsig[0][0]: tsig[0][1]})
-				token = achall.token
+    """
+    zope.interface.implements(interfaces.IAuthenticator)
 
-				# send request
-				if send_request(add_record, zone, token, tsig_keyring, self.dns_server, self.dns_server_port, constants.DNS_CHALLENGE_SOURCE_PORT, constants.DNS_CHALLENGE_TIMEOUT):
-					responses.append(challenges.DNSResponse())
-			else:
-				raise errors.LetsEncryptDNSAuthError("Unexpected Challenge")
-		return responses
+    def __init__(self, config):
+        # super(DNSAuthenticator, self).__init__(config)
+        self.dns_server = config.dns_server
+        self.dns_server_port = config.dns_server_port
+        self.dns_tsig_keys = config.dns_tsig_keys
 
-	def cleanup(self, achalls):
-		if not achalls or not isinstance(achalls, list):
-                        raise ValueError(".cleanup() was called without challenge list")
-                for achall in achalls:
-                        if isinstance(achall, achallenges.DNS):
-                                zone = achall.domain
-                                tsig_keyring = dns.tsigkeyring.from_text({achall.tsig_key_name: achall.tsig_key})
-				token = achall.token
+    @staticmethod
+    def get_chall_pref(unused_domain):
+        """Get challenge preferences.
 
-				# send it, raises error on absent records etc...
-				send_request(del_record, zone, token, tsig_keyring, DNS_SERVER, DNS_PORT, constants.DNS_CHALLENGE_SOURCE_PORT, constants.DNS_CHALLENGE_TIMEOUT)
-			else:
-				raise errors.LetsEncryptDNSAuthError("Unexpected Challenge")
+        :returns: A list containing only 'challenges.DNS'.
+
+        """
+        return [challenges.DNS]
+
+    def perform(self, achalls):
+        """Perform the challenges.
+
+        """
+        if not achalls or not isinstance(achalls, list):
+            raise ValueError(".perform() was called without challenge list")
+        responses = []
+        for achall in achalls:
+            if isinstance(achall, achallenges.DNS):
+                zone = achall.domain
+                # this could be cleaned up, esp since we use it here and in
+                # cleanup, could prob just be a function...
+                tsig = [
+                    [t[0], t[1]] for t in self.dns_tsig_keys if zone in t[2]
+                ]
+                if not tsig:
+                    raise ValueError(
+                        "No TSIG key provided for domain '%s'" % (zone))
+                tsig_keyring = dns.tsigkeyring.from_text(
+                    {tsig[0][0]: tsig[0][1]})
+                token = achall.token
+
+                # send request
+                if send_request(
+                    add_record,
+                    zone,
+                    token,
+                    tsig_keyring,
+                    self.dns_server,
+                    self.dns_server_port
+                ):
+                    responses.append(challenges.DNSResponse())
+            else:
+                raise errors.LetsEncryptDNSAuthError("Unexpected Challenge")
+        return responses
+
+    def cleanup(self, achalls):
+        """Clean up, remove all challenge subdomains that were provisioned.
+
+        """
+        if not achalls or not isinstance(achalls, list):
+            raise ValueError(".cleanup() was called without challenge list")
+        for achall in achalls:
+            if isinstance(achall, achallenges.DNS):
+                zone = achall.domain
+                tsig = [
+                    [t[0], t[1]] for t in self.dns_tsig_keys if zone in t[2]
+                ]
+                if not tsig:
+                    raise ValueError(
+                        "No TSIG key provided for domain '%s'" % (zone))
+                tsig_keyring = dns.tsigkeyring.from_text(
+                    {tsig[0][0]: tsig[0][1]})
+                token = achall.token
+
+                # send it, raises error on absent records etc...
+                send_request(
+                    del_record,
+                    zone,
+                    token,
+                    tsig_keyring,
+                    self.dns_server,
+                    self.dns_server_port
+                )
+            else:
+                raise errors.LetsEncryptDNSAuthError("Unexpected Challenge")
--- letsencrypt/client/tests/dns_authenticator_test.py
+++ letsencrypt/client/tests/dns_authenticator_test.py
@@ -4,82 +4,178 @@ import unittest
 import mock
 
 import dns.message
+from dns.query import UnexpectedSource, BadResponse
+from dns.resolver import NoAnswer
 
 from letsencrypt.acme import challenges
 
 from letsencrypt.client import achallenges
 from letsencrypt.client import errors
 
-class PerformTest(unittest.TestCase):
-	"""Test DNS perform function."""
 
-	def setUp(self):
-		from letsencrypt.client.dns_authenticator import DNSAuthenticator
+class PerformCleanupTest(unittest.TestCase): # pylint: disable=too-many-public-methods
+    """Test DNS perform function."""
 
-		self.authenticator = DNSAuthenticator(mock.MagicMock(
-			dns_server="localhost",
-			dns_server_port=53,
-			dns_tsig_keys=[
-				["example.key",
-				"+Cdjlkef9ZTSeixERZ433Q==",
-				[
-					"example.com",
-					"final-example.com"
-				]],
-				["other-example.key",
-				"+Lkalkef7EdSeixZXC433Q==",
-				[
-					"other-example.com"
-				]]
-			]
-		))
+    def setUp(self):
+        from letsencrypt.client.dns_authenticator import DNSAuthenticator
 
-		good_chall = achallenges.DNS(chall=challenges.DNS(token="17817c66b60ce2e4012dfad92657527"), domain="example.com")
-		second_good_chall = achallenges.DNS(chall=challenges.DNS(token="17817c66b60ce2e4012dfad92657527"), domain="other-example.com")
-		third_good_chall = achallenges.DNS(chall=challenges.DNS(token="17817c66b60ce2e4012dfad92657527"), domain="final-example.com")
+        self.authenticator = DNSAuthenticator(mock.MagicMock(
+            dns_server="localhost",
+            dns_server_port=53,
+            dns_tsig_keys=[
+                ["example.key",
+                "+Cdjlkef9ZTSeixERZ433Q==",
+                [
+                    "example.com",
+                    "final-example.com"
+                ]],
+                ["other-example.key",
+                "+Lkalkef7EdSeixZXC433Q==",
+                [
+                    "other-example.com"
+                ]]
+            ]
+        ))
 
-		bad_chall = ("this", "isnt", "a", "dns", "challenge")
+        good_chall = achallenges.DNS(
+            chall=challenges.DNS(token="17817c66b60ce2e4012dfad92657527"),
+            domain="example.com"
+        )
+        second_good_chall = achallenges.DNS(
+            chall=challenges.DNS(token="17817c66b60ce2e4012dfad92657527"),
+            domain="other-example.com"
+        )
+        third_good_chall = achallenges.DNS(
+            chall=challenges.DNS(token="17817c66b60ce2e4012dfad92657527"),
+            domain="final-example.com"
+        )
 
-		self.achalls = [good_chall, second_good_chall, third_good_chall, bad_chall]
+        bad_chall = ("this", "isnt", "a", "dns", "challenge")
+        no_key_chall = achallenges.DNS(
+            chall=challenges.DNS(token="17817c66b60ce2e4012dfad92657527"),
+            domain="no-key-example.com"
+        )
 
-		self.good_dns_msg = dns.message.Message()
-		self.good_dns_msg.set_rcode(0)
+        self.achalls = [
+            good_chall,
+            second_good_chall,
+            third_good_chall,
+            bad_chall,
+            no_key_chall
+        ]
 
-		self.bad_dns_msgs = []
-		for rcode in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17]:
-			bad_dns_msg = dns.message.Message()
-			bad_dns_msg.set_rcode(rcode)
-			self.bad_dns_msgs.append(bad_dns_msg)
+        self.good_dns_msg = dns.message.Message()
+        self.good_dns_msg.set_rcode(0)
 
-	def test_chall_pref(self):
-		self.assertEqual(
-			self.authenticator.get_chall_pref("example.com"), [challenges.DNS])
+        self.bad_dns_msgs = []
+        for rcode in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17]:
+            bad_dns_msg = dns.message.Message()
+            bad_dns_msg.set_rcode(rcode)
+            self.bad_dns_msgs.append(bad_dns_msg)
 
-	def test_good_challs_perform(self):
-		with mock.patch("dns.query.tcp") as query:
-			query.return_value = self.good_dns_msg
-			
-			results = self.authenticator.perform(self.achalls[:3])
+    def test_chall_pref(self):
+        self.assertEqual(
+            self.authenticator.get_chall_pref("example.com"), [challenges.DNS])
 
-			self.assertTrue(isinstance(results, list))
-			self.assertEqual(len(results), 3)
-			self.assertTrue(isinstance(results[0], challenges.ChallengeResponse))
-			self.assertTrue(isinstance(results[1], challenges.ChallengeResponse))
-			self.assertTrue(isinstance(results[2], challenges.ChallengeResponse))
+    def test_good_challs_perform(self):
+        with mock.patch("dns.query.tcp") as query:
+            query.return_value = self.good_dns_msg
 
-	def test_bad_challs_perform(self):
-		# no challenges
-		self.assertRaises(ValueError, self.authenticator.perform, [])
-		# invalid challenge
-		self.assertRaises(errors.LetsEncryptDNSAuthError, self.authenticator.perform, [self.achalls[3]])
+            results = self.authenticator.perform(self.achalls[:3])
 
-		# bad DNS message responses
-		for bad_dns_msg in self.bad_dns_msgs:
-			with mock.patch("dns.query.tcp") as query:
-				query.return_value = bad_dns_msg
+            self.assertTrue(isinstance(results, list))
+            self.assertEqual(len(results), 3)
+            self.assertTrue(
+                isinstance(results[0], challenges.ChallengeResponse))
+            self.assertTrue(
+                isinstance(results[1], challenges.ChallengeResponse))
+            self.assertTrue(
+                isinstance(results[2], challenges.ChallengeResponse))
 
-				self.assertRaises(errors.LetsEncryptDNSAuthError, self.authenticator.perform, [self.achalls[0]])
+    def test_bad_challs_perform(self):
+        # no challenges
+        self.assertRaises(ValueError, self.authenticator.perform, [])
+        # invalid challenge
+        self.assertRaises(
+            errors.LetsEncryptDNSAuthError,
+            self.authenticator.perform,
+            [self.achalls[3]]
+        )
 
+        # bad DNS message responses
+        for bad_dns_msg in self.bad_dns_msgs:
+            with mock.patch("dns.query.tcp") as query:
+                query.return_value = bad_dns_msg
+
+                self.assertRaises(
+                    errors.LetsEncryptDNSAuthError,
+                    self.authenticator.perform,
+                    [self.achalls[0]]
+                )
+
+        # no TSIG key
+        self.assertRaises(
+                    ValueError,
+                    self.authenticator.perform,
+                    [self.achalls[4]]
+                )
+
+        # query exceptions
+        for excep in [NoAnswer, UnexpectedSource, BadResponse, OSError]:
+        	with mock.patch("dns.query.tcp") as query:
+        		query.side_effect = excep
+
+        		self.assertRaises(
+                    errors.LetsEncryptDNSAuthError,
+                    self.authenticator.perform,
+                    [self.achalls[0]]
+                )
+
+    def test_good_cleanup(self):
+        with mock.patch("dns.query.tcp") as query:
+            query.return_value = self.good_dns_msg
+
+            self.authenticator.cleanup(self.achalls[:3])
+
+    def test_bad_challs_cleanup(self):
+        # no challenges
+        self.assertRaises(ValueError, self.authenticator.cleanup, [])
+
+        # invalid challenge
+        self.assertRaises(
+            errors.LetsEncryptDNSAuthError,
+            self.authenticator.cleanup,
+            [self.achalls[3]]
+        )
+
+        # bad DNS message responses
+        for bad_dns_msg in self.bad_dns_msgs:
+            with mock.patch("dns.query.tcp") as query:
+                query.return_value = bad_dns_msg
+
+                self.assertRaises(
+                    errors.LetsEncryptDNSAuthError,
+                    self.authenticator.cleanup,
+                    [self.achalls[0]]
+                )
+
+        # no TSIG key
+        self.assertRaises(
+                    ValueError,
+                    self.authenticator.cleanup,
+                    [self.achalls[4]]
+                )
+
+        # query exceptions
+        for excep in [NoAnswer, UnexpectedSource, BadResponse, OSError]:
+        	with mock.patch("dns.query.tcp") as query:
+        		query.side_effect = excep
+
+        		self.assertRaises(
+                    errors.LetsEncryptDNSAuthError,
+                    self.authenticator.cleanup,
+                    [self.achalls[0]]
+                )
 
 
 if __name__ == '__main__':
