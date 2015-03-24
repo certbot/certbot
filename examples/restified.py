a6e1c3ed1771582d3e21f44fb0dc5f2ea8d432f7
Jakub Warmuz <jakub@warmuz.org>
2015-03-24 17:02:02 +0000

    Current Boulder supports registrations

--- examples/restified.py
+++ examples/restified.py
@@ -21,13 +21,9 @@ key = jose.JWKRSA.load(pkg_resources.resource_string(
     'letsencrypt.acme.jose', os.path.join('testdata', 'rsa512_key.pem')))
 net = network2.Network(NEW_REG_URL, key)
 
-contact = contact=('mailto:cert-admin@example.com', 'tel:+12025551212')
-# Boulder does not support registrations
-#regr = net.register(contact=contact)
-regr = messages2.RegistrationResource(
-    body=messages2.Registration(contact=contact, key=key.public()),
-    uri=NEW_REG_URL + '/fooooo',
-    new_authz_uri=NEW_AUTHZ_URL)
+regr = net.register(contact=(
+    'mailto:cert-admin@example.com', 'tel:+12025551212'))
+logging.debug(regr)
 
 authzr = net.request_challenges(
     identifier=messages2.Identifier(
