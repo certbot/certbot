07615e89514802416d579310c1bd7d73c090414c
cooperq <cooperq@eff.org>
2015-03-26 16:53:13 -0700

    refactor command line arguments into subcommands with seperate arguments. fixes #285

--- letsencrypt/client/configuration.py
+++ letsencrypt/client/configuration.py
@@ -34,21 +34,82 @@ class NamespaceConfig(object):
         return getattr(self.namespace, name)
 
     @property
+    def config_dir(self): # pylint: disable=missing-docstring
+        return constants.CONFIG_DIR
+
+    @property
+    def work_dir(self): # pylint: disable=missing-docstring
+        return constants.WORK_DIR
+
+    @property
+    def backup_dir(self):  # pylint: disable=missing-docstring
+        return os.path.join(
+            constants.WORK_DIR, constants.BACKUP_DIR)
+
+    @property
+    def key_dir(self):  # pylint: disable=missing-docstring
+        return os.path.join(
+            constants.CONFIG_DIR, constants.KEY_DIR)
+        
+    @property
+    def cert_dir(self):  # pylint: disable=missing-docstring
+        return os.path.join(
+            constants.CONFIG_DIR, constants.CERT_DIR)
+        
+    @property
+    def cert_path(self):  # pylint: disable=missing-docstring
+        return os.path.join(
+            constants.CONFIG_DIR, constants.CERT_DIR, constants.CERT_NAME)
+        
+    @property
+    def chain_path(self):  # pylint: disable=missing-docstring
+        return os.path.join(
+            constants.CONFIG_DIR, constants.CERT_DIR, constants.CHAIN_NAME)
+
+    @property
     def temp_checkpoint_dir(self):  # pylint: disable=missing-docstring
         return os.path.join(
-            self.namespace.work_dir, constants.TEMP_CHECKPOINT_DIR)
+            constants.WORK_DIR, constants.TEMP_CHECKPOINT_DIR)
 
     @property
     def in_progress_dir(self):  # pylint: disable=missing-docstring
-        return os.path.join(self.namespace.work_dir, constants.IN_PROGRESS_DIR)
+        return os.path.join(constants.WORK_DIR, constants.IN_PROGRESS_DIR)
 
     @property
     def cert_key_backup(self):  # pylint: disable=missing-docstring
         return os.path.join(
-            self.namespace.work_dir, constants.CERT_KEY_BACKUP_DIR,
+            constants.WORK_DIR, constants.CERT_KEY_BACKUP_DIR,
             self.namespace.server.partition(":")[0])
 
     # TODO: This should probably include the server name
     @property
     def rec_token_dir(self):  # pylint: disable=missing-docstring
-        return os.path.join(self.namespace.work_dir, constants.REC_TOKEN_DIR)
+        return os.path.join(constants.WORK_DIR, constants.REC_TOKEN_DIR)
+
+    @property
+    def apache_server_root(self):  # pylint: disable=missing-docstring
+        return constants.APACHE_SERVER_ROOT
+
+    @property
+    def apache_mod_ssl_conf(self):  # pylint: disable=missing-docstring
+        return constants.APACHE_MOD_SSL_CONF
+
+    @property
+    def apache_ctl(self):  # pylint: disable=missing-docstring
+        return constants.APACHE_CTL
+
+    @property
+    def apache_enmod(self):  # pylint: disable=missing-docstring
+        return constants.APACHE_ENMOD
+        
+    @property
+    def apache_init_script(self):  # pylint: disable=missing-docstring
+        return constants.APACHE_INIT_SCRIPT
+
+    @property
+    def le_vhost_ext(self):  # pylint: disable=missing-docstring
+        return constants.LE_VHOST_EXT
+
+    @property
+    def rollback(self):  # pylint: disable=missing-docstring
+        return constants.ROLLBACK
