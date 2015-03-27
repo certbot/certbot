903aaf12d1263e4ef2a869bcd83c4d97e1c2227d
cooperq <cooperq@eff.org>
2015-03-26 17:32:13 -0700

    fix tests

--- letsencrypt/client/tests/configuration_test.py
+++ letsencrypt/client/tests/configuration_test.py
@@ -10,19 +10,26 @@ class NamespaceConfigTest(unittest.TestCase):
     def setUp(self):
         from letsencrypt.client.configuration import NamespaceConfig
         namespace = mock.MagicMock(
-            work_dir='/tmp/foo', foo='bar', server='acme-server.org:443')
+            foo='bar', server='acme-server.org:443')
         self.config = NamespaceConfig(namespace)
 
     def test_proxy_getattr(self):
         self.assertEqual(self.config.foo, 'bar')
-        self.assertEqual(self.config.work_dir, '/tmp/foo')
 
     @mock.patch('letsencrypt.client.configuration.constants')
     def test_dynamic_dirs(self, constants):
+        constants.WORK_DIR='/tmp/foo'
+        constants.CONFIG_DIR='/tmp/etc/foo'
         constants.TEMP_CHECKPOINT_DIR = 't'
         constants.IN_PROGRESS_DIR = '../p'
         constants.CERT_KEY_BACKUP_DIR = 'c/'
+        constants.BACKUP_DIR = 'b'
+        constants.KEY_DIR = 'k'
         constants.REC_TOKEN_DIR = '/r'
+        self.assertEqual(self.config.work_dir, '/tmp/foo')
+        self.assertEqual(self.config.config_dir, '/tmp/etc/foo')
+        self.assertEqual(self.config.backup_dir, '/tmp/foo/b')
+        self.assertEqual(self.config.key_dir, '/tmp/etc/foo/k')
         self.assertEqual(self.config.temp_checkpoint_dir, '/tmp/foo/t')
         self.assertEqual(self.config.in_progress_dir, '/tmp/foo/../p')
         self.assertEqual(
