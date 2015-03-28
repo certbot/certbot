b67068e9865817a15cc958647794beb052d0a86b
James Kasten <jdkasten@umich.edu>
2015-03-27 21:09:03 -0700

    fix typo in challenges doc

--- letsencrypt/acme/challenges.py
+++ letsencrypt/acme/challenges.py
@@ -184,7 +184,7 @@ class ProofOfPossession(ClientChallenge):
         """Hints for "proofOfPossession" challenge.
 
         :ivar jwk: JSON Web Key (:class:`letsencrypt.acme.jose.JWK`)
-        :ivar list certs: List of :class:`M2Crypto.X509.X509` cetificates.
+        :ivar list certs: List of :class:`M2Crypto.X509.X509` certificates.
 
         """
         jwk = jose.Field("jwk", decoder=jose.JWK.from_json)
