7eee393b80eeede17c88ab77a77a898f0112a6f8
Jakub Warmuz <jakub@warmuz.org>
2015-04-02 09:05:53 +0000

    apt-get install dpkg-dev (fixes #276)

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -2,9 +2,14 @@
 
 # Tested with:
 #   - 12.04 (Travis)
-#   - 14.04 (Vagrant)
+#   - 14.04 (digitalocean x64, Vagrant)
+#   - 14.10 (digitalocean x64)
+
+# dpkg-dev: dpkg-architecture binary necessary to compile M2Crypto, c.f.
+#           #276, https://github.com/martinpaljak/M2Crypto/issues/62,
+#           M2Crypto setup.py:add_multiarch_paths
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
      python python-setuptools python-virtualenv python-dev gcc swig \
-     dialog libaugeas0 libssl-dev libffi-dev ca-certificates
+     dialog libaugeas0 libssl-dev libffi-dev ca-certificates dpkg-dev
