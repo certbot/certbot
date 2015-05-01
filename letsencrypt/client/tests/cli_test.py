17e8ddcb5cca97d6f3568ebbcc3cd755363b59da
Jakub Warmuz <jakub@warmuz.org>
2015-05-01 20:19:26 +0000

    Assert CLI test (--help) raises SystemExit

--- letsencrypt/client/tests/cli_test.py
+++ letsencrypt/client/tests/cli_test.py
@@ -5,6 +5,7 @@ class CLITest(unittest.TestCase):
 
     def test_it(self):
         from letsencrypt.client import cli
+        self.assertRaises(SystemExit, cli.main, ['--help'])
 
 
 if __name__ == '__main__':
