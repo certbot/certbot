5fbc5cee2e2ee3bbaa131d29391c9e61ea8465a2
Jakub Warmuz <jakub@warmuz.org>
2015-05-10 16:52:32 +0000

    Add tox.cover.sh for proper coveralls experience.

---
+++ tox.cover.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script is used by tox.ini (and thus Travis CI) in order to
+# generate separate stats for each package. It should be removed once
+# those packages are moved to separate repo.
+
+cover () {
+  # "-c /dev/null" makes sure setup.cfg is not loaded (multiple
+  # --with-cover add up, --cover-erase must not be set for coveralls
+  # to get all the data); --with-cover scopes coverage to only
+  # specific package, positional argument scopes tests only to
+  # specific package directory
+  nosetests -c /dev/null --with-cover --cover-package "$1" \
+            --cover-min-percentage="$2" "$1"
+}
+
+# don't use sequential composition (;), if letsencrypt_nginx returns
+# 0, coveralls submit will be triggered (c.f. .travis.yml,
+# after_success)
+cover letsencrypt 88 && cover acme 100 && \
+    cover letsencrypt_apache 67 && cover letsencrypt_nginx 90
