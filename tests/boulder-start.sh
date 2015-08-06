ffd0d6d1480fb1123774c7d89aa85cbc0cad8f0f
Brad Warren <bradmw@umich.edu>
2015-08-05 17:13:10 -0700

    Removed go get

--- tests/boulder-start.sh
+++ tests/boulder-start.sh
@@ -2,8 +2,6 @@
 # Download and run Boulder instance for integration testing
 
 export GOPATH="${GOPATH:-/tmp/go}"
-
-go get -d github.com/letsencrypt/boulder
 cd $GOPATH/src/github.com/letsencrypt/boulder
 ./start.py &
 # Hopefully start.py bootstraps before integration test is started...
