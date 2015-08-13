f39e6c672d2b0c3d7e231297916379ccaf7842c1
Brad Warren <bradmw@umich.edu>
2015-08-13 12:24:34 -0700

    Create databases in travis

--- tests/boulder-start.sh
+++ tests/boulder-start.sh
@@ -5,5 +5,10 @@ export GOPATH="${GOPATH:-/tmp/go}"
 
 go get -d github.com/letsencrypt/boulder
 cd $GOPATH/src/github.com/letsencrypt/boulder
+
+if [ "${TRAVIS}" == "true" ]; then
+  ./test/create_db.sh || die "unable to create the boulder database with test/create_db.sh"
+fi
+
 ./start.py &
 # Hopefully start.py bootstraps before integration test is started...
