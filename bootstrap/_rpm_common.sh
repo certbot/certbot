84d9c773a2727c6702871f7251bbdad53c6972be
Jakub Warmuz <jakub@warmuz.org>
2015-09-05 17:38:11 +0000

    #673 review comments

--- bootstrap/_rpm_common.sh
+++ bootstrap/_rpm_common.sh
@@ -4,30 +4,26 @@
 #   - Fedora 22 (x64)
 #   - Centos 7 (x64: on AWS EC2 t2.micro, DigitalOcean droplet)
 
+if type yum 2>/dev/null
+then
+    tool=yum
+elif type dnf 2>/dev/null
+then
+    tool=dnf
+else
+    echo "Neither yum nor dnf found. Aborting bootstrap!"
+    exit 1
+
 # "git-core" seems to be an alias for "git" in CentOS 7 (yum search fails)
-bootstrap() {
-
-    pkgs="git-core
-        python 
-        python-devel 
-        python-virtualenv 
-        python-devel 
-        gcc dialog 
-        augeas-libs 
-        openssl-devel 
-        libffi-devel 
-        ca-certificates"
-
-    if hash yum 2>/dev/null; then
-      yum install -y $pkgs;
-
-    elif hash dnf 2>/dev/null; then
-      dnf install -y $pkgs;
-
-    else
-      echo "Neither yum nor dnf found. Aborting bootstrap!"
-      exit 1;
-
-    fi
-}
-bootstrap
+$tool install -y \
+  git-core \
+  python \
+  python-devel \
+  python-virtualenv \
+  python-devel \
+  gcc \
+  dialog \
+  augeas-libs \
+  openssl-devel \
+  libffi-devel \
+  ca-certificates \
