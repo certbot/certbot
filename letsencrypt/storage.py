1a5f0c434e2b0956aabe8efc41cad6c0d629af00
James Kasten <jdkasten@umich.edu>
2015-09-11 00:02:09 -0700

    remove source of abspath problem... not side-effect

--- letsencrypt/storage.py
+++ letsencrypt/storage.py
@@ -223,7 +223,7 @@ class RenewableCert(object):  # pylint: disable=too-many-instance-attributes
         target = os.readlink(link)
         if not os.path.isabs(target):
             target = os.path.join(os.path.dirname(link), target)
-        return target
+        return os.path.abspath(target)
 
     def current_version(self, kind):
         """Returns numerical version of the specified item.
@@ -268,8 +268,7 @@ class RenewableCert(object):  # pylint: disable=too-many-instance-attributes
         if kind not in ALL_FOUR:
             raise errors.CertStorageError("unknown kind of item")
         where = os.path.dirname(self.current_target(kind))
-        return os.path.abspath(
-            os.path.join(where, "{0}{1}.pem".format(kind, version)))
+        return os.path.join(where, "{0}{1}.pem".format(kind, version))
 
     def available_versions(self, kind):
         """Which alternative versions of the specified kind of item exist?
