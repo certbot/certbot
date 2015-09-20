7f42beda95c189ffaf457bbba0d3585f27b25a6b
Brandon Kreisel <kreiselb@gmail.com>
2015-09-20 19:07:55 -0400

    Add homebrew and add dialog dependency per #413

--- bootstrap/mac.sh
+++ bootstrap/mac.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+if hash brew 2>/dev/null; then
+    echo "Homebrew Installed"
+else
+    echo "Homebrew Not Installed\nDownloading..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 brew install augeas
+brew install dialog
