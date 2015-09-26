d337865f484ddc9aa3dda8628d845685e2a20c5d
Jakub Warmuz <jakub@warmuz.org>
2015-09-26 10:54:24 +0000

    add missing pkg_resources comment

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ install_requires = [
     'python2-pythondialog>=3.2.2rc1',  # Debian squeeze support, cf. #280
     'pytz',
     'requests',
-    'setuptools',
+    'setuptools',  # pkg_resources
     'zope.component',
     'zope.interface',
 ]
