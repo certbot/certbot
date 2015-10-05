eec5542cb3a479f57f6f69c407aa9aaae287e247
Jakub Warmuz <jakub@warmuz.org>
2015-10-05 21:39:34 +0000

    lint archlinux bootstrap script

--- bootstrap/archlinux.sh
+++ bootstrap/archlinux.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-pacman -S git python2 python2-virtualenv gcc dialog augeas openssl libffi ca-certificates
+pacman -S \
+  git \
+  python2 \
+  python2-virtualenv \
+  gcc \
+  dialog \
+  augeas \
+  openssl \
+  libffi \
+  ca-certificates \
