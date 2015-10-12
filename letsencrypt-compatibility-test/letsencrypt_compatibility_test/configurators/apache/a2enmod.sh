7defdb18193500cfb287bde092a089c087887b33
Brad Warren <bradmw@umich.edu>
2015-10-12 10:08:35 -0700

    Updated a2enmod comments

--- letsencrypt-compatibility-test/letsencrypt_compatibility_test/configurators/apache/a2enmod.sh
+++ letsencrypt-compatibility-test/letsencrypt_compatibility_test/configurators/apache/a2enmod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # An extremely simplified version of `a2enmod` for enabling modules in the
-# httpd docker image. First argument is the ServerRoot and the second is the
-# module to be enabled.
+# httpd docker image. First argument is the Apache ServerRoot which should be
+# an absolute path. The second is the module to be enabled, such as `ssl`.
 
 confdir=$1
 module=$2
