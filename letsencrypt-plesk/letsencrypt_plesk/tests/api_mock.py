b101b4e83f2fa9c85a46e1e6cd07cac51a04e231
Eugene Kazakov <ekazakov@parallels.com>
2015-10-13 11:49:46 +0600

    Satisfy pylint.

--- letsencrypt-plesk/letsencrypt_plesk/tests/api_mock.py
+++ letsencrypt-plesk/letsencrypt_plesk/tests/api_mock.py
@@ -6,24 +6,30 @@ import os
 from letsencrypt_plesk import api_client
 
 
-class PleskApiMock:
+class PleskApiMock(object):
+    """Class helper for mock PleskApiClient"""
+
     def __init__(self):
         self.request = mock.MagicMock()
         self._request = None
 
     def expects_request(self, request):
+        """Register a new expectation of the request"""
         with open(self._api_file(request)) as f:
             self._request = api_client.XmlToDict(f.read(), force_array=True)
 
     def will_response(self, response):
+        """Stub by returning the response"""
         with open(self._api_file(response)) as f:
             self.request.return_value = api_client.XmlToDict(f.read())
 
     def assert_called(self):
+        """Assert that API has met the expectations"""
         if self._request:
             self.request.assert_called_once_with(self._request)
         else:
             self.request.assert_not_called()
+        self._request = None
 
     @staticmethod
     def _api_file(filename):
