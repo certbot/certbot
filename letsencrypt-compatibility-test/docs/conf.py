4a3350e90e02388b1d303e55c34ea57a55238db0
Jakub Warmuz <jakub@warmuz.org>
2015-10-18 15:04:46 +0000

    Add extensions for subpkgs docs

--- letsencrypt-compatibility-test/docs/conf.py
+++ letsencrypt-compatibility-test/docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'repoze.sphinx.autointerface',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
