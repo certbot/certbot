607ea59fd35c8c039c44b4a7022bf0347985507f
Jakub Warmuz <jakub@warmuz.org>
2015-10-18 15:04:46 +0000

    Add extensions for subpkgs docs

--- acme/docs/conf.py
+++ acme/docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.programoutput',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
