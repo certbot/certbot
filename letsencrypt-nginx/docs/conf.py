dfdb64c505ed0f16c7d32afeef4a457593fb6edb
Jakub Warmuz <jakub@warmuz.org>
2015-10-18 15:31:06 +0000

    sphinx: default_role py:obj

--- letsencrypt-nginx/docs/conf.py
+++ letsencrypt-nginx/docs/conf.py
@@ -87,7 +87,7 @@ exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-#default_role = None
+default_role = 'py:obj'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
