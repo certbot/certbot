e8f90ff9939b018f40444490bdff87ef38f62415
root <xamnesiax@soldierx.com>
2015-10-24 23:59:36 +0100

    Add flag in to _common.sh integration test

--- tests/integration/_common.sh
+++ tests/integration/_common.sh
@@ -20,6 +20,7 @@ letsencrypt_test () {
         --manual-test-mode \
         $store_flags \
         --text \
+        --redirect \
         --agree-dev-preview \
         --agree-tos \
         --email "" \
