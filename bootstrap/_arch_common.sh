8c7d0614e5b13f1ca487a8bd1ab0568fcf3f0437
Chhatoi Pritam Baral <pritam@pritambaral.com>
2015-10-28 03:40:08 +0530

    Respect pre-installed gcc package on Arch Linux

--- bootstrap/_arch_common.sh
+++ bootstrap/_arch_common.sh
@@ -4,6 +4,15 @@
 #   - Manjaro 15.09 (x86_64)
 #   - ArchLinux (x86_64)
 
+# Both "gcc-multilib" and "gcc" packages provide gcc. If user already has
+# "gcc-multilib" installed, let's stick to their choice
+if pacman -Qc gcc-multilib &>/dev/null
+then
+	GCC_PACKAGE="gcc-multilib";
+else
+	GCC_PACKAGE="gcc";
+fi
+
 # "python-virtualenv" is Python3, but "python2-virtualenv" provides
 # only "virtualenv2" binary, not "virtualenv" necessary in
 # ./bootstrap/dev/_common_venv.sh
@@ -11,7 +20,7 @@ pacman -S --needed \
   git \
   python2 \
   python-virtualenv \
-  gcc \
+  "$GCC_PACKAGE" \
   dialog \
   augeas \
   openssl \
