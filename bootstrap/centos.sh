e9b1c25a8df4f78a596f71a455160fdde8da4a77
Nick <linickx@gmail.com>
2015-10-28 13:15:26 +0000

    Update letsencrypt-auto with CentOS6 SCL for python 2.7

--- bootstrap/centos.sh
+++ bootstrap/centos.sh
@@ -1,1 +1,27 @@
-_rpm_common.sh
+#!/bin/sh
+
+# Tested with:
+# - CentOS 6.7 (x64)
+
+
+if [ `lsb_release -rs | cut -f1 -d.` -eq 6 ]
+then
+	# Setup SCL Repo - https://wiki.centos.org/AdditionalResources/Repositories/SCL
+	yum -y install centos-release-SCL
+
+	# Now install Deps
+	yum -y install \
+		git-core \
+		python27 \
+		python27-python-devel \
+		python27-python-virtualenv  \
+		gcc \
+		dialog \
+		augeas-libs \
+		openssl-devel \
+		libffi-devel \
+		ca-certificates
+
+else
+	source $BOOTSTRAP/_rpm_common.sh
+fi
