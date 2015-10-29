c3fbed1f81484daf1cb440b55ca3dc2f0af55ee7
Jakub Warmuz <jakub@warmuz.org>
2015-10-29 08:19:54 +0000

    Offline unittest v2.
    Supersedes https://github.com/letsencrypt/letsencrypt/pull/1183.

--- acme/acme/challenges_test.py
+++ acme/acme/challenges_test.py
@@ -359,9 +359,9 @@ class DVSNIResponseTest(unittest.TestCase):
             cert=mock.sentinel.cert))
         mock_verify_cert.assert_called_once_with(self.msg, mock.sentinel.cert)
 
-    def test_simple_verify_false_on_probe_error(self):
-        chall = mock.Mock()
-        chall.probe_cert.side_effect = errors.Error
+    @mock.patch('acme.challenges.DVSNIResponse.probe_cert')
+    def test_simple_verify_false_on_probe_error(self, mock_probe_cert):
+        mock_probe_cert.side_effect = errors.Error
         self.assertFalse(self.msg.simple_verify(
             self.chall, self.domain, self.key.public_key()))
 
