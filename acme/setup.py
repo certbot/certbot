f8185c1913610c4911d03e2512c1d0b7d59fb774
Jakub Warmuz <jakub@warmuz.org>
2015-10-31 11:47:25 +0000

    Add Python 2.6 setup.py classifiers.

--- acme/setup.py
+++ acme/setup.py
@@ -58,6 +58,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
