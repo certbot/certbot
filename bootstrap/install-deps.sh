be4cd194377aceb166fbf552c4afd693e0cb237f
Timo Gurr <timo.gurr@gmail.com>
2015-11-02 16:23:17 +0100

    Add Exherbo bootstrapping

--- bootstrap/install-deps.sh
+++ bootstrap/install-deps.sh
@@ -26,6 +26,9 @@ elif [ -f /etc/redhat-release ] ; then
 elif [ -f /etc/gentoo-release ] ; then
   echo "Bootstrapping dependencies for Gentoo-based OSes..."
   $SUDO $BOOTSTRAP/_gentoo_common.sh
+elif [ -f /etc/exherbo-release ] ; then
+  echo "Bootstrapping dependencies for Exherbo-based OSes..."
+  $SUDO $BOOTSTRAP/exherbo.sh
 elif uname | grep -iq FreeBSD ; then
   echo "Bootstrapping dependencies for FreeBSD..."
   $SUDO $BOOTSTRAP/freebsd.sh
