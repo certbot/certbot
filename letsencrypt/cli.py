e289b22e932bd4ed8c4580b57b6f5e2aab644eda
Miquel Ruiz <mruiz@cpan.org>
2015-11-15 18:04:59 +0000

    Fix broken tests and add a new one

--- letsencrypt/cli.py
+++ letsencrypt/cli.py
@@ -129,7 +129,7 @@ def _determine_account(args, config):
         else:  # no account registered yet
             if args.email is None and not args.register_unsafely_without_email:
                 args.email = display_ops.get_email()
-            else:
+            if not args.email:  # get_email might return ""
                 args.email = None
 
             def _tos_cb(regr):
