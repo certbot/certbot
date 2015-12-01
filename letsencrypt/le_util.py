4a2e40c365ec4fa5bb6d19aa0d997ede6f0a0820
Brad Warren <bradmw@umich.edu>
2015-11-30 18:55:28 -0800

    Added add_deprecated_argument tests

--- letsencrypt/le_util.py
+++ letsencrypt/le_util.py
@@ -6,8 +6,9 @@ import logging
 import os
 import platform
 import re
-import subprocess
 import stat
+import subprocess
+import sys
 
 from letsencrypt import errors
 
@@ -258,7 +259,7 @@ def safe_email(email):
         return False
 
 
-def add_deprecated_argument(add_argument, argument_name):
+def add_deprecated_argument(add_argument, argument_name, nargs):
     """Adds a deprecated argument with the name argument_name.
 
     Deprecated arguments are not shown in the help. If they are used on
@@ -268,11 +269,14 @@ def add_deprecated_argument(add_argument, argument_name):
     :param callable add_argument: Function that adds arguments to an
         argument parser/group.
     :param str argument_name: Name of deprecated argument.
+    :param nargs: Value for nargs when adding the argument to argparse.
 
     """
     class ShowWarning(argparse.Action):
         """Action to log a warning when an argument is used."""
         def __call__(self, unused1, unused2, unused3, option_string=None):
-            print "Use of {0} is deprecated".format(option_string)
+            sys.stderr.write(
+                "Use of {0} is deprecated\n".format(option_string))
 
-    add_argument(argument_name, action=ShowWarning, help=argparse.SUPPRESS)
+    add_argument(argument_name, action=ShowWarning,
+                 help=argparse.SUPPRESS, nargs=nargs)
