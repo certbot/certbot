4ef302c8a6ba171e68417abb27483f2bb5061284
Vince <git@darkain.com>
2015-12-03 13:39:19 -0800

    Fixing grammar

--- letsencrypt/cli.py
+++ letsencrypt/cli.py
@@ -306,7 +306,7 @@ def _report_new_cert(cert_path, fullchain_path):
 def _suggest_donate():
     "Suggest a donation to support Let's Encrypt"
     reporter_util = zope.component.getUtility(interfaces.IReporter)
-    msg = ("If like Let's Encrypt, please consider supporting our work by:\n\n"
+    msg = ("If you like Let's Encrypt, please consider supporting our work by:\n\n"
            "Donating to ISRG / Let's Encrypt:   https://letsencrypt.org/donate\n"
            "Donating to EFF:                    https://eff.org/donate-le\n\n")
     reporter_util.add_message(msg, reporter_util.LOW_PRIORITY)
