ce843a69df379d2d249dadd9ce6f12384dca4c9d
Bas Westerbaan <bas@westerbaan.name>
2015-12-06 02:12:34 +0100

    demandimport: disable for python <2.7

--- letsencrypt/ondemandimport.py
+++ letsencrypt/ondemandimport.py
@@ -3,10 +3,14 @@
     modules are not used at all --- however, some third-party modules
     might cause problems as some `ImportError's aren't raised immediately. """
 
+import sys
 import demandimport
 
 def enable():
     """ Configures and enables on-demand module importing. """
+    if sys.version_info < (2, 7):
+        return # demandimport isn't reliable on Python <2.7
+
     demandimport.enable()
 
     demandimport.ignore('PyICU')      # parsedatetime
