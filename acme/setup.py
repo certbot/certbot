a28f8fe4427c12c2523b16903325d0362b53123e
Brad Warren <bradmw@umich.edu>
2015-12-17 17:47:15 -0800

    Drop version dependency

--- acme/setup.py
+++ acme/setup.py
@@ -12,8 +12,8 @@ install_requires = [
     'cryptography>=0.8',
     'ndg-httpsclient',  # urllib3 InsecurePlatformWarning (#304)
     'pyasn1',  # urllib3 InsecurePlatformWarning (#304)
-    # Connection.set_tlsext_host_name (>=0.13), X509Req.get_extensions (>=0.15)
-    'PyOpenSSL>=0.15',
+    # Connection.set_tlsext_host_name (>=0.13)
+    'PyOpenSSL>=0.13',
     'pyrfc3339',
     'pytz',
     'requests',
