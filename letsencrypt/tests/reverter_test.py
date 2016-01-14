54207f9ef3458bb1b22c4481e699476211cbb7c6
Filip Ochnik <filip.ochnik@gmail.com>
2016-01-14 23:15:22 +0700

    Test view_config_changes with for_logging=True

--- letsencrypt/tests/reverter_test.py
+++ letsencrypt/tests/reverter_test.py
@@ -385,6 +385,15 @@ class TestFullCheckpointsReverter(unittest.TestCase):
         self.assertRaises(
             errors.ReverterError, self.reverter.view_config_changes)
 
+    def test_view_config_changes_for_logging(self):
+        self._setup_three_checkpoints()
+
+        config_changes = self.reverter.view_config_changes(for_logging=True)
+
+        self.assertIn("First Checkpoint", config_changes)
+        self.assertIn("Second Checkpoint", config_changes)
+        self.assertIn("Third Checkpoint", config_changes)
+
     def _setup_three_checkpoints(self):
         """Generate some finalized checkpoints."""
         # Checkpoint1 - config1
