c87b5d6a7834553379b8f1a105adb35685499b96
dmwilcox <dmwilcox@gmail.com>
2016-01-21 00:56:29 -0800

    Hook the MTA config generation into the new config container.

--- TestConfig.py
+++ TestConfig.py
@@ -1,4 +1,5 @@
 import copy
+import itertools
 import logging
 import unittest
 
@@ -67,6 +68,63 @@ class TestAcceptableMX(unittest.TestCase):
         new_policy = Config.AcceptableMX(domain=self.old_config.domain)
         mx_policy = self.old_config.update(new_policy)
         self.assertEquals(mx_policy.domain, self.old_config.domain)
+
+
+class TestConfig(unittest.TestCase):
+    """Test entire configuration.
+
+    Currently lower coverage is being obtained since string sets are
+    being compared rather than returned objects. Comparison logic for
+    the config objects isn't clear yet and proof that they function is enough.
+    """
+
+    def setUp(self):
+        self.config = Config.Config()
+        domain_policies = self.config._data['acceptable-mxs']
+        self.mail_domains = ['gmail.com', 'yahoo.com', 'hotmail.com', '123.cn', 'qq.com']
+        for domain in self.mail_domains:
+            new = Config.AcceptableMX(domain=domain)
+            new.add_acceptable_mx('.' + domain)
+            domain_policies[domain] = new
+
+    def testGetAllMxItems(self):
+        """Make sure the basic use case of get_all_mx_items functions."""
+        # [ ('.gmail.com', 'gmail.com'), ('.yahoo.com', 'yahoo.com'), ... ]
+        control_data = [ ('.' + domain, domain) for domain in self.mail_domains ]
+        test_data = [ (mx, p.domain) for mx, p in self.config.get_all_mx_items() ]
+        self.assertListEqual(sorted(test_data), sorted(control_data))
+
+    def testGetAllMxItemsMultiMX(self):
+        config = copy.deepcopy(self.config)
+        domain_policy = config.acceptable_mxs.get('gmail.com')
+        # deal with reality, mail.google.com
+        domain_policy.add_acceptable_mx('.mail.google.com')
+        control_data = [ ('.' + domain, domain) for domain in self.mail_domains ]
+        control_data.append(('.mail.google.com', 'gmail.com'))
+        test_data = [ (mx, p.domain) for mx, p in config.get_all_mx_items() ]
+        self.assertListEqual(sorted(test_data), sorted(control_data))
+
+    def testGetMXtoDomainPolicy(self):
+        control_data = dict([ ('.' + domain, set([domain]))
+                             for domain in self.mail_domains ])
+        test_data = {}
+        for mx, pset in self.config.get_mx_to_domain_policy_map().items():
+            policy_list = [ p.domain for p in pset ]
+            test_data[mx] = set(policy_list)
+        self.assertDictEqual(test_data, control_data)
+
+    def testGetMXtoDomainPolicyMultiMX(self):
+        config = copy.deepcopy(self.config)
+        domain_policy = config.acceptable_mxs.get('gmail.com')
+        domain_policy.add_acceptable_mx('.mail.google.com')
+        control_data = dict([ ('.' + domain, set([domain]))
+                            for domain in self.mail_domains ])
+        control_data['.mail.google.com'] = set(['gmail.com'])
+        test_data = {}
+        for mx, pset in config.get_mx_to_domain_policy_map().items():
+            policy_list = [ p.domain for p in pset ]
+            test_data[mx] = set(policy_list)
+        self.assertDictEqual(test_data, control_data)
 
 
 if __name__ == '__main__':
