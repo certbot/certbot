e6c608b0a69ecd87318f2eef13874a5be94a8fe1
Peter Eckersley <pde@eff.org>
2016-01-21 15:55:44 -0800

    Do not autoexpand

--- letsencrypt/cli.py
+++ letsencrypt/cli.py
@@ -323,7 +323,7 @@ def _handle_subset_cert_request(config, domains, cert):
              br=os.linesep)
     if config.expand or config.renew_by_default or zope.component.getUtility(
             interfaces.IDisplay).yesno(question, "Expand", "Cancel",
-                                       default=True):
+                                       cli_flag="--expand (or in some cases, --duplicate)"):
         return "renew", cert
     else:
         reporter_util = zope.component.getUtility(interfaces.IReporter)
