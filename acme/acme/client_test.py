f1faedaa72e7833da49e6f33ecb2e3ce6a12cf6d
Peter Eckersley <pde@eff.org>
2016-02-12 12:48:20 -0800

    This two digit year case is hard to trigger

--- acme/acme/client_test.py
+++ acme/acme/client_test.py
@@ -194,7 +194,7 @@ class ClientTest(unittest.TestCase):
         self.assertEqual(
             datetime.datetime(1999, 12, 31, 23, 59, 59),
             self.client.retry_after(response=self.response, default=10))
-        self.response.headers['Retry-After'] = 'Fri, 31 Dec 17 23:59:59 GMT'
+        self.response.headers['Retry-After'] = 'Fri, 31-Dec-17 23:59:59 GMT'
         self.assertEqual(
             datetime.datetime(2017, 12, 31, 23, 59, 59),
             self.client.retry_after(response=self.response, default=10))
