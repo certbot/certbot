f72bcb5ea40b3aaa9f94ce4ddded6032c5bed448
Noah Swartz <noah@eff.org>
2016-02-17 15:57:14 -0800

    print only challenge changes to configs

--- letsencrypt-apache/letsencrypt_apache/configurator.py
+++ letsencrypt-apache/letsencrypt_apache/configurator.py
@@ -1429,7 +1429,6 @@ class ApacheConfigurator(augeas_configurator.AugeasConfigurator):
 
         """
         self.config_test()
-        logger.debug(self.reverter.view_config_changes(for_logging=True))
         self._reload()
 
     def _reload(self):
