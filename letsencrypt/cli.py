55eeb655bbcc55520b5a415d68e812d7d9166a5a
Brad Warren <bmw@eff.org>
2016-03-14 20:11:31 -0700

    Moved VERBS back to cli.py

--- letsencrypt/cli.py
+++ letsencrypt/cli.py
@@ -629,9 +629,13 @@ class HelpfulArgumentParser(object):
     """
 
     def __init__(self, args, plugins, detect_defaults=False):
-
         from letsencrypt import main
-        self.VERBS = main.VERBS
+        self.VERBS = {"auth": main.obtain_cert, "certonly": main.obtain_cert,
+                      "config_changes": main.config_changes, "run": main.run,
+                      "install": main.install, "plugins": main.plugins_cmd,
+                      "renew": renew, "revoke": main.revoke,
+                      "rollback": main.rollback, "everything": main.run}
+
         # List of topics for which additional help can be provided
         HELP_TOPICS = ["all", "security", "paths", "automation", "testing"] + list(self.VERBS)
 
