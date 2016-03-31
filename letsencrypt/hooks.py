8f8d80e7d1f1a35b7db1c36b81c14eed907740a9
Peter Eckersley <pde@eff.org>
2016-03-30 17:50:22 -0700

    Don't strip " from PATH entries
    There shouldn't be any, but if they're there perhaps they're there for a
    reason.

--- letsencrypt/hooks.py
+++ letsencrypt/hooks.py
@@ -91,7 +91,6 @@ def _which(program):
             return program
     else:
         for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
             exe_file = os.path.join(path, program)
             if _is_exe(exe_file):
                 return exe_file
