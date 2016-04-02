dd14b9980e7e173729ce7c040f0293fd1dbc9ce3
Seth Schoen <schoen@eff.org>
2016-04-01 18:21:16 -0700

    Don't call our tempfile "tempfile"

--- letsencrypt/tests/storage_test.py
+++ letsencrypt/tests/storage_test.py
@@ -746,18 +746,18 @@ class RenewableCertTests(BaseRenewableCertTest):
         # Mostly tested by the process of creating and updating lineages,
         # but we can test that this successfully creates files, removes
         # unneeded items, and preserves comments.
-        tempfile = os.path.join(self.tempdir, "sample-file")
-        tempfile2 = os.path.join(self.tempdir, "sample-file.new")
-        with open(tempfile, "w") as f:
+        temp = os.path.join(self.tempdir, "sample-file")
+        temp2 = os.path.join(self.tempdir, "sample-file.new")
+        with open(temp, "w") as f:
             f.write("[renewalparams]\nuseful = value # A useful value\n"
                     "useless = value # Not needed\n")
         target = {}
         for x in ALL_FOUR:
             target[x] = "somewhere"
-        relevant = {"useful": "new_value"}
+        relevant_data = {"useful": "new_value"}
         from letsencrypt import storage
-        storage.write_renewal_config(tempfile, tempfile2, target, relevant)
-        with open(tempfile2, "r") as f:
+        storage.write_renewal_config(temp, temp2, target, relevant_data)
+        with open(temp2, "r") as f:
             content = f.read()
         # useful value was updated
         assert "useful = new_value" in content
