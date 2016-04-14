d6492867249dcb1f9a470ad9a671d18c1220e07e
Brent Royal-Gordon <brent@architechies.com>
2016-04-14 00:45:33 -0700

    Initial checkin of Heroku plugin

---
+++ letsencrypt/plugins/heroku.py
@@ -0,0 +1,239 @@
+"""Herkou plugin."""
+import os
+import logging
+import pipes
+import shutil
+import signal
+import socket
+import subprocess
+import sys
+import tempfile
+import time
+
+try:
+    from shlex import quote as cmd_quote
+except ImportError:
+    from pipes import quote as cmd_quote
+
+from subprocess import check_output, CalledProcessError
+
+import zope.component
+import zope.interface
+
+from acme import challenges
+
+from letsencrypt import errors
+from letsencrypt import interfaces
+from letsencrypt.plugins import common
+
+
+logger = logging.getLogger(__name__)
+
+
+@zope.interface.implementer(interfaces.IAuthenticator)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Authenticator(common.Plugin):
+    """Heroku Authenticator.
+
+    This plugin writes challenge files into the ./public folder, then 
+    commits them to git and pushes them to a remote repository. 
+    On Heroku, this will deploy the challenge files to the server, where
+    they'll be served to Let's Encrypt as needed.
+
+    .. todo:: Support for `~.challenges.TLSSNI01`.
+
+    """
+    hidden = False
+
+    description = "Authenticate a Heroku app"
+
+    # a disclaimer about your current IP being transmitted to Let's Encrypt's servers.
+    IP_DISCLAIMER = """\
+NOTE: The IP of this machine will be publicly logged as having requested this certificate. \
+If you're running letsencrypt in manual mode on a machine that is not your server, \
+please ensure you're okay with that.
+
+Are you OK with your IP being logged?
+"""
+
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self._root = self.conf("root")
+        self._remote = self.conf("remote")
+        self._branch = self.conf("branch")
+        self._httpd = None
+
+    @classmethod
+    def add_parser_arguments(cls, add):
+        add("root", default="public", help="Directory containing static assets.")
+        add("remote", default="heroku", help="git remote to push to for deployment.")
+        add("branch", default="master", help="git branch to push for deployment.")
+        add("public-ip-logging-ok", action="store_true",
+            help="Automatically allows public IP logging.")
+
+    def prepare(self):  # pylint: disable=missing-docstring,no-self-use
+        #if self.config.noninteractive_mode and not self.conf("test-mode"):
+        #    raise errors.PluginError("Running manual mode non-interactively is not supported")
+        pass
+
+    def more_info(self):  # pylint: disable=missing-docstring,no-self-use
+        return ("This plugin requires user's manual intervention in setting "
+                "up an HTTP server for solving http-01 challenges and thus "
+                "does not need to be run as a privileged process. "
+                "Alternatively shows instructions on how to use Python's "
+                "built-in HTTP server.")
+
+    def get_chall_pref(self, domain):
+        # pylint: disable=missing-docstring,no-self-use,unused-argument
+        return [challenges.HTTP01]
+
+    def perform(self, achalls):  # pylint: disable=missing-docstring
+        # XXX make these configurable
+        root = self._root
+        remote = self._remote
+        branch = self._branch
+        
+        self._preflight(root=root, remote=remote, branch=branch)
+        
+        owner = os.stat(root).st_uid
+        directory = root + "/" + achalls[0].URI_ROOT_PATH
+        
+        self._clear_directory(directory=directory)
+        for achall in achalls:
+            self._write_challenge(achall, directory=directory)
+        self._chown_challenges(root=root, directory=directory, owner=owner)
+        self._commit(directory=directory)
+
+        self._deploy(remote=remote)
+
+        responses = []
+        # TODO: group achalls by the same socket.gethostbyname(_ex)
+        # and prompt only once per server (one "echo -n" per domain)
+        for achall in achalls:
+            responses.append(self._wait_for_challenge_validation(achall))
+        return responses
+    
+    def _preflight(self, root, remote, branch):
+        if not self.conf("public-ip-logging-ok"):
+            if not zope.component.getUtility(interfaces.IDisplay).yesno(
+                    self.IP_DISCLAIMER, "Yes", "No",
+                    cli_flag="--manual-public-ip-logging-ok"):
+                raise errors.PluginError("Must agree to IP logging to proceed")
+
+        if not os.path.exists(root):
+            raise errors.PluginError("The '" + root + "' folder doesn't exist")
+        
+        # Make sure we're on the right branch
+        try:
+            output = self._run_as_user(["git", "symbolic-ref", "--short", "-q", "HEAD"], skip_if_dry=False)
+            checked_out = output.rstrip()
+            if checked_out != branch:
+                raise errors.PluginError("Working copy has '" + checked_out +"' checked out, not '" + branch + "'")
+        except CalledProcessError:
+            raise errors.PluginError("Cannot identify a checked-out git branch")
+
+        # git remote update will fail if there's no such remote, but it's also necessary 
+        # for getting the status in the next step.
+        try:
+            self._run_as_user(["git", "remote", "update", remote], skip_if_dry=False)
+        except CalledProcessError:
+            raise errors.PluginError("The '" + remote + "' git remote is not configured (use --heroku-remote to set a different one)")
+
+        try:
+            self._run_as_user(["git", "diff", "--staged", "--quiet", remote + "/" + branch], skip_if_dry=False)
+        except CalledProcessError:
+            raise errors.PluginError("The working copy is out of date with the '" + remote + "' remote")
+
+    def _clear_directory(self, directory):
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+
+    def _write_challenge(self, achall, directory):
+        response, validation = achall.response_and_validation()
+        
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file = directory + "/" + achall.chall.encode("token")
+        with open(file, "w") as validation_file:
+            validation_file.write(validation.encode())
+    
+    def _chown_challenges(self, root, directory, owner):
+        while root != os.path.dirname(directory):
+            directory = os.path.dirname(directory)
+        
+        logger.warning("Chowning under " + directory)
+        os.chown(directory, owner, -1)
+        for (dirpath, dirs, files) in os.walk(directory):
+            for file in dirs + files:
+                os.chown(os.path.join(dirpath, file), owner, -1)
+
+    def _commit(self, directory):
+        self._run_as_user(["git", "add", directory])
+        
+        commit_message = "Challenges for Let's Encrypt certificate"
+        if self.config.staging:
+            commit_message += " (testing only)"
+        self._run_as_user(["git", "commit", "-m", commit_message])
+
+    def _deploy(self, remote):
+        logger.warning("Pushing to '" + remote + "'...")
+        self._run_as_user(["git", "push", remote])
+
+    def _wait_for_challenge_validation(self, achall):
+        response, validation = achall.response_and_validation()
+
+        logger.warning("Verifying challenge for " + achall.domain + " (Ctrl-C to skip)")
+        try:
+            while not response.simple_verify(
+                    achall.chall, achall.domain,
+                    achall.account_key.public_key(), self.config.http01_port):
+                time.sleep(10)
+        except KeyboardInterrupt:
+            pass
+
+        return response
+
+    def _run_as_user(self, command, shell=False, skip_if_dry=True):
+        # If we need to sudo back, set that up.
+        sudo_user = os.environ["SUDO_USER"]
+
+        if sudo_user is None:
+            full_command = command
+        else:
+            su_part = ["sudo", "-u", sudo_user]
+            if shell:
+                full_command = su_part + ["-s", command]
+                shell = False
+            else:
+                full_command = su_part + command
+        
+        # Format a loggable version of the command.
+        if os.getuid() == 0:
+            prompt = "# "
+        else:
+            prompt = "$ "
+
+        if shell:
+            description = prompt + full_command
+        else:
+            description = prompt + " ".join(map(cmd_quote, full_command))
+
+        # Do it, or don't.
+        if skip_if_dry and self.config.dry_run:
+            logger.warning("Would run: " + description)
+            return None
+        else:
+            logger.info("Running: " + description)
+            return check_output(full_command)
+
+    def _notify_and_wait(self, message):  # pylint: disable=no-self-use
+        # TODO: IDisplay wraps messages, breaking the command
+        #answer = zope.component.getUtility(interfaces.IDisplay).notification(
+        #    message=message, height=25, pause=True)
+        sys.stdout.write(message)
+        raw_input("Press ENTER to continue")
+
+    def cleanup(self, achalls):
+        # pylint: disable=missing-docstring,no-self-use,unused-argument
+        pass
