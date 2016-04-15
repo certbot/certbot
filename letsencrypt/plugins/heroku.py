44d7f9956a5efa756cec230f1a94af63855e1b45
Brent Royal-Gordon <brent@architechies.com>
2016-04-14 17:57:20 -0700

    Fix weird ordering issue

--- letsencrypt/plugins/heroku.py
+++ letsencrypt/plugins/heroku.py
@@ -130,12 +130,12 @@ class Authenticator(common.Plugin):
     def _chown_challenges(self, root, directory, owner):
         while root != os.path.dirname(directory):
             directory = os.path.dirname(directory)
-        
+
         os.chown(directory, owner, -1)
         for (dirpath, dirs, files) in os.walk(directory):
             for file in dirs + files:
                 os.chown(os.path.join(dirpath, file), owner, -1)
-    
+
     def _commit(self, directory):
         self._git_client.stage_file(directory)
     
@@ -143,14 +143,14 @@ class Authenticator(common.Plugin):
         if self.config.staging:
             commit_message += " (testing only)"
         self._git_client.commit(message=commit_message)
-        
+    
     def _deploy(self, remote):
         logger.debug("Pushing to '" + remote + "'...")
         self._git_client.push_to_remote(remote)
         
     def _wait_for_challenge_validation(self, achall):
         response, validation = achall.response_and_validation()
-
+        
         logger.warning("Verifying challenge for " + achall.domain + ". This might take a few minutes if your app is restarting. (Ctrl-C to skip.)")
         try:
             while not response.simple_verify(
@@ -165,86 +165,6 @@ class Authenticator(common.Plugin):
     def cleanup(self, achalls):
         # pylint: disable=missing-docstring,no-self-use,unused-argument
         pass
-
-class GitClient:
-    def __init__(self, dry_run=False):
-        self.dry_run = dry_run
-
-    """
-    Builds a git command with the provided arguments.
-    """
-    def git(self, *args):
-        command = Command('git', *args).resudoed()
-        command.on_returncode(128, raises=GitClient.NoRepositoryError)
-        return command
-    
-    """
-    Determines the branch that the working copy in the current working directory
-    has checked out.
-    """
-    def checked_out_branch(self):
-        command = self.git("symbolic-ref", "--short", "-q", "HEAD")
-        command.on_returncode(1, raises=GitClient.DetachedHeadError)
-
-        output = command.capture()
-        return output.rstrip()
-
-    """
-    Updates the indicated remote in the working copy's branch. Doesn't actually
-    affect the checked-out code; this is more like a fetch than a pull.
-    """
-    def update_remote(self, remote):
-        # XXX how can I quiet this?
-        command = self.git("remote", "update", remote)
-        command.on_returncode(1, raises=GitClient.NoRemoteError)
-
-        command.run()
-    
-    """
-    Returns True if the working copy and the (local cached copy of the) remote
-    are on the same commit and there are no staged changes, False otherwise.
-    """
-    def is_up_to_date(self, branch, remote):
-        command = self.git("diff", "--staged", "--quiet", remote + "/" + branch)
-        
-        command.on_returncode(0, returns=True)
-        command.on_returncode(1, returns=False)
-        
-        return command.run()
-    
-    """
-    Adds the specified file (or contents of the specified directory) to the
-    working copy's index.
-    """
-    def stage_file(self, path):
-        self.git("add", path).run(dry_run=self.dry_run)
-    
-    """
-    Commits the working copy's staged changes with the indicated message.
-    """
-    def commit(self, message):
-        self.git("commit", "-m", message).run(dry_run=self.dry_run)
-    
-    """
-    Pushes recent commits to the indicated remote.
-    """
-    def push_to_remote(self, remote):
-        self.git("push", remote).run(dry_run=self.dry_run)
-    
-    class Error(Command.ProcessError):
-        pass
-    
-    class NoRepositoryError(Error):
-        def __str__(self):
-            return "The current directory does not appear to be in a git repository."
-    
-    class NoRemoteError(Error):
-        def __str__(self):
-            return "The git repository does not appear to have a remote named '" + self.process.command.arguments[-1] + "'. (Use --heroku-remote to set a different one.)"
-    
-    class DetachedHeadError(Error):
-        def __str__(self):
-            return "The git repository appears to have a detached HEAD, so there is no branch checked out."
 
 class Command(object):
     def __init__(self, *arguments):
@@ -370,3 +290,83 @@ class Command(object):
     class UnhandledProcessError(ProcessError):
         def __str__(self):
             return "The command " + str(self.process.command) + " failed with error code " + self.returncode + "."
+        
+class GitClient:
+    def __init__(self, dry_run=False):
+        self.dry_run = dry_run
+    
+    """
+    Builds a git command with the provided arguments.
+    """
+    def git(self, *args):
+        command = Command('git', *args).resudoed()
+        command.on_returncode(128, raises=GitClient.NoRepositoryError)
+        return command
+
+    """
+    Determines the branch that the working copy in the current working directory
+    has checked out.
+    """
+    def checked_out_branch(self):
+        command = self.git("symbolic-ref", "--short", "-q", "HEAD")
+        command.on_returncode(1, raises=GitClient.DetachedHeadError)
+
+        output = command.capture()
+        return output.rstrip()
+    
+    """
+    Updates the indicated remote in the working copy's branch. Doesn't actually
+    affect the checked-out code; this is more like a fetch than a pull.
+    """
+    def update_remote(self, remote):
+        # XXX how can I quiet this?
+        command = self.git("remote", "update", remote)
+        command.on_returncode(1, raises=GitClient.NoRemoteError)
+        
+        command.run()
+    
+    """
+    Returns True if the working copy and the (local cached copy of the) remote
+    are on the same commit and there are no staged changes, False otherwise.
+    """
+    def is_up_to_date(self, branch, remote):
+        command = self.git("diff", "--staged", "--quiet", remote + "/" + branch)
+        
+        command.on_returncode(0, returns=True)
+        command.on_returncode(1, returns=False)
+        
+        return command.run()
+    
+    """
+    Adds the specified file (or contents of the specified directory) to the
+    working copy's index.
+    """
+    def stage_file(self, path):
+        self.git("add", path).run(dry_run=self.dry_run)
+    
+    """
+    Commits the working copy's staged changes with the indicated message.
+    """
+    def commit(self, message):
+        self.git("commit", "-m", message).run(dry_run=self.dry_run)
+    
+    """
+    Pushes recent commits to the indicated remote.
+    """
+    def push_to_remote(self, remote):
+        self.git("push", remote).run(dry_run=self.dry_run)
+    
+    class Error(Command.ProcessError):
+        pass
+    
+    class NoRepositoryError(Error):
+        def __str__(self):
+            return "The current directory does not appear to be in a git repository."
+    
+    class NoRemoteError(Error):
+        def __str__(self):
+            return "The git repository does not appear to have a remote named '" + self.process.command.arguments[-1] + "'. (Use --heroku-remote to set a different one.)"
+    
+    class DetachedHeadError(Error):
+        def __str__(self):
+            return "The git repository appears to have a detached HEAD, so there is no branch checked out."
