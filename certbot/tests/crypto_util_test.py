8222211f5c2dae5ea3d99a7cf071e3c0ab32aacf
osirisinferi <github@flut.demon.nl>
2016-04-23 14:44:56 +0200

    Missed an `letsencrypt` somewhere

--- certbot/tests/crypto_util_test.py
+++ certbot/tests/crypto_util_test.py
@@ -143,10 +143,10 @@ class MakeKeyRSATest(unittest.TestCase):  # pylint: disable=too-few-public-metho
 
 
 class MakeKeyECDSATest(unittest.TestCase):
-    """Tests for letsencrypt.crypto_util.make_key_ecdsa."""
+    """Tests for certbot.crypto_util.make_key_ecdsa."""
 
     def test_it(self):
-        from letsencrypt.crypto_util import make_key_ecdsa
+        from certbot.crypto_util import make_key_ecdsa
         from cryptography.hazmat.backends import default_backend
         from cryptography.hazmat.primitives.serialization import load_pem_private_key
         from cryptography.hazmat.primitives.asymmetric import ec
