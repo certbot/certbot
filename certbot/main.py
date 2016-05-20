9446f6bed25014ebcce2130c14982494e13b79f8
Amjad Mashaal <me@amjad.io>
2016-05-20 21:45:49 +0200

    Fixing question in non-interactive mode

--- certbot/main.py
+++ certbot/main.py
@@ -123,7 +123,9 @@ def _handle_subset_cert_request(config, domains, cert):
              existing,
              ", ".join(domains),
              br=os.linesep)
-    if config.expand or config.renew_by_default or zope.component.getUtility(
+    if config.noninteractive_mode:
+        return "newcert", None
+    elif config.expand or config.renew_by_default or zope.component.getUtility(
             interfaces.IDisplay).yesno(question, "Expand", "Cancel",
                                        cli_flag="--expand"):
         return "renew", cert
