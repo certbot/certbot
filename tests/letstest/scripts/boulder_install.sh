c01e2c259af0e200222e66bdf27434f3dba47613
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-05-24 15:38:03 -0700

    Check out Boulder master instead of branch.

--- tests/letstest/scripts/boulder_install.sh
+++ tests/letstest/scripts/boulder_install.sh
@@ -3,7 +3,7 @@
 # >>>> only tested on Ubuntu 14.04LTS <<<<
 
 # Check out special branch until latest docker changes land in Boulder master.
-git clone -b rev-rev https://github.com/letsencrypt/boulder $BOULDERPATH
+git clone https://github.com/letsencrypt/boulder $BOULDERPATH
 cd $BOULDERPATH
 sed -i 's/FAKE_DNS: .*/FAKE_DNS: 172.17.42.1/' docker-compose.yml
 docker-compose up -d
