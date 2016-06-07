1c363716a086ec4dfb3b0c3143e915a75efd106f
Brad Warren <bmw@eff.org>
2016-06-07 16:33:04 -0700

    Wrap mageia bootstrap script in bash function

--- letsencrypt-auto-source/pieces/bootstrappers/mageia_common.sh
+++ letsencrypt-auto-source/pieces/bootstrappers/mageia_common.sh
@@ -1,24 +1,23 @@
-#!/bin/sh
+BootstrapMageiaCommon() {
+    if ! $SUDO urpmi --force  \
+           python \
+           libpython-devel \
+           python-virtualenv
+    then
+      echo "Could not install Python dependencies. Aborting bootstrap!"
+      exit 1
+    fi
 
-# Tested on mageia 5 x86_64
-if ! urpmi --force  \
-       python \
-       libpython-devel \
-       python-virtualenv
-then
-  echo "Could not install Python dependencies. Aborting bootstrap!"
-  exit 1
-fi
-
-if ! urpmi --force \
-       git \
-       gcc \
-       cdialog \
-       python-augeas \
-       libopenssl-devel \
-       libffi-devel \
-       rootcerts
-then
-    echo "Could not install additional dependencies. Aborting bootstrap!"
-    exit 1
-fi
+    if ! $SUDO urpmi --force \
+           git \
+           gcc \
+           cdialog \
+           python-augeas \
+           libopenssl-devel \
+           libffi-devel \
+           rootcerts
+    then
+        echo "Could not install additional dependencies. Aborting bootstrap!"
+        exit 1
+    fi
+}
