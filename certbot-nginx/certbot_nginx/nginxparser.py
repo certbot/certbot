0dc4639cbffe023520c18af375d2e909cdab0137
Peter Eckersley <pde@eff.org>
2016-06-24 18:56:30 -0700

    Be more explicit about range policing
    (rather than doing it in some roundabout crazy way)

--- certbot-nginx/certbot_nginx/nginxparser.py
+++ certbot-nginx/certbot_nginx/nginxparser.py
@@ -239,6 +239,8 @@ class UnspacedList(list):
         # Normalize indexes like list[-1] etc, and save the result
         if idx < 0:
             idx = len(self) + idx
+        if not 0 <= idx < len(self):
+            raise IndexError("list index out of range")
         idx0 = idx
         # Count the number of spaces in the spaced list before idx in the unspaced one
         while idx != -1:
