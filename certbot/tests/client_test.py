ceb5207d56758feb72edfd02c7e56c9c2692ca17
Noah Swartz <swartzcr@gmail.com>
2016-08-16 18:06:21 -0700

    lint

--- certbot/tests/client_test.py
+++ certbot/tests/client_test.py
@@ -60,8 +60,7 @@ class RegisterTest(unittest.TestCase):
                 self._call()
 
     @mock.patch("certbot.account.report_new_account")
-    @mock.patch("certbot.client.display_ops.get_email")
-    def test_email_invalid_noninteractive(self, _rep, mock_get_email):
+    def test_email_invalid_noninteractive(self, _rep):
         from acme import messages
         msg = "DNS problem: NXDOMAIN looking up MX for example.com"
         mx_err = messages.Error(detail=msg, typ="urn:acme:error:invalidEmail")
