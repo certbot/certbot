cd5b91e4ae0e323044a7264dcd00e4a70d18615e
Amjad Mashaal <me@amjad.io>
2016-08-27 19:14:42 +0200

    Adding root certbot-auto to modification check

--- tests/modification-check.sh
+++ tests/modification-check.sh
@@ -3,19 +3,33 @@
 temp_dir=`mktemp -d`
 
 # Script should be run from Certbot's root directory
-cp letsencrypt-auto ${temp_dir}/to-be-checked
+cp letsencrypt-auto ${temp_dir}/letsencrypt-to-be-checked
+cp certbot-auto ${temp_dir}/certbot-to-be-checked
+
 cp letsencrypt-auto-source/pieces/fetch.py ${temp_dir}/fetch.py
 cd ${temp_dir}
 
 LATEST_VERSION=`python fetch.py --latest-version`
 python fetch.py --le-auto-script v${LATEST_VERSION}
 
-cmp -s letsencrypt-auto to-be-checked
+cmp -s letsencrypt-auto letsencrypt-to-be-checked
 
 if [ $? != 0 ]; then
 	echo "Root letsencrypt-auto has changed."
 	rm -rf temp_dir
 	exit 1
+else
+	echo "Root letsencrypt-auto is unchanged."
+fi
+
+cmp -s letsencrypt-auto certbot-to-be-checked
+
+if [ $? != 0 ]; then
+	echo "Root certbot-auto has changed."
+	rm -rf temp_dir
+	exit 1
+else
+	echo "Root certbot-auto is unchanged."
 fi
 
 rm -rf temp_dir
