3eaafcecad22913169b67751728a19f3f05957b5
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-09-16 18:45:21 -0700

    Use cli_command to print.

--- certbot/plugins/selection.py
+++ certbot/plugins/selection.py
@@ -261,10 +261,12 @@ def diagnose_configurator_problem(cfg_type, requested, plugins):
                    "your existing configuration.\nThe error was: {1!r}"
                    .format(requested, plugins[requested].problem))
     elif cfg_type == "installer":
+        from certbot.cli import cli_command
         msg = ('Certbot doesn\'t know how to automatically configure the web '
           'server on this system. However, it can still get a certificate for '
-          'you. Please run "certbot[-auto] certonly" to do so. You\'ll need to '
-          'manually configure your web server to use the resulting certificate.')
+          'you. Please run "{0} certonly" to do so. You\'ll need to '
+          'manually configure your web server to use the resulting '
+          'certificate.').format(cli_command)
     else:
         msg = "{0} could not be determined or is not installed".format(cfg_type)
     raise errors.PluginSelectionError(msg)
