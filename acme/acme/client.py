7b967b799ce3d84d547e3080b724a564e0fdbcf6
Blake Griffith <blake.a.griffith@gmail.com>
2016-10-01 22:46:24 -0700

    Add Client.deactivate method.

--- acme/acme/client.py
+++ acme/acme/client.py
@@ -132,11 +132,15 @@ class Client(object):  # pylint: disable=too-many-instance-attributes
 
         """
         update = regr.body if update is None else update
-        updated_regr = self._send_recv_regr(
-            regr, body=messages.UpdateRegistration(**dict(update)))
+        body = messages.UpdateRegistration(**dict(update))
+        updated_regr = self._send_recv_regr(regr, body=body)
         if updated_regr != regr:
             raise errors.UnexpectedUpdate(regr)
         return updated_regr
+
+    def deactivate(self, regr):
+        update = {'status': 'deactivate'}
+        return self.update_registration(regr, update=update)
 
     def query_registration(self, regr):
         """Query server about registration.
