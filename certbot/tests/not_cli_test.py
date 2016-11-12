f30635ba345598fb9f717b8c23e5309df3fa0ec3
Blake Griffith <cowlicks@riseup.net>
2016-11-11 17:24:35 -0800

    refactor porse tests

--- certbot/tests/not_cli_test.py
+++ certbot/tests/not_cli_test.py
@@ -24,66 +24,66 @@ class TestUtil(unittest.TestCase):
 
 class ParseTest(unittest.TestCase):
     '''Test the cli args entrypoint'''
-    def _get_argument_parser(self):
-        plugins = disco.PluginsRegistry.find_all()
-        return functools.partial(cli.prepare_and_parse_args, plugins)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.plugins = disco.PluginsRegistry.find_all()
+        cls.parse = functools.partial(cli.prepare_and_parse_args, cls.plugins)
+
+    def test_help(self):
+        self.assertRaises(SystemExit, self.parse, ['--help'])
+        self.assertRaises(SystemExit, self.parse, ['--help', 'all'])
 
     def test_parse_domains(self):
-        parse = self._get_argument_parser()
-
         short_args = ['-d', 'example.com']
-        namespace = parse(short_args)
+        namespace = self.parse(short_args)
         self.assertEqual(namespace.domains, ['example.com'])
 
         short_args = ['-d', 'trailing.period.com.']
-        namespace = parse(short_args)
+        namespace = self.parse(short_args)
         self.assertEqual(namespace.domains, ['trailing.period.com'])
 
         short_args = ['-d', 'example.com,another.net,third.org,example.com']
-        namespace = parse(short_args)
+        namespace = self.parse(short_args)
         self.assertEqual(namespace.domains, ['example.com', 'another.net',
                                              'third.org'])
 
         long_args = ['--domains', 'example.com']
-        namespace = parse(long_args)
+        namespace = self.parse(long_args)
         self.assertEqual(namespace.domains, ['example.com'])
 
         long_args = ['--domains', 'trailing.period.com.']
-        namespace = parse(long_args)
+        namespace = self.parse(long_args)
         self.assertEqual(namespace.domains, ['trailing.period.com'])
 
         long_args = ['--domains', 'example.com,another.net,example.com']
-        namespace = parse(long_args)
+        namespace = self.parse(long_args)
         self.assertEqual(namespace.domains, ['example.com', 'another.net'])
 
     def test_preferred_challenges(self):
         from acme.challenges import HTTP01, TLSSNI01, DNS01
-        parse = self._get_argument_parser()
 
         short_args = ['--preferred-challenges', 'http, tls-sni-01, dns']
-        namespace = parse(short_args)
+        namespace = self.parse(short_args)
 
         self.assertEqual(namespace.pref_challs, [HTTP01, TLSSNI01, DNS01])
 
         short_args = ['--preferred-challenges', 'jumping-over-the-moon']
-        self.assertRaises(argparse.ArgumentTypeError, parse, short_args)
+        self.assertRaises(argparse.ArgumentTypeError, self.parse, short_args)
 
     def test_server_flag(self):
-        parse = self._get_argument_parser()
-        namespace = parse('--server example.com'.split())
+        namespace = self.parse('--server example.com'.split())
         self.assertEqual(namespace.server, 'example.com')
 
     def test_must_staple_flag(self):
-        parse = self._get_argument_parser()
         short_args = ['--must-staple']
-        namespace = parse(short_args)
+        namespace = self.parse(short_args)
         self.assertTrue(namespace.must_staple)
         self.assertTrue(namespace.staple)
 
     def _check_server_conflict_message(self, parser_args, conflicting_args):
-        parse = self._get_argument_parser()
         try:
-            parse(parser_args)
+            self.parse(parser_args)
             self.fail(  # pragma: no cover
                 "The following flags didn't conflict with "
                 '--server: {0}'.format(', '.join(conflicting_args)))
@@ -93,9 +93,8 @@ class ParseTest(unittest.TestCase):
                 self.assertTrue(arg in str(error))
 
     def test_staging_flag(self):
-        parse = self._get_argument_parser()
         short_args = ['--staging']
-        namespace = parse(short_args)
+        namespace = self.parse(short_args)
         self.assertTrue(namespace.staging)
         self.assertEqual(namespace.server, constants.STAGING_URI)
 
@@ -116,26 +115,25 @@ class ParseTest(unittest.TestCase):
             self.assertFalse(namespace.register_unsafely_without_email)
 
     def test_dry_run_flag(self):
-        parse = self._get_argument_parser()
         config_dir = tempfile.mkdtemp()
         short_args = '--dry-run --config-dir {0}'.format(config_dir).split()
-        self.assertRaises(errors.Error, parse, short_args)
+        self.assertRaises(errors.Error, self.parse, short_args)
 
         self._assert_dry_run_flag_worked(
-            parse(short_args + ['auth']), False)
+            self.parse(short_args + ['auth']), False)
         self._assert_dry_run_flag_worked(
-            parse(short_args + ['certonly']), False)
+            self.parse(short_args + ['certonly']), False)
         self._assert_dry_run_flag_worked(
-            parse(short_args + ['renew']), False)
+            self.parse(short_args + ['renew']), False)
 
         account_dir = os.path.join(config_dir, constants.ACCOUNTS_DIR)
         os.mkdir(account_dir)
         os.mkdir(os.path.join(account_dir, 'fake_account_dir'))
 
-        self._assert_dry_run_flag_worked(parse(short_args + ['auth']), True)
-        self._assert_dry_run_flag_worked(parse(short_args + ['renew']), True)
+        self._assert_dry_run_flag_worked(self.parse(short_args + ['auth']), True)
+        self._assert_dry_run_flag_worked(self.parse(short_args + ['renew']), True)
         short_args += ['certonly']
-        self._assert_dry_run_flag_worked(parse(short_args), True)
+        self._assert_dry_run_flag_worked(self.parse(short_args), True)
 
         short_args += '--server example.com'.split()
         conflicts = ['--dry-run']
