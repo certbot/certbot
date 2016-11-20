8ee63f9f81471f0841f17c803061819c01e22044
Nicholas Fong <dev@nicholas-fong.com>
2016-11-20 00:46:43 -0500

    Add handling of $QUIET to Arch and Open Suse

--- letsencrypt-auto-source/pieces/bootstrappers/suse_common.sh
+++ letsencrypt-auto-source/pieces/bootstrappers/suse_common.sh
@@ -6,7 +6,11 @@ BootstrapSuseCommon() {
     install_flags="-l"
   fi
 
-  $SUDO zypper $zypper_flags in $install_flags \
+  if [ "$QUIET" == 1 ]; then
+      QUIET_FLAG='-qq'
+  fi
+
+  $SUDO zypper $QUIET_FLAG $zypper_flags in $install_flags \
     python \
     python-devel \
     python-virtualenv \
