6195bd4bdbdc295ed464b01391e4af5e5bc1a174
Peter Eckersley <pde@eff.org>
2016-11-30 20:44:21 -0800

    Allow nosetests to figure out how many cores to use

--- tox.cover.sh
+++ tox.cover.sh
@@ -17,19 +17,19 @@ fi
 cover () {
   if [ "$1" = "certbot" ]; then
     min=98
-    proc="--processes=2 --process-timeout=100"
+    proc="--processes=-1 --process-timeout=100"
   elif [ "$1" = "acme" ]; then
     min=100
-    proc="--processes=2"
+    proc="--processes=-1"
   elif [ "$1" = "certbot_apache" ]; then
     min=100
-    proc="--processes=2 --process-timeout=80"
+    proc="--processes=-1 --process-timeout=80"
   elif [ "$1" = "certbot_nginx" ]; then
     min=97
-    proc="--processes=2 --process-timeout=60"
+    proc="--processes=-1 --process-timeout=60"
   elif [ "$1" = "letshelp_certbot" ]; then
     min=100
-    proc="--processes=2"
+    proc="--processes=-1"
   else
     echo "Unrecognized package: $1"
     exit 1
