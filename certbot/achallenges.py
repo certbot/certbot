54b01cea30167065e3682834a71144b81e96c07f
Brad Warren <bmw@eff.org>
2016-12-09 10:40:40 -0800

    make achall more helpful

--- certbot/achallenges.py
+++ certbot/achallenges.py
@@ -49,8 +49,32 @@ class KeyAuthorizationAnnotatedChallenge(AnnotatedChallenge):
     """Client annotated `KeyAuthorizationChallenge` challenge."""
     __slots__ = ('challb', 'domain', 'account_key')
 
+    def response(self, *args, **kwargs):
+        """Generate response to the challenge.
+
+        :returns: Response (initialized `response_cls`) to the challenge.
+        :rtype: acme.challenges.KeyAuthorizationChallengeResponse
+
+        """
+        return self.chalb.chall.response(self.account_key, *args, **kwargs)
+
+    def validation(self, *args, **kwargs):
+        """Generate validation for the challenge.
+
+        :returns: Challenge-specific validation.
+
+        """
+        return self.chalb.chall.validation(self.account_key, *args, **kwargs)
+
     def response_and_validation(self, *args, **kwargs):
-        """Generate response and validation."""
+        """Generate response and validation.
+
+        Convenience function that return results of `response` and
+        `validation`.
+
+        :rtype: tuple
+
+        """
         return self.challb.chall.response_and_validation(
             self.account_key, *args, **kwargs)
 
