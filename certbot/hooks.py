dcf400ec7de20a6fe0a6e2be69e08e81b4f622e3
Brad Warren <bmw@eff.org>
2016-12-13 10:21:47 -0800

    Add comment about universal_newlines

--- certbot/hooks.py
+++ certbot/hooks.py
@@ -84,6 +84,8 @@ def execute(shell_cmd):
 
     :returns: `tuple` (`str` stderr, `str` stdout)"""
 
+    # universal_newlines causes Popen.communicate()
+    # to return str objects instead of bytes in Python 3
     cmd = Popen(shell_cmd, shell=True, stdout=PIPE,
                 stderr=PIPE, universal_newlines=True)
     out, err = cmd.communicate()
