052df574106a1730ba7d364804d3dc6b6f10b8f6
Osiris Inferi <github@flut.demon.nl>
2017-01-05 15:34:15 +0100

    Use `format()` for message formatting

--- certbot-apache/certbot_apache/augeas_configurator.py
+++ certbot-apache/certbot_apache/augeas_configurator.py
@@ -68,11 +68,12 @@ class AugeasConfigurator(common.Plugin):
             # As aug.get may return null
             if lens_path and lens in lens_path:
                 msg = (
-                    "There has been an error in parsing the file %s on line %d: %s",
+                    "There has been an error in parsing the file {} on line {}: "
+                    "{}".format(
                     # Strip off /augeas/files and /error
                     path[13:len(path) - 6],
                     self.aug.get(path + "/line"),
-                    self.aug.get(path + "/message"))
+                    self.aug.get(path + "/message")))
                 raise errors.PluginError(msg)
 
     # TODO: Cleanup this function
