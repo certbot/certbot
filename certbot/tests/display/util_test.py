19ec38381a8347e8520c32251a98f41750be44fe
Brad Warren <bmw@eff.org>
2017-01-10 14:09:32 -0800

    Add test_input_assertion_fail2()

--- certbot/tests/display/util_test.py
+++ certbot/tests/display/util_test.py
@@ -98,6 +98,11 @@ class FileOutputDisplayTest(unittest.TestCase):
         self.assertRaises(Exception, self._force_noninteractive,
                           self.displayer.input, "message", cli_flag="--flag")
 
+    @mock.patch("certbot.display.util.assert_valid_call")
+    def test_input_assertion_fail2(self, mock_assert_valid_call):
+        self.assertRaises(errors.Error, self._force_noninteractive,
+                          self.displayer.input, "message", cli_flag="--flag")
+
     def test_yesno(self):
         with mock.patch("six.moves.input", return_value="Yes"):
             self.assertTrue(self.displayer.yesno(
