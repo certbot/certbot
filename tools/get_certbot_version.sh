39a6f00367d6d88ed068c32979ad2c1a2f99181f
Brad Warren <bmw@eff.org>
2017-01-19 16:38:17 -0800

    use readlink not realpath

--- tools/get_certbot_version.sh
+++ tools/get_certbot_version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-INIT_PATH=$(realpath $(dirname $0)"/../certbot/__init__.py")
+INIT_PATH=$(readlink -f $(dirname $0)"/../certbot/__init__.py")
 grep "__version__" $INIT_PATH | cut -d\' -f2
