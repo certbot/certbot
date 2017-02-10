2440657017e052d170479a7cf94aadfa312ecd94
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-02-10 14:03:00 -0800

    Put all path logic in boulder-fetch.sh

--- tests/boulder-fetch.sh
+++ tests/boulder-fetch.sh
@@ -2,8 +2,12 @@
 # Download and run Boulder instance for integration testing
 set -xe
 
-# Check out special branch until latest docker changes land in Boulder master.
+# Clone Boulder into a GOPATH-style directory structure even if Go isn't
+# installed, because Boulder's docker-compose.yml file wll look for it there.
+GOPATH=${GOPATH:-$HOME/gopath}
+BOULDERPATH=${BOULDERPATH:-$GOPATH/src/github.com/letsencrypt/boulder}
 git clone --depth=1 https://github.com/letsencrypt/boulder $BOULDERPATH
+
 cd $BOULDERPATH
 FAKE_DNS=$(ifconfig docker0 | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}')
 sed -i "s/FAKE_DNS: .*/FAKE_DNS: $FAKE_DNS/" docker-compose.yml
