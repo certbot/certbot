47f95cd040ce85f77198dd4da6cef8716a6edb32
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-02-13 18:39:55 -0800

    Add export to boulder-fetch.sh.

--- tests/boulder-fetch.sh
+++ tests/boulder-fetch.sh
@@ -4,7 +4,7 @@ set -xe
 
 # Clone Boulder into a GOPATH-style directory structure even if Go isn't
 # installed, because Boulder's docker-compose.yml file wll look for it there.
-GOPATH=${GOPATH:-$HOME/gopath}
+export GOPATH=${GOPATH:-$HOME/gopath}
 BOULDERPATH=${BOULDERPATH:-$GOPATH/src/github.com/letsencrypt/boulder}
 git clone --depth=1 https://github.com/letsencrypt/boulder $BOULDERPATH
 
