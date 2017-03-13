88fd1cee80e5aeefea0e3a522165b5e2b9954672
Erica Portnoy <ebportnoy@gmail.com>
2017-03-13 15:13:24 -0700

    remove cruft

--- certbot-nginx/certbot_nginx/configurator.py
+++ certbot-nginx/certbot_nginx/configurator.py
@@ -835,27 +835,15 @@ def nginx_restart(nginx_ctl, nginx_conf="/etc/nginx.conf"):
 
         if proc.returncode != 0:
             # Maybe Nginx isn't running
+            # Write to temporary files instead of piping because of communication issues on Arch
+            # https://github.com/certbot/certbot/issues/4324
             with tempfile.TemporaryFile() as out, tempfile.TemporaryFile() as err:
-                nginx_proc = subprocess.Popen([nginx_ctl, "-c", nginx_conf],
-                                              stdout=out,
-                                              stderr=err)
+                nginx_proc = subprocess.Popen([nginx_ctl, "-c", nginx_conf], stdout=out, stderr=err)
                 nginx_proc.communicate()
-                # stdout, stderr = out.read(), err.read()
                 if nginx_proc.returncode != 0:
                     # Enter recovery routine...
                     raise errors.MisconfigurationError(
                         "nginx restart failed:\n%s\n%s" % (out.read(), err.read()))
-            # Minor hack: don't call communicate(), because it hangs on Arch
-            # if nginx_proc.poll() == 0
-            # https://github.com/certbot/certbot/issues/4324
-            # and we can't use wait() here, because that is known to block with piped output
-            # while nginx_proc.poll() is None:
-            #     time.sleep(.1)
-            # if nginx_proc.poll() != 0:
-            #     # Enter recovery routine...
-            #     raise errors.MisconfigurationError(
-            #         "nginx restart failed:\n%s\n%s" % (nginx_proc.stdout.read(),
-            #             nginx_proc.stderr.read()))
 
     except (OSError, ValueError):
         raise errors.MisconfigurationError("nginx restart failed")
