5aa0dd909517efb915660fee3e13288b0411e239
Brad Warren <bmw@eff.org>
2017-03-29 14:43:08 -0700

    Copy except_hook to log

--- certbot/log.py
+++ certbot/log.py
@@ -1,8 +1,17 @@
 """Logging utilities for Certbot."""
+from __future__ import print_function
 import logging
+import os
 import sys
+import traceback
 
+from acme import messages
+
+from certbot import cli
+from certbot import errors
 from certbot import util
+
+logger = logging.getLogger(__name__)
 
 
 class ColoredStreamHandler(logging.StreamHandler):
@@ -43,3 +52,56 @@ class ColoredStreamHandler(logging.StreamHandler):
             return ''.join((util.ANSI_SGR_RED, out, util.ANSI_SGR_RESET))
         else:
             return out
+
+
+def except_hook(exc_type, exc_value, trace, config):
+    """Logs exceptions and reports them to the user.
+
+    Config is used to determine how to display exceptions to the user. In
+    general, if config.debug is True, then the full exception and traceback is
+    shown to the user, otherwise it is suppressed. If config itself is None,
+    then the traceback and exception is attempted to be written to a logfile.
+    If this is successful, the traceback is suppressed, otherwise it is shown
+    to the user. sys.exit is always called with a nonzero status.
+
+    """
+    tb_str = "".join(traceback.format_exception(exc_type, exc_value, trace))
+    logger.debug("Exiting abnormally:%s%s", os.linesep, tb_str)
+
+    if issubclass(exc_type, Exception) and (config is None or not config.debug):
+        if config is None:
+            logfile = "certbot.log"
+            try:
+                with open(logfile, "w") as logfd:
+                    traceback.print_exception(
+                        exc_type, exc_value, trace, file=logfd)
+                assert "--debug" not in sys.argv  # config is None if this explodes
+            except:  # pylint: disable=bare-except
+                sys.exit(tb_str)
+            if "--debug" in sys.argv:
+                sys.exit(tb_str)
+
+        if issubclass(exc_type, errors.Error):
+            sys.exit(exc_value)
+        else:
+            # Here we're passing a client or ACME error out to the client at the shell
+            # Tell the user a bit about what happened, without overwhelming
+            # them with a full traceback
+            err = traceback.format_exception_only(exc_type, exc_value)[0]
+            # Typical error from the ACME module:
+            # acme.messages.Error: urn:ietf:params:acme:error:malformed :: The
+            # request message was malformed :: Error creating new registration
+            # :: Validation of contact mailto:none@longrandomstring.biz failed:
+            # Server failure at resolver
+            if (messages.is_acme_error(err) and ":: " in err and
+                 config.verbose_count <= cli.flag_default("verbose_count")):
+                # prune ACME error code, we have a human description
+                _code, _sep, err = err.partition(":: ")
+            msg = "An unexpected error occurred:\n" + err + "Please see the "
+            if config is None:
+                msg += "logfile '{0}' for more details.".format(logfile)
+            else:
+                msg += "logfiles in {0} for more details.".format(config.logs_dir)
+            sys.exit(msg)
+    else:
+        sys.exit(tb_str)
--- certbot/tests/log_test.py
+++ certbot/tests/log_test.py
@@ -1,9 +1,14 @@
 """Tests for certbot.log."""
 import logging
+import traceback
 import unittest
 
+import mock
 import six
 
+from acme import messages
+
+from certbot import errors
 from certbot import util
 
 
@@ -35,6 +40,62 @@ class ColoredStreamHandlerTest(unittest.TestCase):
                          '{0}{1}{2}\n'.format(util.ANSI_SGR_RED,
                                               msg,
                                               util.ANSI_SGR_RESET))
+
+
+class ExceptHookTest(unittest.TestCase):
+    """Test log.except_hook"""
+    @classmethod
+    def _call(cls, *args, **kwargs):
+        from certbot.log import except_hook
+        return except_hook(*args, **kwargs)
+
+    @mock.patch('certbot.log.sys')
+    def test_handle_exception(self, mock_sys):
+        config = mock.MagicMock()
+        mock_open = mock.mock_open()
+
+        with mock.patch('certbot.log.open', mock_open, create=True):
+            exception = Exception('detail')
+            config.verbose_count = 1
+            self._call(
+                Exception, exc_value=exception, trace=None, config=None)
+            mock_open().write.assert_any_call(''.join(
+                traceback.format_exception_only(Exception, exception)))
+            error_msg = mock_sys.exit.call_args_list[0][0][0]
+            self.assertTrue('unexpected error' in error_msg)
+
+        with mock.patch('certbot.log.open', mock_open, create=True):
+            mock_open.side_effect = [KeyboardInterrupt]
+            error = errors.Error('detail')
+            self._call(
+                errors.Error, exc_value=error, trace=None, config=None)
+            # assert_any_call used because sys.exit doesn't exit in cli.py
+            mock_sys.exit.assert_any_call(''.join(
+                traceback.format_exception_only(errors.Error, error)))
+
+        bad_typ = messages.ERROR_PREFIX + 'triffid'
+        exception = messages.Error(detail='alpha', typ=bad_typ, title='beta')
+        config = mock.MagicMock(debug=False, verbose_count=-3)
+        self._call(
+            messages.Error, exc_value=exception, trace=None, config=config)
+        error_msg = mock_sys.exit.call_args_list[-1][0][0]
+        self.assertTrue('unexpected error' in error_msg)
+        self.assertTrue('acme:error' not in error_msg)
+        self.assertTrue('alpha' in error_msg)
+        self.assertTrue('beta' in error_msg)
+        config = mock.MagicMock(debug=False, verbose_count=1)
+        self._call(
+            messages.Error, exc_value=exception, trace=None, config=config)
+        error_msg = mock_sys.exit.call_args_list[-1][0][0]
+        self.assertTrue('unexpected error' in error_msg)
+        self.assertTrue('acme:error' in error_msg)
+        self.assertTrue('alpha' in error_msg)
+
+        interrupt = KeyboardInterrupt('detail')
+        self._call(
+            KeyboardInterrupt, exc_value=interrupt, trace=None, config=None)
+        mock_sys.exit.assert_called_with(''.join(
+            traceback.format_exception_only(KeyboardInterrupt, interrupt)))
 
 
 if __name__ == "__main__":
