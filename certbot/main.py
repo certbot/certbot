72b6179e0eb2519b04d0c58dea1a1005d8ac5889
Peter Eckersley <pde@eff.org>
2017-04-28 18:45:42 -0700

    Remove vestigial thingy

--- certbot/main.py
+++ certbot/main.py
@@ -690,7 +690,6 @@ def certonly(config, plugins):
 def renew(config, unused_plugins):
     """Renew previously-obtained certificates."""
     try:
-        config.renewing = "yes"
         renewal.handle_renewal_request(config)
     finally:
         hooks.run_saved_post_hooks()
