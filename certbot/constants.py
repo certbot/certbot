300025b1070bd6f351653f036a8416db5fd41ad4
Peter Eckersley <pde@eff.org>
2017-05-11 15:41:36 -0700

    lint

--- certbot/constants.py
+++ certbot/constants.py
@@ -38,8 +38,8 @@ CLI_DEFAULTS = dict(
 STAGING_URI = "https://acme-staging.api.letsencrypt.org/directory"
 
 # for use with --certbot-dir
-WORK_SUBDIR=".internal"
-LOGS_SUBDIR="logs"
+WORK_SUBDIR = ".internal"
+LOGS_SUBDIR = "logs"
 
 # The set of reasons for revoking a certificate is defined in RFC 5280 in
 # section 5.3.1. The reasons that users are allowed to submit are restricted to
