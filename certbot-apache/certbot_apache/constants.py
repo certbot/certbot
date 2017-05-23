21565c08051657355bccb65c8be8b5782b641bd9
Erica Portnoy <ebportnoy@gmail.com>
2017-05-23 16:25:39 -0700

    Mechanism for automatically updating options-ssl-apache.conf file
    * add file update mechanism + tests to apache
    
    * update with actual hashes, and update apache test to match since there aren't previous versions

--- certbot-apache/certbot_apache/constants.py
+++ certbot-apache/certbot_apache/constants.py
@@ -131,6 +131,16 @@ CLI_DEFAULTS = {
 MOD_SSL_CONF_DEST = "options-ssl-apache.conf"
 """Name of the mod_ssl config file as saved in `IConfig.config_dir`."""
 
+
+UPDATED_MOD_SSL_CONF_DIGEST = ".updated-options-ssl-apache-conf-digest.txt"
+"""Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`."""
+
+ALL_SSL_OPTIONS_HASHES = [
+    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
+    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
+]
+"""SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC"""
+
 AUGEAS_LENS_DIR = pkg_resources.resource_filename(
     "certbot_apache", "augeas_lens")
 """Path to the Augeas lens directory"""
