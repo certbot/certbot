215c85d7bed0de619de6921ffc45a595c4e9cc12
Zach Shepherd <shepherdz@vmware.com>
2017-06-07 14:40:55 -0700

    Provide basic Cloudflare documentation

--- certbot-dns-cloudflare/certbot_dns_cloudflare/__init__.py
+++ certbot-dns-cloudflare/certbot_dns_cloudflare/__init__.py
@@ -1,1 +1,17 @@
-"""Cloudflare DNS Authenticator"""
+"""Cloudflare DNS Authenticator
+
+This plugin automates the process of completing a dns-01 challenge
+(`~acme.challenges.DNS01`) using the Cloudflare API.
+
+Use of this plugin requires a configuration file containing Cloudflare API
+credentials, obtained from your Cloudflare
+`account page <https://www.cloudflare.com/a/account/my-account>`_.
+
+Example:
+
+.. code-block:: ini
+
+  dns_cloudflare_email = cloudflare@example.com
+  dns_cloudflare_api_key = 0123456789abcdef0123456789abcdef01234567
+
+"""
