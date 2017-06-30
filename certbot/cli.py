d57e8bfaa39f0b1286f82c8a5d33e2cbf46558fb
Brad Warren <bmw@eff.org>
2017-06-30 09:11:51 -0400

    add --deploy-hook

--- certbot/cli.py
+++ certbot/cli.py
@@ -1095,6 +1095,16 @@ def prepare_and_parse_args(plugins, args, detect_defaults=False):  # pylint: dis
         " space-delimited list of renewed certificate domains (for example,"
         " \"example.com www.example.com\"")
     helpful.add(
+        "renew", "--deploy-hook",
+        help="Command to be run in a shell once for each successfully"
+        " issued certificate. For this command, the shell variable"
+        " $RENEWED_LINEAGE will point to the config live subdirectory"
+        ' (for example, "/etc/letsencrypt/live/example.com") containing'
+        " the new certificates and keys; the shell variable"
+        " $RENEWED_DOMAINS will contain a space-delimited list of"
+        ' renewed certificate domains (for example, "example.com'
+        ' www.example.com"')
+    helpful.add(
         "renew", "--disable-hook-validation",
         action='store_false', dest='validate_hooks', default=True,
         help="Ordinarily the commands specified for"
