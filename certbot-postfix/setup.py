49cdfcec06564a9c56b1f592225b8600d0417cda
Brad Warren <bmw@eff.org>
2017-08-04 10:20:36 -0700

    add six dependency

--- certbot-postfix/setup.py
+++ certbot-postfix/setup.py
@@ -12,6 +12,7 @@ install_requires = [
     # For pkg_resources. >=1.0 so pip resolves it to a version cryptography
     # will tolerate; see #2599:
     'setuptools>=1.0',
+    'six',
     'zope.interface',
 ]
 
