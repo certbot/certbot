2e7ec00e8c5a605c272a72a702f34803463bc5df
Bob Strecansky <bstrecansky@rsglab.com>
2017-08-07 08:55:43 -0400

    [#4535] - Unwrap max retries exceeded errors

--- acme/acme/client_test.py
+++ acme/acme/client_test.py
@@ -634,7 +634,7 @@ class ClientNetworkTest(unittest.TestCase):
 
         # Requests Library Exceptions
         except requests.exceptions.ConnectionError as z: #pragma: no cover
-            self.assertEqual("('Connection aborted.', error(111, 'Connection Refused'))", str(z))
+            self.assertEqual("('Connection aborted.', error(111, 'Connection refused'))", str(z))
 
 class ClientNetworkWithMockedResponseTest(unittest.TestCase):
     """Tests for acme.client.ClientNetwork which mock out response."""
