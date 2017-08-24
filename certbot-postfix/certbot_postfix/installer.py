baf0d3343aceba3568dc06898c654c0322bd428a
Brad Warren <bmw@eff.org>
2017-08-24 15:13:02 -0700

    Remove postfix version notes

--- certbot-postfix/certbot_postfix/installer.py
+++ certbot-postfix/certbot_postfix/installer.py
@@ -89,49 +89,6 @@ class Installer(plugins_common.Plugin):
         """
         if self._get_version() < (2, 11, 0):
             raise errors.NotSupportedError('Postfix version is too old')
-        # Postfix has changed support for TLS features, supported protocol versions
-        # KEX methods, ciphers et cetera over the years. We sort out version dependend
-        # differences here and pass them onto other configuration functions.
-        # see:
-        #  http://www.postfix.org/TLS_README.html
-        #  http://www.postfix.org/FORWARD_SECRECY_README.html
-
-        # Postfix == 2.2:
-        # - TLS support introduced via 3rd party patch, see:
-        #   http://www.postfix.org/TLS_LEGACY_README.html
-
-        # Postfix => 2.2:
-        # - built-in TLS support added
-        # - Support for PFS introduced
-        # - Support for (E)DHE params >= 1024bit (need to be generated), default 1k
-
-        # Postfix => 2.5:
-        # - Syntax to specify mandatory protocol version changes:
-        #   *  < 2.5: `smtpd_tls_mandatory_protocols = TLSv1`
-        #   * => 2.5: `smtpd_tls_mandatory_protocols = !SSLv2, !SSLv3`
-        # - Certificate fingerprint verification added
-
-        # Postfix => 2.6:
-        # - Support for ECDHE NIST P-256 curve (enable `smtpd_tls_eecdh_grade = strong`)
-        # - Support for configurable cipher-suites and protocol versions added, pre-2.6
-        #   releases always set EXPORT, options: `smtp_tls_ciphers` and `smtp_tls_protocols`
-        # - `smtp_tls_eccert_file` and `smtp_tls_eckey_file` config. options added
-
-        # Postfix => 2.8:
-        # - Override Client suite preference w. `tls_preempt_cipherlist = yes`
-        # - Elliptic curve crypto. support enabled by default
-
-        # Postfix => 2.9:
-        # - Public key fingerprint support added
-        # - `permit_tls_clientcerts`, `permit_tls_all_clientcerts` and
-        #   `check_ccert_access` config. options added
-
-        # Postfix <= 2.9.5:
-        # - BUG: Public key fingerprint is computed incorrectly
-
-        # Postfix => 3.1:
-        # - Built-in support for TLS management and DANE added, see:
-        #   http://www.postfix.org/postfix-tls.1.html
 
     def _lock_config_dir(self):
         """Stop two Postfix plugins from modifying the config at once.
