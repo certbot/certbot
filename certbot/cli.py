b4a0324b06763e557ae32362dfe8947cc9f5d0b3
Noah Swartz <swartzcr@gmail.com>
2017-09-20 13:40:47 -0700

    fix nit

--- certbot/cli.py
+++ certbot/cli.py
@@ -892,14 +892,14 @@ def prepare_and_parse_args(plugins, args, detect_defaults=False):  # pylint: dis
         help="Domain names to apply. For multiple domains you can use "
              "multiple -d flags or enter a comma separated list of domains "
              "as a parameter. The first domain provided will be the "
-             "subject CN of the certificate, and all domains will be"
+             "subject CN of the certificate, and all domains will be "
              "Subject Alternative Names on the certificate. "
              "The first domain will also be used in "
              "some software user interfaces and as the file paths for the "
              "certificate and related material unless otherwise "
              "specified or you already have a certificate with the same "
-             "name. In that case it will append a number like 0001 "
-             "to the file path name. (default: Ask)")
+             "name. In the case of a name collision it will append a number "
+             "like 0001 to the file path name. (default: Ask)")
     helpful.add(
         [None, "run", "certonly", "manage", "delete", "certificates"],
         "--cert-name", dest="certname",
