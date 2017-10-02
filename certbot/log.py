4742458d32c3321cb3ac3bdf81e21227fd8c1729
rsiddharth <s@ricketyspace.net>
2017-10-02 21:28:34 +0000

    certbot: Stop using print in log module.
    * Update certbot.log.post_arg_parse_except_hook function.
    * Update certbot.tests.log_test._test_common method.
    
    See discussion #3720.

--- certbot/log.py
+++ certbot/log.py
@@ -359,11 +359,11 @@ def post_arg_parse_except_hook(exc_type, exc_value, trace, debug, log_path):
         logger.debug('Exiting abnormally:', exc_info=exc_info)
         if issubclass(exc_type, errors.Error):
             sys.exit(exc_value)
-        print('An unexpected error occurred:', file=sys.stderr)
+        logger.error('An unexpected error occurred:')
         if messages.is_acme_error(exc_value):
             # Remove the ACME error prefix from the exception
             _, _, exc_str = str(exc_value).partition(':: ')
-            print(exc_str, file=sys.stderr)
+            logger.error(exc_str)
         else:
             traceback.print_exception(exc_type, exc_value, None)
     exit_with_log_path(log_path)
