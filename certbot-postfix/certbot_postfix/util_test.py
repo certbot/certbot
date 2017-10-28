d18dd1fc18858f3f0631346fe921b1f4dd5b8def
Brad Warren <bmw@eff.org>
2017-10-27 17:08:39 -0700

    test check_all_output

--- certbot-postfix/certbot_postfix/util_test.py
+++ certbot-postfix/certbot_postfix/util_test.py
@@ -6,6 +6,49 @@ import unittest
 import mock
 
 
+class CheckAllOutputTest(unittest.TestCase):
+    """Tests for certbot_postfix.util.check_all_output."""
+
+    @classmethod
+    def _call(cls, *args, **kwargs):
+        from certbot_postfix.util import check_all_output
+        return check_all_output(*args, **kwargs)
+
+    @mock.patch('certbot_postfix.util.logger')
+    @mock.patch('certbot_postfix.util.subprocess.Popen')
+    def test_command_error(self, mock_popen, mock_logger):
+        command = 'foo'
+        retcode = 42
+        output = 'bar'
+        err = 'baz'
+
+        mock_popen().communicate.return_value = (output, err)
+        mock_popen().poll.return_value = 42
+
+        self.assertRaises(subprocess.CalledProcessError, self._call, command)
+        log_args = mock_logger.debug.call_args[0]
+        for value in (command, retcode, output, err,):
+            self.assertTrue(value in log_args)
+
+    @mock.patch('certbot_postfix.util.subprocess.Popen')
+    def test_success(self, mock_popen):
+        command = 'foo'
+        expected = ('bar', '')
+        mock_popen().communicate.return_value = expected
+        mock_popen().poll.return_value = 0
+
+        self.assertEqual(self._call(command), expected)
+
+    def test_stdout_error(self):
+        self.assertRaises(ValueError, self._call, stdout=None)
+
+    def test_stderr_error(self):
+        self.assertRaises(ValueError, self._call, stderr=None)
+
+    def test_universal_newlines_error(self):
+        self.assertRaises(ValueError, self._call, universal_newlines=False)
+
+
 class CheckCallTest(unittest.TestCase):
     """Tests for certbot_postfix.util.check_call."""
 
