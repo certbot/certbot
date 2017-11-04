032a5139472007a1ed2dc6ddf622c90f952e9dc7
Erica Portnoy <ebportnoy@gmail.com>
2017-11-03 18:05:28 -0700

    don't add listen 80 to newly created ssl block

--- certbot-nginx/certbot_nginx/configurator.py
+++ certbot-nginx/certbot_nginx/configurator.py
@@ -487,12 +487,6 @@ class NginxConfigurator(common.Installer):
         :type vhost: :class:`~certbot_nginx.obj.VirtualHost`
 
         """
-        # If the vhost was implicitly listening on the default Nginx port,
-        # have it continue to do so.
-        if len(vhost.addrs) == 0:
-            listen_block = [['\n    ', 'listen', ' ', self.DEFAULT_LISTEN_PORT]]
-            self.parser.add_server_directives(vhost, listen_block, replace=False)
-
         snakeoil_cert, snakeoil_key = self._get_snakeoil_paths()
 
         ssl_block = ([
