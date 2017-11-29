30eba031a664b1aedd05978a89aee8269a6e1cf8
Brad Warren <bmw@eff.org>
2017-11-29 12:33:43 -0800

    tell me more

--- tools/install_and_test.sh
+++ tools/install_and_test.sh
@@ -1,9 +1,11 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 # pip installs the requested packages in editable mode and runs unit tests on
 # them. Each package is installed and tested in the order they are provided
 # before the script moves on to the next package. If CERTBOT_NO_PIN is set not
 # set to 1, packages are installed using certbot-auto's requirements file as
 # constraints.
+
+python -c 'from __future__ import print_function; import pip; import setuptools; import wheel; print(pip.__version__); print(setuptools.__version__); print(wheel.__version__)'
 
 if [ "$CERTBOT_NO_PIN" = 1 ]; then
   pip_install="pip install -e"
