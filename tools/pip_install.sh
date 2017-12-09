61f5ee1c2a6ee10186bce515d47a853c14b6e378
Brad Warren <bmw@eff.org>
2017-12-08 17:42:09 -0800

    Update constraints gen in pip_install

--- tools/pip_install.sh
+++ tools/pip_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 # pip installs packages using pinned package versions. If CERTBOT_OLDEST is set
 # to 1, a combination of tools/oldest_constraints.txt and
 # tools/dev_constraints.txt is used, otherwise, a combination of certbot-auto's
@@ -8,21 +8,19 @@
 # get the root of the Certbot repo
 tools_dir=$(dirname $("$(dirname $0)/readlink.py" $0))
 dev_constraints="$tools_dir/dev_constraints.txt"
-temp_dir=$(mktemp -d)
-trap "rm -rf $temp_dir" EXIT
-full_constraints="$temp_dir/full_constraints.txt"
-partial_constraints="$temp_dir/partial_constraints.txt"
+merge_reqs="$tools_dir/merge_requirements.py"
+test_constraints=$(mktemp)
+trap "rm -f $test_constraints" EXIT
 
 if [ "$CERTBOT_OLDEST" = 1 ]; then
-    cp "$tools_dir/oldest_constraints.txt" "$partial_constraints"
+    cp "$tools_dir/oldest_constraints.txt" "$test_constraints"
 else
     repo_root=$(dirname "$tools_dir")
     certbot_requirements="$repo_root/letsencrypt-auto-source/pieces/dependency-requirements.txt"
-    sed -n -e 's/^\([^[:space:]]*==[^[:space:]]*\).*$/\1/p' "$certbot_requirements" > "$partial_constraints"
+    sed -n -e 's/^\([^[:space:]]*==[^[:space:]]*\).*$/\1/p' "$certbot_requirements" > "$test_constraints"
 fi
-"$tools_dir/merge_requirements.py" "$dev_constraints" "$partial_constraints" > "$full_constraints"
 
 set -x
 
 # install the requested packages using the pinned requirements as constraints
-pip install -q --constraint "$full_constraints" "$@"
+pip install -q --constraint <("$merge_reqs" "$dev_constraints" "$test_constraints") "$@"
