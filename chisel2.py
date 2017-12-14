15a8a1e7d824585ec9a11d8f01a96f170bb22a2d
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-12-14 12:54:33 -0800

    Remove verify_ssl=False.

--- chisel2.py
+++ chisel2.py
@@ -42,7 +42,7 @@ def make_client(email=None):
     """Build an acme.Client and register a new account with a random key."""
     key = jose.JWKRSA(key=rsa.generate_private_key(65537, 2048, default_backend()))
 
-    net = acme_client.ClientNetwork(key, verify_ssl=False, acme_version=2,
+    net = acme_client.ClientNetwork(key, acme_version=2,
                                     user_agent="Boulder integration tester")
 
     client = acme_client.Client(DIRECTORY, key=key, net=net, acme_version=2)
