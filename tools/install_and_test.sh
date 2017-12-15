c5d43bcebf48ebaebb4316a34405c4542ef91432
Brad Warren <bmw@eff.org>
2017-12-15 08:49:00 -0800

    Undo changes to install_and_test.sh

--- tools/install_and_test.sh
+++ tools/install_and_test.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -e
 # pip installs the requested packages in editable mode and runs unit tests on
 # them. Each package is installed and tested in the order they are provided
-# before the script moves on to the next package.
+# before the script moves on to the next package. If CERTBOT_NO_PIN is set not
+# set to 1, packages are installed using certbot-auto's requirements file as
+# constraints.
 
-pip_install="$(dirname $0)/pip_install_editable.sh"
+if [ "$CERTBOT_NO_PIN" = 1 ]; then
+  pip_install="pip install -q -e"
+else
+  pip_install="$(dirname $0)/pip_install_editable.sh"
+fi
 
 set -x
 for requirement in "$@" ; do
