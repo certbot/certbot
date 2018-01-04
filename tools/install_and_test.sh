504c5e0453a041e169c1e815ab58abb9ab6b0fae
Brad Warren <bmw@eff.org>
2018-01-04 14:26:28 -0800

    test moar things

--- tools/install_and_test.sh
+++ tools/install_and_test.sh
@@ -22,5 +22,5 @@ for requirement in "$@" ; do
     # See https://travis-ci.org/certbot/certbot/jobs/308774157#L1333.
     pkg=$(echo "$pkg" | tr - _)
   fi
-  PYTEST_DEBUG=1 pytest --numprocesses 2 -vvvv --pyargs $pkg
+  PYTEST_DEBUG=1 pytest --max-slave-restart 0 --numprocesses auto -vvvv --pyargs $pkg
 done
