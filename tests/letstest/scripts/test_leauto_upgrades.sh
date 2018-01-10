684541794541e66f85016e3265aa050d6971dba1
Brad Warren <bmw@eff.org>
2018-01-10 08:39:49 -0800

    Add double venv test to leauto_upgrades

--- tests/letstest/scripts/test_leauto_upgrades.sh
+++ tests/letstest/scripts/test_leauto_upgrades.sh
@@ -78,6 +78,13 @@ if [ $(python -V 2>&1 | cut -d" " -f 2 | cut -d. -f1,2 | sed 's/\.//') -eq 26 ];
         echo "New directory shouldn't have been created!"
         exit 1
     fi
+    # Create a 2nd venv at the new path to ensure we properly handle this case
+    export VENV_PATH="/opt/eff.org/certbot/venv"
+    if ! sudo ./letsencrypt-auto -v --debug --version --no-self-upgrade 2>&1 | grep 0.5.0 ; then
+        echo second installation appeared to fail
+        exit 1
+    fi
+    unset VENV_PATH
     EXPECTED_VERSION=$(grep -m1 LE_AUTO_VERSION certbot-auto | cut -d\" -f2)
     if ! ./cb-auto -v --debug --version -n | grep "$EXPECTED_VERSION" ; then
         echo "Certbot didn't upgrade as expected!"
