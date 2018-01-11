3100dca8808cd7c5c91577e4d135fb20b784bdcd
Brad Warren <bmw@eff.org>
2018-01-11 14:05:09 -0800

    faster tests

--- certbot-nginx/tests/boulder-integration.sh
+++ certbot-nginx/tests/boulder-integration.sh
@@ -26,7 +26,7 @@ test_deployment_and_rollback() {
     # Arguments: certname
     echo | openssl s_client -connect localhost:5001 \
         | openssl x509 -out $root/nginx.pem
-    diff -q $root/nginx.pem "$root/conf/live/$certname/cert.pem"
+    diff -q $root/nginx.pem "$root/conf/live/$1/cert.pem"
 
     certbot_test_nginx rollback --checkpoints 9001
     diff -q <(echo "$original") $nginx_conf
