9ada7711f66c868f8424257a76581f2ec1e6cc1f
Brad Warren <bmw@eff.org>
2018-01-11 13:04:26 -0800

    test it

--- certbot-nginx/tests/boulder-integration.sh
+++ certbot-nginx/tests/boulder-integration.sh
@@ -22,13 +22,19 @@ certbot_test_nginx () {
         "$@"
 }
 
-certbot_test_nginx --domains nginx.wtf run
-echo | openssl s_client -connect localhost:5001 \
-    | openssl x509 -out $root/nginx.pem
-diff -q $root/nginx.pem $root/conf/live/nginx.wtf/cert.pem
+test_deployment_and_rollback() {
+    echo | openssl s_client -connect localhost:5001 \
+        | openssl x509 -out $root/nginx.pem
+    diff -q $root/nginx.pem $root/conf/live/nginx.wtf/cert.pem
 
-certbot_test_nginx rollback --checkpoints 9001
-diff -q <(echo "$original") $nginx_conf
+    certbot_test_nginx rollback --checkpoints 9001
+    diff -q <(echo "$original") $nginx_conf
+}
+
+certbot_test_nginx --domains nginx.wtf run
+test_deployment_and_rollback
+certbot_test_nginx --force-renewal --preferred-challenges http
+test_deployment_and_rollback
 
 # note: not reached if anything above fails, hence "killall" at the
 # top
