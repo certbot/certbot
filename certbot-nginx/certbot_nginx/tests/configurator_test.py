557815bf94d4284399290499668a374af3059abb
Erica Portnoy <ebportnoy@gmail.com>
2018-01-10 19:09:53 -0800

    update existing nginx tests

--- certbot-nginx/certbot_nginx/tests/configurator_test.py
+++ certbot-nginx/certbot_nginx/tests/configurator_test.py
@@ -100,7 +100,7 @@ class NginxConfiguratorTest(util.NginxTest):
             errors.PluginError, self.config.enhance, 'myhost', 'unknown_enhancement')
 
     def test_get_chall_pref(self):
-        self.assertEqual([challenges.TLSSNI01],
+        self.assertEqual([challenges.TLSSNI01, challenges.HTTP01],
                          self.config.get_chall_pref('myhost'))
 
     def test_save(self):
