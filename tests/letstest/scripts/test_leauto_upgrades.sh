07c7fc2a873027501c60f67ca1a1f234fe042047
Hugo <hugovk@users.noreply.github.com>
2018-02-13 10:20:37 +0200

    Revert to test installing Py3 on Py2.6 systems

--- tests/letstest/scripts/test_leauto_upgrades.sh
+++ tests/letstest/scripts/test_leauto_upgrades.sh
@@ -64,23 +64,32 @@ iQIDAQAB
 -----END PUBLIC KEY-----
 "
 
-if ./letsencrypt-auto -v --debug --version | grep "WARNING: couldn't find Python" ; then
-    echo "Had problems checking for updates!"
-    exit 1
-fi
-
-EXPECTED_VERSION=$(grep -m1 LE_AUTO_VERSION certbot-auto | cut -d\" -f2)
-if ! /opt/eff.org/certbot/venv/bin/letsencrypt --version 2>&1 | grep "$EXPECTED_VERSION" ; then
-    echo upgrade appeared to fail
-    exit 1
-fi
-
-if ! diff letsencrypt-auto letsencrypt-auto-source/letsencrypt-auto ; then
-    echo letsencrypt-auto and letsencrypt-auto-source/letsencrypt-auto differ
-    exit 1
-fi
-
-if [ "$RUN_PYTHON3_TESTS" = 1 ]; then
+if [ $(python -V 2>&1 | cut -d" " -f 2 | cut -d. -f1,2 | sed 's/\.//') -eq 26 ]; then
+    if command -v python3; then
+        echo "Didn't expect Python 3 to be installed!"
+        exit 1
+    fi
+    cp letsencrypt-auto cb-auto
+    if ! ./cb-auto -v --debug --version 2>&1 | grep 0.5.0 ; then
+        echo "Certbot shouldn't have updated to a new version!"
+        exit 1
+    fi
+    if [ -d "/opt/eff.org" ]; then
+        echo "New directory shouldn't have been created!"
+        exit 1
+    fi
+    # Create a 2nd venv at the new path to ensure we properly handle this case
+    export VENV_PATH="/opt/eff.org/certbot/venv"
+    if ! sudo -E ./letsencrypt-auto -v --debug --version --no-self-upgrade 2>&1 | grep 0.5.0 ; then
+        echo second installation appeared to fail
+        exit 1
+    fi
+    unset VENV_PATH
+    EXPECTED_VERSION=$(grep -m1 LE_AUTO_VERSION certbot-auto | cut -d\" -f2)
+    if ! ./cb-auto -v --debug --version -n 2>&1 | grep "$EXPECTED_VERSION" ; then
+        echo "Certbot didn't upgrade as expected!"
+        exit 1
+    fi
     if ! command -v python3; then
         echo "Python3 wasn't properly installed"
         exit 1
@@ -89,7 +98,11 @@ if [ "$RUN_PYTHON3_TESTS" = 1 ]; then
         echo "Python3 wasn't used in venv!"
         exit 1
     fi
+elif ! ./letsencrypt-auto -v --debug --version || ! diff letsencrypt-auto letsencrypt-auto-source/letsencrypt-auto ; then
+    echo upgrade appeared to fail
+    exit 1
 fi
+
 echo upgrade appeared to be successful
 
 if [ "$(tools/readlink.py ${XDG_DATA_HOME:-~/.local/share}/letsencrypt)" != "/opt/eff.org/certbot/venv" ]; then
