613a2e63ecf60eb1164801b16a04bd58df8daebc
Sydney Li <sydli@stanford.edu>
2018-02-15 11:55:10 -0800

    Fix for MacOS

--- certbot/plugins/webroot.py
+++ certbot/plugins/webroot.py
@@ -180,10 +180,10 @@ to serve all files under specified web root ({0})."""
                             logger.info("Unable to change owner and uid of webroot directory")
                             logger.debug("Error was: %s", exception)
                     except OSError as exception:
-                        if exception.errno != errno.EEXIST:
+                        if exception.errno not in (errno.EEXIST, errno.EISDIR):
                             raise errors.PluginError(
                                 "Couldn't create root for {0} http-01 "
-                                "challenge responses: {1}", name, exception)
+                                "challenge responses: {1}".format(name, exception))
             finally:
                 os.umask(old_umask)
 
