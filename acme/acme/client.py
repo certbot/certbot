b07ee70a8f0dc122eaf9a9eda68a79bf5e187ec6
Brad Warren <bmw@eff.org>
2018-02-22 12:22:39 -0800

    Add content_type when downloading certificate.

--- acme/acme/client.py
+++ acme/acme/client.py
@@ -652,7 +652,8 @@ class ClientV2(ClientBase):
             if body.error is not None:
                 raise errors.IssuanceError(body.error)
             if body.certificate is not None:
-                certificate_response = self.net.get(body.certificate).text
+                certificate_response = self.net.get(body.certificate,
+                                                    content_type=DER_CONTENT_TYPE).text
                 return orderr.update(body=body, fullchain_pem=certificate_response)
         raise errors.TimeoutError()
 
