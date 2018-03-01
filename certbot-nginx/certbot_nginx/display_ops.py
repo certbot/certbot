82b93ff8cc868094cfd968bc62699bbd3c9e72c6
Erica Portnoy <ebportnoy@gmail.com>
2018-02-28 20:59:55 -0800

    take selecting vhosts out of loop

--- certbot-nginx/certbot_nginx/display_ops.py
+++ certbot-nginx/certbot_nginx/display_ops.py
@@ -20,15 +20,17 @@ def select_vhost_multiple(vhosts):
     """
     if not vhosts:
         return list()
-    tags_list = [vhost.display_repr() for vhost in vhosts]
-    while True:
-        code, names = zope.component.getUtility(interfaces.IDisplay).checklist(
-            "Which server blocks would you like to modify?",
-            tags=tags_list, force_interactive=True)
-        if code == display_util.OK:
-            return_vhosts = _reversemap_vhosts(names, vhosts)
-            return return_vhosts
-        return []
+    tags_list = [vhost.display_repr()+"\n" for vhost in vhosts]
+    # Remove the extra newline from the last entry
+    if len(tags_list):
+        tags_list[-1] = tags_list[-1][:-1]
+    code, names = zope.component.getUtility(interfaces.IDisplay).checklist(
+        "Which server blocks would you like to modify?",
+        tags=tags_list, force_interactive=True)
+    if code == display_util.OK:
+        return_vhosts = _reversemap_vhosts(names, vhosts)
+        return return_vhosts
+    return []
 
 def _reversemap_vhosts(names, vhosts):
     """Helper function for select_vhost_multiple for mapping string
@@ -37,6 +39,6 @@ def _reversemap_vhosts(names, vhosts):
 
     for selection in names:
         for vhost in vhosts:
-            if vhost.display_repr() == selection:
+            if vhost.display_repr().strip() == selection.strip():
                 return_vhosts.append(vhost)
     return return_vhosts
