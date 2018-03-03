41097f088b7146931759be777f863f4422d9acb8
Brad Warren <bmw@eff.org>
2018-03-02 16:36:45 -0800

    Install requirements in separate step.

--- tools/pip_install.sh
+++ tools/pip_install.sh
@@ -7,26 +7,30 @@
 
 # get the root of the Certbot repo
 tools_dir=$(dirname $("$(dirname $0)/readlink.py" $0))
-dev_constraints="$tools_dir/dev_constraints.txt"
-merge_reqs="$tools_dir/merge_requirements.py"
-requirements=""
+all_constraints=$(mktemp)
 test_constraints=$(mktemp)
-trap "rm -f $test_constraints" EXIT
+trap "rm -f $all_constraints $test_constraints" EXIT
 
 if [ "$CERTBOT_OLDEST" = 1 ]; then
     if [ "$1" != "-e" -o "$#" -ne "2" ]; then
         echo "When CERTBOT_OLDEST is set, this script must be run with a single -e <path> argument."
         exit 1
     fi
+    requirements="$2/local-oldest-requirements.txt"
     cp "$tools_dir/oldest_constraints.txt" "$test_constraints"
-    requirements="-r$2/local-oldest-requirements.txt"
 else
     repo_root=$(dirname "$tools_dir")
     certbot_requirements="$repo_root/letsencrypt-auto-source/pieces/dependency-requirements.txt"
     sed -n -e 's/^\([^[:space:]]*==[^[:space:]]*\).*$/\1/p' "$certbot_requirements" > "$test_constraints"
 fi
 
+"$tools_dir/merge_requirements.py" "$tools_dir/dev_constraints.txt" \
+                                   "$test_constraints" > "$all_constraints"
+
 set -x
 
 # install the requested packages using the pinned requirements as constraints
-pip install -q --constraint <("$merge_reqs" "$dev_constraints" "$test_constraints") $requirements "$@"
+if [ -n "$requirements" ]; then
+    pip install -q --constraint "$all_constraints" --requirement "$requirements"
+fi
+pip install -q --constraint "$all_constraints" "$@"
