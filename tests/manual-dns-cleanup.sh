68cfc9d199947a4684995f77939ca09c6afeb43e
Brad Warren <bmw@users.noreply.github.com>
2018-03-08 11:12:33 -0800

    Fix --allow-subset-of-names (#5690)
    * Remove aauthzr instance variable
    
    * If domain begins with fail, fail the challenge.
    
    * test --allow-subset-of-names
    
    * Fix renewal and add extra check
    
    * test after hook checks
    
    (cherry picked from commit cc24b4e40af5841c8dfddeecd9bde7d1acce62e8)

--- tests/manual-dns-cleanup.sh
+++ tests/manual-dns-cleanup.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-curl -X POST 'http://localhost:8055/clear-txt' -d \
-    "{\"host\": \"_acme-challenge.$CERTBOT_DOMAIN.\"}"
+#!/bin/bash
+
+# If domain begins with fail, we didn't complete the challenge so there is
+# nothing to clean up.
+if [[ "$CERTBOT_DOMAIN" != fail* ]]; then
+    curl -X POST 'http://localhost:8055/clear-txt' -d \
+        "{\"host\": \"_acme-challenge.$CERTBOT_DOMAIN.\"}"
+fi
