2ca2e91886246a53736701d78665f15974ab8854
Erik Tews <erik@datenzone.de>
2018-03-09 16:19:31 +0100

    Improve error reporting

--- certbot-dns-rfc2136/certbot_dns_rfc2136/dns_rfc2136.py
+++ certbot-dns-rfc2136/certbot_dns_rfc2136/dns_rfc2136.py
@@ -94,7 +94,7 @@ class _RFC2136Client(object):
         try:
             self.server = self._get_ip_for_hostname(server)
         except Exception as e:
-            raise errors.PluginError("Failed to resolv {0}".format(server))
+            raise errors.PluginError("Failed to resolv {0}: {1}".format(server, e))
 
         self.keyring = dns.tsigkeyring.from_text({
             key_name: key_secret
