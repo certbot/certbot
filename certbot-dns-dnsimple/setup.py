8231b1a19cc62262f099dc17609d4d1041e2e5da
sydneyli <sydli@stanford.edu>
2018-03-29 17:09:21 -0700

    Pin Lexicon version to 2.2.1 (#5803)

--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -11,7 +11,7 @@ version = '0.23.0.dev0'
 install_requires = [
     'acme>=0.21.1',
     'certbot>=0.21.1',
-    'dns-lexicon',
+    'dns-lexicon>=2.2.1', # Support for >1 TXT record per name
     'mock',
     'setuptools',
     'zope.interface',
