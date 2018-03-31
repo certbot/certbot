ad5134331cfea4c6b4f20acb8e901297e6549557
Brad Warren <bmw@eff.org>
2018-03-30 17:37:14 -0700

    allow for running individual integration tests

---
+++ tests/boulder-integration.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$TOXENV" != "py27-nginx-oldest" ]; then
+   tests/certbot-boulder-integration.sh
+fi
+if [ "$TOXENV" != "py27-certbot-oldest" ]; then
+    # Most CI systems set this variable to true.
+    # If the tests are running as part of CI, Nginx should be available.
+    if ${CI:-false} || type nginx; then
+        certbot-nginx/tests/boulder-integration.sh
+    fi
+fi
