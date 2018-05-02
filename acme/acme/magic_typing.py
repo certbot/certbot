be1abbbc8dc796e2411174d5787f07de7ae6768d
Erica Portnoy <ebportnoy@gmail.com>
2018-05-01 18:04:52 -0700

    comment magic_typing.py

--- acme/acme/magic_typing.py
+++ acme/acme/magic_typing.py
@@ -1,11 +1,15 @@
+"""Shim class to not have to depend on typing module in prod."""
 import sys
 
 class TypingClass(object):
+    """Ignore import errors by getting anything"""
     def __getattr__(self, name):
         return None
 
 try:
+    # mypy doesn't respect modifying sys.modules
     from typing import *
+    # cache into sys.modules for when we're actually running
     import typing
     sys.modules[__name__] = typing
 except ImportError:
