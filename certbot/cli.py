2916a80f1a7772c171c61775d69c9f964f416145
Alexandre Macabies <web+oss@zopieux.com>
2018-05-03 20:09:48 +0200

    Add certbot-dns-alwaysdata module into certbot main tree
    This includes the main cli entrypoint and the development tools.

--- certbot/cli.py
+++ certbot/cli.py
@@ -1396,6 +1396,10 @@ def _plugins_parsing(helpful, plugins):
     helpful.add(["plugins", "certonly"], "--webroot", action="store_true",
                 default=flag_default("webroot"),
                 help="Obtain certificates by placing files in a webroot directory.")
+    helpful.add(["plugins", "certonly"], "--dns-alwaysdata", action="store_true",
+                default=flag_default("dns_alwaysdata"),
+                help=("Obtain certificates using a DNS TXT record (if you are "
+                      "using Alwaysdata for DNS)."))
     helpful.add(["plugins", "certonly"], "--dns-cloudflare", action="store_true",
                 default=flag_default("dns_cloudflare"),
                 help=("Obtain certificates using a DNS TXT record (if you are "
