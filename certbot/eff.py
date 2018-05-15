722dac86d558af58c1aed27ad9d2b71bfee946ae
Douglas Anger <douganger@gmail.com>
2018-05-15 15:50:47 -0400

    Fix crash when email submission endpoint unavailable (#6002)
    * Fix crash when email submission endpoint unavailable
    
    Handle KeyError and ValueError so that if the email submission endpoint
    goes down, Certbot can still run.
    
    Add tests to eff_test.py:
     - simulate non-JSON response as described in issue #5858
     - simulate JSON response without 'status' element
    
    Non-JSON response throws an uncaught ValueError when attempting to
    decode as JSON. A JSON response missing the 'status' element throws an
    uncaught KeyError when checking whether status is True or False.
    
    Teach _check_response to handle ValueError and KeyError and report an
    issue to the user.
    
    Rewrite if statement as assertion with try-except block to make error
    handling consistent within the function. Update test_not_ok to make
    mocked raise_for_status function raise a requests.exceptions.HTTPError.
    
    Resolves #5858
    
    * Update PR with requested changes
    
    - Use `if` instead of `assert` to check `status` element of response JSON
    - Handle KeyError and ValueError in the same way
    - Import requests at the beginning of eff_test.py
    - Clear JSON in test case in a more idiomatic way

--- certbot/eff.py
+++ certbot/eff.py
@@ -71,11 +71,14 @@ def _check_response(response):
 
     """
     logger.debug('Received response:\n%s', response.content)
-    if response.ok:
-        if not response.json()['status']:
+    try:
+        response.raise_for_status()
+        if response.json()['status'] == False:
             _report_failure('your e-mail address appears to be invalid')
-    else:
+    except requests.exceptions.HTTPError:
         _report_failure()
+    except (ValueError, KeyError):
+        _report_failure('there was a problem with the server response')
 
 
 def _report_failure(reason=None):
