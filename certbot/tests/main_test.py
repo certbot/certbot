41e1976c178dac8875584e8f11d68e28515e0a01
Brad Warren <bmw@users.noreply.github.com>
2018-05-16 06:24:14 -0700

    Fix noisy tests (#6004)
    * Fixes #5570.
    
    The issue is calls to atexit aren't mocked out. During the tests there are many
    repeated calls registering functions to be called when the process exits so
    when the tests finishes, it prints a ton of output from running those
    registered functions. This suppresses that by mocking out atexit.
    
    * Mock at a lower level.
    
    This ensures we don't mess with any other mocks in this test class by mocking
    at the lowest level we can. Other tests shouldn't be mocking out specific
    internals of functions in other modules, so this should work just fine.

--- certbot/tests/main_test.py
+++ certbot/tests/main_test.py
@@ -626,7 +626,8 @@ class MainTest(test_util.ConfigTestCase):  # pylint: disable=too-many-public-met
         toy_stdout = stdout if stdout else six.StringIO()
         with mock.patch('certbot.main.sys.stdout', new=toy_stdout):
             with mock.patch('certbot.main.sys.stderr') as stderr:
-                ret = main.main(args[:])  # NOTE: parser can alter its args!
+                with mock.patch("certbot.util.atexit"):
+                    ret = main.main(args[:])  # NOTE: parser can alter its args!
         return ret, toy_stdout, stderr
 
     def test_no_flags(self):
