64a424eefee3c1843500b4cfaa379e6379694f13
Joona Hoikkala <joona@kuori.org>
2018-05-25 19:29:52 +0300

    Use ConfigTestCase instead of mocking config objects manually

--- certbot/tests/renewupdater_test.py
+++ certbot/tests/renewupdater_test.py
@@ -9,10 +9,11 @@ from certbot import updater
 import certbot.tests.util as test_util
 
 
-class RenewUpdaterTest(unittest.TestCase):
+class RenewUpdaterTest(test_util.ConfigTestCase):
     """Tests for interfaces.RenewDeployer and interfaces.GenericUpdater"""
 
     def setUp(self):
+        super(RenewUpdaterTest, self).setUp()
         class MockInstallerGenericUpdater(interfaces.GenericUpdater):
             """Mock class that implements GenericUpdater"""
             def __init__(self, *args, **kwargs):
@@ -33,20 +34,10 @@ class RenewUpdaterTest(unittest.TestCase):
         self.generic_updater = MockInstallerGenericUpdater()
         self.renew_deployer = MockInstallerRenewDeployer()
 
-    def get_config(self, args):
-        """Get mock config from dict of parameters"""
-        config = mock.MagicMock()
-        for key in args.keys():
-            config.__dict__[key] = args[key]
-        return config
-
     @mock.patch('certbot.main._get_and_save_cert')
     @mock.patch('certbot.plugins.selection.choose_configurator_plugins')
     @test_util.patch_get_utility()
     def test_server_updates(self, _, mock_select, mock_getsave):
-        config = self.get_config({"disable_renew_updates": False,
-                                  "dry_run": False})
-
         lineage = mock.MagicMock()
         lineage.names.return_value = ['firstdomain', 'seconddomain']
         mock_getsave.return_value = lineage
@@ -55,38 +46,34 @@ class RenewUpdaterTest(unittest.TestCase):
         # Generic Updater
         mock_select.return_value = (mock_generic_updater, None)
         with mock.patch('certbot.main._init_le_client'):
-            main.renew_cert(config, None, mock.MagicMock())
+            main.renew_cert(self.config, None, mock.MagicMock())
         self.assertTrue(mock_generic_updater.restart.called)
 
         mock_generic_updater.restart.reset_mock()
         mock_generic_updater.callcounter.reset_mock()
-        updater.run_generic_updaters(config, lineage, None)
+        updater.run_generic_updaters(self.config, lineage, None)
         self.assertEqual(mock_generic_updater.callcounter.call_count, 2)
         self.assertFalse(mock_generic_updater.restart.called)
 
     def test_renew_deployer(self):
-        config = self.get_config({"disable_renew_updates": False,
-                                  "dry_run": False})
         lineage = mock.MagicMock()
         lineage.names.return_value = ['firstdomain', 'seconddomain']
         mock_deployer = self.renew_deployer
-        updater.run_renewal_deployer(config, lineage, mock_deployer)
+        updater.run_renewal_deployer(self.config, lineage, mock_deployer)
         self.assertTrue(mock_deployer.callcounter.called_with(lineage))
 
     @mock.patch("certbot.updater.logger.debug")
     def test_updater_skip_dry_run(self, mock_log):
-        config = self.get_config({"disable_renew_updates": False,
-                                  "dry_run": True})
-        updater.run_generic_updaters(config, None, None)
+        self.config.dry_run = True
+        updater.run_generic_updaters(self.config, None, None)
         self.assertTrue(mock_log.called)
         self.assertEquals(mock_log.call_args[0][0],
                           "Skipping updaters in dry-run mode.")
 
     @mock.patch("certbot.updater.logger.debug")
     def test_deployer_skip_dry_run(self, mock_log):
-        config = self.get_config({"disable_renew_updates": False,
-                                  "dry_run": True})
-        updater.run_renewal_deployer(config, None, None)
+        self.config.dry_run = True
+        updater.run_renewal_deployer(self.config, None, None)
         self.assertTrue(mock_log.called)
         self.assertEquals(mock_log.call_args[0][0],
                           "Skipping renewal deployer in dry-run mode.")
