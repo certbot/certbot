792b651dc5785a69d03245ce65d3bd64f81463d7
Trinopoty Biswas <trinopoty@hotmail.com>
2018-06-20 16:47:27 +0530

    Bump version to 0.26.0

--- certbot-dns-linode/setup.py
+++ certbot-dns-linode/setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.25.0.dev0'
+version = '0.26.0.dev0'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
