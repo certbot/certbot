0a7a58d1b408b419cb36d34554d017d8c95e9aca
Brad Warren <bmw@eff.org>
2018-06-27 10:26:42 -0700

    remove unused import

--- acme/acme/standalone_test.py
+++ acme/acme/standalone_test.py
@@ -16,7 +16,6 @@ import requests
 
 from acme import challenges
 from acme import crypto_util
-from acme import errors
 from acme import test_util
 from acme.magic_typing import Set # pylint: disable=unused-import, no-name-in-module
 
