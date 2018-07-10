0672e63176f51b30b5faaa089e425bbe4aa28dd9
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-07-10 13:52:58 -0700

    Remove main components from Alpha. (#6187)
    acme, certbot, and the Nginx and Apache plugins should no longer be considered alpha-quality.

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
     license='Apache License 2.0',
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Console :: Curses',
         'Intended Audience :: System Administrators',
