0a6d520d26c8466c69078734dfbff096470ae807
Brad Warren <bmw@eff.org>
2018-07-11 14:18:44 -0700

    Bump version to 0.27.0

--- certbot-dns-ovh/setup.py
+++ certbot-dns-ovh/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '0.26.0'
+version = '0.27.0.dev0'
 
 # Remember to update local-oldest-requirements.txt when changing the minimum
 # acme/certbot version.
