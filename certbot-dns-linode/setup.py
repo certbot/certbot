a0d68338a2da9ff0d7c751777086dc56b88839b2
Erica Portnoy <ebportnoy@gmail.com>
2018-07-16 16:36:59 -0700

    Release 0.26.1

--- certbot-dns-linode/setup.py
+++ certbot-dns-linode/setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.26.0'
+version = '0.26.1'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
