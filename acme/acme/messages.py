d19698251da19f363d94ece9e5ee9dcc425afad2
Joona Hoikkala <joona@kuori.org>
2018-07-31 17:08:39 +0300

    Do not send status or resource fields in newOrder payloads for ACMEv2

--- acme/acme/messages.py
+++ acme/acme/messages.py
@@ -509,11 +509,10 @@ class Revocation(jose.JSONObjectWithFields):
     reason = jose.Field('reason')
 
 
-class Order(ResourceBody):
+class OrderBase(ResourceBody):
     """Order Resource Body.
 
     :ivar list of .Identifier: List of identifiers for the certificate.
-    :ivar acme.messages.Status status:
     :ivar list of str authorizations: URLs of authorizations.
     :ivar str certificate: URL to download certificate as a fullchain PEM.
     :ivar str finalize: URL to POST to to request issuance once all
@@ -522,8 +521,6 @@ class Order(ResourceBody):
     :ivar .Error error: Any error that occurred during finalization, if applicable.
     """
     identifiers = jose.Field('identifiers', omitempty=True)
-    status = jose.Field('status', decoder=Status.from_json,
-                        omitempty=True, default=STATUS_PENDING)
     authorizations = jose.Field('authorizations', omitempty=True)
     certificate = jose.Field('certificate', omitempty=True)
     finalize = jose.Field('finalize', omitempty=True)
@@ -533,6 +530,16 @@ class Order(ResourceBody):
     @identifiers.decoder
     def identifiers(value):  # pylint: disable=missing-docstring,no-self-argument
         return tuple(Identifier.from_json(identifier) for identifier in value)
+
+
+class Order(OrderBase):
+    """Order Resource Body for ACMEv1
+
+    :ivar acme.messages.Status status:
+    """
+    status = jose.Field('status', decoder=Status.from_json,
+                        omitempty=True, default=STATUS_PENDING)
+
 
 class OrderResource(ResourceWithURI):
     """Order Resource.
@@ -549,8 +556,8 @@ class OrderResource(ResourceWithURI):
     authorizations = jose.Field('authorizations')
     fullchain_pem = jose.Field('fullchain_pem', omitempty=True)
 
+
 @Directory.register
-class NewOrder(Order):
-    """New order."""
-    resource_type = 'new-order'
-    resource = fields.Resource(resource_type)
+class NewOrder(OrderBase):
+    """New order for ACMEv2"""
+    resource_type = "new-order"
