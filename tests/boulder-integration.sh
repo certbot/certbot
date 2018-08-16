cc3702f2d12de1b0377165f0099d26ec5def1a61
Brad Warren <bmw@eff.org>
2018-08-16 11:08:38 -0700

    Use INTEGRATION_TEST in script

--- tests/boulder-integration.sh
+++ tests/boulder-integration.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-if [ "$TOXENV" != "py27-nginx-oldest" ]; then
+if [ "$INTEGRATION_TEST" = "certbot" ]; then
+    tests/certbot-boulder-integration.sh
+elif [ "$INTEGRATION_TEST" = "nginx" ]; then
+    certbot-nginx/tests/boulder-integration.sh
+else
    tests/certbot-boulder-integration.sh
-fi
-if [ "$TOXENV" != "py27-certbot-oldest" ]; then
     # Most CI systems set this variable to true.
     # If the tests are running as part of CI, Nginx should be available.
     if ${CI:-false} || type nginx; then
