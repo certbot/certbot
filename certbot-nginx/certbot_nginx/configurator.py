3977e4e7d25a87aa8f7f0099e076daaa91ec74ce
Erica Portnoy <ebportnoy@gmail.com>
2018-09-13 15:15:06 -0700

    update choose_http_and_https_vhosts to always have an http and https vhost, when possible

--- certbot-nginx/certbot_nginx/configurator.py
+++ certbot-nginx/certbot_nginx/configurator.py
@@ -497,39 +497,32 @@ class NginxConfigurator(common.Installer):
         :rtype: tuple of :class:`~certbot_nginx.obj.VirtualHost`
 
         """
-        if util.is_wildcard_domain(target_name):
-            raise errors.NotSupportedError("Wildcard issuance not supported by this method.")
-
         # goal: return a set of vhosts that contains at least one http server and at least one
         # https server
+        if util.is_wildcard_domain(target_name):
+            raise errors.NotSupportedError("Wildcard issuance not supported by this method.")
 
         # first, fix choose_vhost to prefer all matching ssl server blocks over all matching
         # non-ssl server blocks
 
         # after that, just run choose_vhosts with create_if_no_match
+        try:
+            https_vhosts = choose_vhosts(target_name, create_if_no_match=True)
+        except errors.MisconfigurationError:
+            # we couldn't choose a default and there was no match
+            https_vhosts = []
         # the thing it returns will be definitely https, maybe also http
         # what if there's an ssl-only block?
         # run choose_redirect vhosts to find a matching http block
-        # if they're the same, remove one
-        # return
-
         try:
-            # take first item because we don't support wildcards
-            http_vhost = self.choose_redirect_vhosts(target_name, http_port,
-                create_if_no_match=True)[0]
+            http_vhosts = self.choose_redirect_vhosts(target_name, http_port,
+                create_if_no_match=True)
         except errors.MisconfigurationError:
-            http_vhost = None
-        try:
-            # take first item because we don't support wildcards
-            https_vhosts = self._choose_vhost_single(target_name)
-            # first, try to find an already-ssl block that matches by server name
-            # if that doesn't exist, find the most-matching non-ssl block
-            # if none exist, make sure to create one
-        except errors.MisconfigurationError:
-            https_vhosts = None
-        https_vhost = https_vhosts[0].ssl if https_vhosts and https_vhosts[0].ssl else None
+            # we couldn't choose a default and there was no match
+            https_vhosts = []
 
-        return (http_vhost, https_vhost)
+        vhosts = set(https_vhosts).update(http_vhosts)
+        return vhosts
 
     def _port_matches(self, test_port, matching_port):
         # test_port is a number, matching is a number or "" or None
