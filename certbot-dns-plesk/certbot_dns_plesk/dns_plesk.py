4d68071dd55e8d45f5a54c10a997e28e428583a6
Noci <noci2012@github.com>
2018-09-29 01:06:22 +0200

    bugfixing.... seems to work now.

--- certbot-dns-plesk/certbot_dns_plesk/dns_plesk.py
+++ certbot-dns-plesk/certbot_dns_plesk/dns_plesk.py
@@ -39,9 +39,9 @@ class Authenticator(dns_common.DNSAuthenticator):
             'credentials',
             'Plesk credentials INI file',
             {
-                'username': 'Username for Plesk API. (See {0}.)',
-                'password': 'Password for Plesk API. (See {0}.)',
-                'plesk_server': 'Url of the host https://hostname:port',
+                'username': 'Username for Plesk API.',
+                'password': 'Password for Plesk API.',
+                'pleskserver': 'Url of the host https://hostname:port',
             }
         )
 
@@ -52,9 +52,9 @@ class Authenticator(dns_common.DNSAuthenticator):
         self._get_plesk_client(domain).del_txt_record(domain, validation_name, validation)
 
     def _get_plesk_client(self,domain):
-        return _PleskLexiconClient(self.credentials.conf('plesk_server'), 
-                 self.credentials.conf('auth_username'),
-                 self.credentials.conf('auth_password'),
+        return _PleskLexiconClient(self.credentials.conf('pleskserver'), 
+                 self.credentials.conf('username'),
+                 self.credentials.conf('password'),
                  domain)
 
 
@@ -63,20 +63,20 @@ class _PleskLexiconClient(dns_common_lexicon.LexiconClient):
     Encapsulates all communication with the Plesk via Lexicon.
     """
 
-    def __init__(self, ples_server, username, password, domain ):
+    def __init__(self, pleskserver, username, password, domain ):
         super(_PleskLexiconClient, self).__init__()
 
         self.provider = plesk.Provider({
             'auth_username': username,
             'auth_password': password,
-            'plesk_server': plesk_server,
+            'plesk_server': pleskserver,
             'domain': domain,
         })
 
     def _handle_http_error(self, e, domain_name):
         hint = None
         if str(e).startswith('401 Client Error: Unauthorized for url:'):
-            hint = 'Is your plesk_server & authentication info correct?'
+            hint = 'Is your pleskserver & authentication info correct?'
 
         return errors.PluginError('Error determining zone identifier for {0}: {1}.{2}'
                                   .format(domain_name, e, ' ({0})'.format(hint) if hint else ''))
