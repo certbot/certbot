1b595f26d867e6ee292f319ff93a2d8bf8ce3f0f
Erica Portnoy <ebportnoy@gmail.com>
2018-11-02 13:31:48 -0700

    Requests no longer vendorizes urllib3

--- acme/acme/client.py
+++ acme/acme/client.py
@@ -32,11 +32,8 @@ logger = logging.getLogger(__name__)
 # for SSL, which does allow these options to be configured.
 # https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning
 if sys.version_info < (2, 7, 9):  # pragma: no cover
-    try:
-        requests.packages.urllib3.contrib.pyopenssl.inject_into_urllib3()  # type: ignore
-    except AttributeError:
-        import urllib3.contrib.pyopenssl  # pylint: disable=import-error
-        urllib3.contrib.pyopenssl.inject_into_urllib3()
+    import urllib3.contrib.pyopenssl  # pylint: disable=import-error
+    urllib3.contrib.pyopenssl.inject_into_urllib3()
 
 DEFAULT_NETWORK_TIMEOUT = 45
 
