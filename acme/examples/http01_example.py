8272be0507c15eadbf37ee0f94ca5d88b615033d
Julie R <jroddeveloper@gmail.com>
2018-12-04 15:12:37 +0100

    Remove unnecessary license header
    This usage example is under the license for the acme package.

--- acme/examples/http01_example.py
+++ acme/examples/http01_example.py
@@ -1,19 +1,5 @@
 """Example ACME-V2 API for HTTP-01 challenge.
 
-Copyright 2018 Intra2net AG - Juliana Rodrigueiro
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
 Brief:
 
 This a complete usage example of the python-acme API.
