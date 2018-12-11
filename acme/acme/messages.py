0fa656ac1fead79e199272524b734b696ac97c9a
Adrien Ferrand <ferrand.ad@gmail.com>
2018-12-11 22:10:41 +0100

    Correct lint

--- acme/acme/messages.py
+++ acme/acme/messages.py
@@ -2,7 +2,7 @@
 import six
 import json
 try:
-    from collections.abc import Hashable
+    from collections.abc import Hashable  # pylint: disable=no-name-in-module
 except ImportError:
     from collections import Hashable
 
