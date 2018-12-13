00f75e851533f15bc8e05934da3e4a5aec5ef112
Erica Portnoy <ebportnoy@gmail.com>
2018-12-13 21:23:01 +0000

    Pin all dependency installation in the release script

--- tools/_release.sh
+++ tools/_release.sh
@@ -143,7 +143,7 @@ pip install -U pip
 # (or our dependencies) have conditional dependencies implemented with if
 # statements in setup.py and we have cached wheels lying around that would
 # cause those ifs to not be evaluated.
-pip install \
+python ../tools/pip_install.py \
   --no-cache-dir \
   --extra-index-url http://localhost:$PORT \
   $SUBPKGS
