e8e5b2253a72b28d91a4bf6cd6f1c83f87409e3c
Brad Warren <bmw@eff.org>
2018-12-14 10:30:04 -0800

    Don't use __import__.

--- acme/acme/__init__.py
+++ acme/acme/__init__.py
@@ -12,13 +12,14 @@ supported version: `draft-ietf-acme-01`_.
 """
 import sys
 
+import josepy
+
 # This code exists to keep backwards compatibility with people using acme.jose
 # before it became the standalone josepy package.
 #
 # It is based on
 # https://github.com/requests/requests/blob/1278ecdf71a312dc2268f3bfc0aabfab3c006dcf/requests/packages.py
 
-jose = __import__('josepy')
 for mod in list(sys.modules):
     # This traversal is apparently necessary such that the identities are
     # preserved (acme.jose.* is josepy.*)
