856bfe354427203136161e037fad92cdb1fa03b3
Adrien Ferrand <adferrand@users.noreply.github.com>
2018-12-19 01:17:54 +0100

    Tag to disable random sleep upon a renew task (#6599)
    * Extraction from #6541 to add flag to disable shuffle sleep on renew action
    
    * Move the logic of random sleep to execute it only if there is effectively a certificate to renew.
    
    * Add comments
    
    * Correct lint
    
    * Suspend lint rule
    
    * Revert code cleaning
    
    * Hide the flag
    
    * Ignore lint
    
    * Update cli.py

--- certbot/cli.py
+++ certbot/cli.py
@@ -1216,6 +1216,10 @@ def prepare_and_parse_args(plugins, args, detect_defaults=False):  # pylint: dis
     helpful.add("renew", "--renew-hook",
                 action=_RenewHookAction, help=argparse.SUPPRESS)
     helpful.add(
+        "renew", "--no-random-sleep-on-renew", action="store_false",
+        default=flag_default("random_sleep_on_renew"), dest="random_sleep_on_renew",
+        help=argparse.SUPPRESS)
+    helpful.add(
         "renew", "--deploy-hook", action=_DeployHookAction,
         help='Command to be run in a shell once for each successfully'
         ' issued certificate. For this command, the shell variable'
