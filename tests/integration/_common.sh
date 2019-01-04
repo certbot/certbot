8febe3129f750a23a20f96fe8921014c7fe77e99
Brad Warren <bmw@eff.org>
2019-01-04 09:37:36 -0800

    Add version_at_least().

--- tests/integration/_common.sh
+++ tests/integration/_common.sh
@@ -9,11 +9,30 @@ tls_sni_01_port=5001
 http_01_port=5002
 sources="acme/,$(ls -dm certbot*/ | tr -d ' \n')"
 export root config_dir store_flags tls_sni_01_port http_01_port sources
+certbot_path="$(command -v certbot)"
 
 certbot_test () {
     certbot_test_no_force_renew \
         --renew-by-default \
         "$@"
+}
+
+# Checks that the Certbot version is at least the given version number. This is
+# useful for making sure Certbot has certain features available. The patch
+# version is currently ignored. You use the exit code of this function to
+# determine the result of the comparison.
+#
+# Arguments:
+#   First argument is the minimum major version
+#   Second argument is the minimum minor version
+version_at_least () {
+    # Certbot major and minor version (e.g. 0.30)
+    major_minor=$("$certbot_path" --version 2>&1 | cut -d' ' -f2 | cut -d. -f1,2)
+    major=$(echo "$major_minor" | cut -d. -f1)
+    minor=$(echo "$major_minor" | cut -d. -f2)
+    # Test that either the major version is greater or major version is equal
+    # and minor version is greater than or equal to.
+    [ \( "$major" -gt "$1" \) -o \( "$major" -eq "$1" -a "$minor" -ge "$2" \) ]
 }
 
 # Use local ACMEv2 endpoint if requested and SERVER isn't already set.
@@ -30,7 +49,7 @@ certbot_test_no_force_renew () {
         --append \
         --source $sources \
         --omit $omit_patterns \
-        $(command -v certbot) \
+        "$certbot_path" \
             --server "${SERVER:-http://localhost:4000/directory}" \
             --no-verify-ssl \
             --tls-sni-01-port $tls_sni_01_port \
