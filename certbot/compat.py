545f61dddde40d3fea430f7aaaeee957951dcb4e
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2019-01-25 09:03:08 +0000

    Add support for FreeBSD specific paths
    Reference:	https://svnweb.freebsd.org/ports/head/security/py-certbot/files/patch-certbot_compat.py

--- certbot/compat.py
+++ certbot/compat.py
@@ -184,6 +184,12 @@ LINUX_DEFAULT_FOLDERS = {
     'logs': '/var/log/letsencrypt',
 }
 
+FREEBSD_DEFAULT_FOLDERS = {
+    'config': '/usr/local/etc/letsencrypt',
+    'work': '/var/db/letsencrypt',
+    'logs': '/var/log/letsencrypt',
+}
+
 def get_default_folder(folder_type):
     """
     Return the relevant default folder for the current OS
@@ -195,7 +201,13 @@ def get_default_folder(folder_type):
 
     """
     if 'fcntl' in sys.modules:
-        # Linux specific
-        return LINUX_DEFAULT_FOLDERS[folder_type]
-    # Windows specific
-    return WINDOWS_DEFAULT_FOLDERS[folder_type]
+        # Unix-like
+        if sys.platform.startswith('freebsd') or sys.platform.startswith('dragonfly'):
+            # FreeBSD specific
+            return FREEBSD_DEFAULT_FOLDERS[folder_type]
+        else:
+            # Linux specific
+            return LINUX_DEFAULT_FOLDERS[folder_type]
+    else:
+        # Windows specific
+        return WINDOWS_DEFAULT_FOLDERS[folder_type]
