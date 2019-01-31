5f79f0fdbc356c49dfe8a8a7314cdc0bc6ef3acd
Joona Hoikkala <joona@kuori.org>
2019-01-31 19:10:09 +0200

    Select all matching VirtualHosts for HTTP-01 challenges instead of just one

--- certbot-apache/certbot_apache/http_01.py
+++ certbot-apache/certbot_apache/http_01.py
@@ -89,15 +89,28 @@ class ApacheHttp01(common.TLSSNI01):
                     self.configurator.enable_mod(mod, temp=True)
 
     def _mod_config(self):
+        selected_vhosts = []
+        http_port = str(self.configurator.config.http01_port)
         for chall in self.achalls:
-            vh = self.configurator.find_best_http_vhost(
-                chall.domain, filter_defaults=False,
-                port=str(self.configurator.config.http01_port))
-            if vh:
-                self._set_up_include_directives(vh)
-            else:
-                for vh in self._relevant_vhosts():
-                    self._set_up_include_directives(vh)
+            # Search for matching VirtualHosts
+            for vh in self._matching_vhosts(chall.domain):
+                if vh not in selected_vhosts:
+                    selected_vhosts.append(vh)
+
+        # Ensure that we have one or more VirtualHosts that we can continue
+        # with. (one that listens to port configured with --http-01-port)
+        found = False
+        for vhost in selected_vhosts:
+            if any(a.is_wildcard() or a.get_port() == http_port for a in vhost.addrs):
+                found = True
+
+        if not found:
+            for vh in self._relevant_vhosts():
+                selected_vhosts.append(vh)
+
+        # Add the challenge configuration
+        for vh in selected_vhosts:
+            self._set_up_include_directives(vh)
 
         self.configurator.reverter.register_file_creation(
             True, self.challenge_conf_pre)
@@ -120,6 +133,20 @@ class ApacheHttp01(common.TLSSNI01):
         logger.debug("writing a post config file with text:\n %s", config_text_post)
         with open(self.challenge_conf_post, "w") as new_conf:
             new_conf.write(config_text_post)
+
+    def _matching_vhosts(self, domain):
+        """Return all VirtualHost objects that have the requested domain name or
+        a wildcard name that would match the domain in ServerName or ServerAlias
+        directive.
+        """
+        matching_vhosts = []
+        for vhost in self.configurator.vhosts:
+            if self.configurator.included_in_wildcard(vhost.get_names(), domain):
+                matching_vhosts.append(vhost)
+            elif domain in vhost.get_names():
+                matching_vhosts.append(vhost)
+
+        return matching_vhosts
 
     def _relevant_vhosts(self):
         http01_port = str(self.configurator.config.http01_port)
