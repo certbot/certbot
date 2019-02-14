3da3df0039db7d8dab3fe97be1fd23c88ff60b49
Brad Warren <bmw@eff.org>
2019-02-14 14:51:33 -0800

    complexity--

--- tests/letstest/scripts/test_tests.sh
+++ tests/letstest/scripts/test_tests.sh
@@ -4,18 +4,17 @@ REPO_ROOT="letsencrypt"
 LE_AUTO="$REPO_ROOT/letsencrypt-auto-source/letsencrypt-auto"
 LE_AUTO="$LE_AUTO --debug --no-self-upgrade --non-interactive"
 MODULES="acme certbot certbot_apache certbot_nginx"
+PIP_INSTALL="$REPO_ROOT/tools/pip_install.py"
 VENV_NAME=venv
 
 # *-auto respects VENV_PATH
 $LE_AUTO --os-packages-only
 LE_AUTO_SUDO="" VENV_PATH="$VENV_NAME" $LE_AUTO --no-bootstrap --version
 . $VENV_NAME/bin/activate
-
-PIP_INSTALL=$("$REPO_ROOT/tools/readlink.py" "$REPO_ROOT/tools/pip_install.py")
+"$PIP_INSTALL" pytest
 
 # change to an empty directory to ensure CWD doesn't affect tests
 cd $(mktemp -d)
-"$PIP_INSTALL" pytest
 
 for module in $MODULES ; do
     echo testing $module
