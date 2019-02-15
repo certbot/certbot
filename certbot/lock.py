6b36b7bb9ea6bb4f57898ad6f472f1ac3631d4cb
Adrien Ferrand <adferrand@users.noreply.github.com>
2019-02-15 01:17:14 +0100

    Update lock.py

--- certbot/lock.py
+++ certbot/lock.py
@@ -205,17 +205,17 @@ class _UnixLockMechanism(_BaseLockMechanism):
 class _WindowsLockMechanism(_BaseLockMechanism):
     """
     A Windows lock file mechanism.
-    By default on Windows, acquiring a file handler give exclusive access to the process
-    and result in an effective lock. However, it is possible to explicitly acquire the
+    By default on Windows, acquiring a file handler gives exclusive access to the process
+    and results in an effective lock. However, it is possible to explicitly acquire the
     file handler in shared access in terms of read and write, and this is done by os.open
     and io.open in Python. So an explicit lock needs to be done through the call of
     msvcrt.locking, that will lock the first byte of the file. In theory, it is also
-    possible to access a file in shared delete access, allowing other process to delete an
-    opened file. But this needs also be done explicitly by all processes using the Windows
+    possible to access a file in shared delete access, allowing other processes to delete an
+    opened file. But this needs also to be done explicitly by all processes using the Windows
     low level APIs, and Python does not do it. As of Python 3.7 and below, Python developers
     state that deleting a file opened by a process from another process is not possible with
-    os.open and io.open is not possible.
-    Consequently, mscvrt.locking is sufficient to have an effective lock, and the race
+    os.open and io.open.
+    Consequently, mscvrt.locking is sufficient to obtain an effective lock, and the race
     condition encountered on Linux is not possible on Windows, leading to a simpler workflow.
     """
     def acquire(self):
