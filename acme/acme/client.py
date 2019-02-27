18f218470773c5c2d7a5eb6e4eebb7cbb21eb405
Brad Warren <bmw@users.noreply.github.com>
2019-02-27 01:44:57 +0100

    Update acme/acme/client.py
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>

--- acme/acme/client.py
+++ acme/acme/client.py
@@ -157,7 +157,7 @@ class ClientBase(object):  # pylint: disable=too-many-instance-attributes
 
         """
         # Because sending keyAuthorization in a response challenge has been removed from the ACME
-        # specs, it is not included in the KeyAuthorizationResponseChallenge JSON by default.
+        # spec, it is not included in the KeyAuthorizationResponseChallenge JSON by default.
         # However as a migration path, we temporarily expect a malformed error from the server,
         # and fallback by resending the challenge response with the keyAuthorization field.
         # TODO: Remove this fallback for Certbot 0.34.0
