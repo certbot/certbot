841f8efd0aa7dc2ba249b303e6be664d0a6647e3
Adrien Ferrand <adferrand@users.noreply.github.com>
2019-03-01 22:18:06 +0100

    [Unix] Create a framework for certbot integration tests: PART 1 (#6578)
    * First part
    
    * Several optimizations about the docker env setup
    
    * Documentation
    
    * Various corrections and documentation. Add acme and certbot explicitly as dependencies of certbot-ci.
    
    * Correct a variable misinterpreted as a pytest hook
    
    * Correct strict parsing option on pebble
    
    * Refactor acme setup to be executed from pytest hooks.
    
    * Pass TRAVIS env variable to trigger specific xdist logic
    
    * Retrigger build.
    
    * Work in progress
    
    * Config operational
    
    * Propagate to xdist
    
    * Corrections on acme and misc
    
    * Correct subnet for pebble
    
    * Remove gobetween, as tls-sni challenges are not tested anymore.
    
    * Improve pebble setup. Reduce LOC.
    
    * Update acme.py
    
    * Optimize acme ca setup, with less temporary assets
    
    * Silent setup
    
    * Clean code
    
    * Remove unused workspace
    
    * Use default network driver
    
    * Remove bridge
    
    * Update package documentation
    
    * Remove rerun capability for integration tests, not needed.
    
    * Add documentation
    
    * Variable for all ports and subnets used by the stack
    
    * Update certbot-ci/certbot_integration_tests/conftest.py
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Update certbot-ci/certbot_integration_tests/utils/acme.py
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Update certbot-ci/certbot_integration_tests/utils/misc.py
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Update tox.ini
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Update certbot-ci/certbot_integration_tests/utils/misc.py
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Update certbot-ci/certbot_integration_tests/utils/acme.py
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Update certbot-ci/certbot_integration_tests/utils/acme.py
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Update certbot-ci/certbot_integration_tests/conftest.py
    
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>
    
    * Rename to acme_server
    
    * Add comment
    
    * Refactor in a unique context fixture
    
    * Remove the need of CERTBOT_ACME_XDIST environment variable
    
    * Remove nonstrict/strict options in pebble
    
    * Clean dependencies
    
    * Clean tox
    
    * Change function name
    
    * Add comment about coveragerc specificities
    
    * Change a comment.
    
    * Update setup.py
    
    * Update conftest.py
    
    * Use the production-ready docker-compose.yml file for Pebble
    
    * New style class
    
    * Tune pebble to have a stable test environment
    
    * Pin a dependency

---
+++ certbot-ci/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+version = '0.32.0.dev0'
+
+install_requires = [
+    'pytest',
+    'pytest-cov',
+    'pytest-xdist',
+    'pytest-sugar',
+    'coverage',
+    'requests',
+    'pyyaml',
+]
+
+setup(
+    name='certbot-ci',
+    version=version,
+    description="Certbot continuous integration framework",
+    url='https://github.com/certbot/certbot',
+    author="Certbot Project",
+    author_email='client-dev@letsencrypt.org',
+    license='Apache License 2.0',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+    ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+)
