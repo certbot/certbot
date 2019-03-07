8045f210758f4f882f503f9762c3b21918c5bceb
Adrien Ferrand <ferrand.ad@gmail.com>
2019-03-07 17:24:04 +0100

    Polish CLI

--- letsencrypt-auto-source/rebuild_dependencies.py
+++ letsencrypt-auto-source/rebuild_dependencies.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 import tempfile
 import os
+import sys
 
 TEST = [
     'asn1crypto==0.23.0\ncertifi==2018.11.29\ncffi==1.12.2\nchardet==3.0.4\nConfigArgParse==0.14.0\nconfigobj==5.0.6\ncryptography==2.6.1\nenum34==1.1.6\nfuncsigs==1.0.2\nfuture==0.17.1\nidna==2.8\nipaddress==1.0.22\njosepy==1.1.0\nmock==2.0.0\nparsedatetime==2.4\npbr==5.1.3\npycparser==2.19\npyOpenSSL==19.0.0\npyparsing==2.3.1\npyRFC3339==1.1\npython-augeas==1.0.3\npytz==2018.9\nrequests==2.21.0\nrequests-toolbelt==0.9.1\nsix==1.12.0\nurllib3==1.24.1\nzope.component==4.5\nzope.deferredimport==4.3\nzope.deprecation==4.4.0\nzope.event==4.4\nzope.hookable==4.2.0\nzope.interface==4.6.0\nzope.proxy==4.3.1\n',
@@ -25,25 +26,22 @@ SCRIPT = """\
 #!/bin/sh
 
 cd /tmp/certbot
-letsencrypt-auto-source/letsencrypt-auto --os-packages-only -n
+letsencrypt-auto-source/letsencrypt-auto --install-only -n
+PYVER=`/opt/eff.org/certbot/venv/bin/python --version 2>&1 | cut -d" " -f 2 | cut -d. -f1,2 | sed 's/\.//'`
 
-python2 letsencrypt-auto-source/pieces/create_venv.py /tmp/venv "27" "1"
+letsencrypt-auto-source/pieces/create_venv.py /tmp/venv "$PYVER" "1"
+
 /tmp/venv/bin/python letsencrypt-auto-source/pieces/pipstrap.py
 /tmp/venv/bin/pip install certbot-nginx certbot-apache
 /tmp/venv/bin/certbot --version
-/tmp/venv/bin/pip freeze >> /tmp/workspace/results_2
-
-#python3 letsencrypt-auto-source/pieces/create_venv.py /tmp/venv "37" "1"
-#/tmp/venv3/bin/python letsencrypt-auto-source/pieces/pipstrap.py
-#/tmp/venv3/bin/pip install certbot-nginx certbot-apache
-#/tmp/venv3/bin/pip freeze >> /tmp/workspace/results_3
-touch /tmp/workspace/results_3
+/tmp/venv/bin/pip freeze >> /tmp/workspace/results
 """
 
 DEPENDENCY_PATTERN = r'(.*)==(.*)'
 
 
 def process_one_distribution(distribution):
+    print('===> Gathering dependencies for {0}.'.format(distribution))
     workspace = tempfile.mkdtemp()
     script = os.path.join(workspace, 'script.sh')
     try:
@@ -55,9 +53,8 @@ def process_one_distribution(distribution):
         command = ['docker', 'run', '--rm', '-v', '{0}:/tmp/certbot'.format(CERTBOT_REPO_PATH),
                    '-v', '{0}:/tmp/workspace'.format(workspace), distribution, '/tmp/workspace/script.sh']
         subprocess.check_call(command)
-        with open(os.path.join(workspace, 'results_2'), 'r') as file_handler_2:
-            with open(os.path.join(workspace, 'results_3'), 'r') as file_handler_3:
-                return file_handler_2.read(), file_handler_3.read()
+        with open(os.path.join(workspace, 'results'), 'r') as file_handler:
+            return file_handler.read()
     finally:
         shutil.rmtree(workspace)
 
@@ -73,6 +70,7 @@ def insert_results(dependencies_map, results, distribution):
 
 
 def process_dependency_map(dependency_map):
+    print('===> Processing the dependency map.')
     requirements = []
     conflicts = []
     for package, versions in dependency_map.items():
@@ -82,6 +80,7 @@ def process_dependency_map(dependency_map):
             version_list = ['{0} ({1})'.format(version, ','.join(distributions)) for version, distributions in reduced_versions.items()]
             conflict = 'package {0} is declared with several versions: {1}'.format(package, ', '.join(version_list))
             conflicts.append(conflict)
+            sys.stderr.write('ERROR: {0}\n'.format(conflict))
         else:
             requirements.append((package, reduced_versions.keys()[0]))
 
@@ -97,34 +96,42 @@ def reduce_versions(versions):
     return version_map
 
 
-def print_requirements(requirements, conflicts):
-    temp_requirement = tempfile.mkstemp()[1]
-    try:
-        for req in requirements:
-            subprocess.check_call(['hashin', '{0}=={1}'.format(req[0], req[1]), '--requirements-file', temp_requirement])
+def write_requirements(dest_file, requirements, conflicts):
+    print('===> Calculating hashes for the requirement file.')
+    if os.path.exists(dest_file):
+        os.remove(dest_file)
+    open(dest_file, 'w').close()
+    for req in requirements:
+        subprocess.check_call(['hashin', '{0}=={1}'.format(req[0], req[1]), '--requirements-file', dest_file])
 
-        if conflicts:
-            with open(temp_requirement, 'a') as file_handler:
-                file_handler.write('\n## ! SOME ERRORS OCCURRED ! ##\n')
-                file_handler.write('\n'.join('# {0}'.format(conflict) for conflict in conflicts))
+    if conflicts:
+        with open(dest_file, 'a') as file_handler:
+            file_handler.write('\n## ! SOME ERRORS OCCURRED WHILE REBUILDING THE REQUIREMENT FILE ! ##\n')
+            file_handler.write('\n'.join('# {0}'.format(conflict) for conflict in conflicts))
+            file_handler.write('\n')
 
-        with open(temp_requirement, 'r') as file_handler:
-            print(file_handler.read())
-    finally:
-        os.remove(temp_requirement)
+    with open(dest_file, 'r') as file_handler:
+        print(file_handler.read())
 
 
-def main():
+def main(dest_file):
     dependencies_map = {}
     for index, distribution in enumerate(DISTRIBUTION_LIST):
         #results = process_one_distribution(distribution)
-        results = (TEST[index], '')
-        insert_results(dependencies_map, results[0], distribution)
+        results = TEST[index]
+        insert_results(dependencies_map, results, distribution)
 
     requirements, conflicts = process_dependency_map(dependencies_map)
 
-    print_requirements(requirements, conflicts)
+    write_requirements(dest_file, requirements, conflicts)
+
+    dest_file_abs = dest_file if os.path.isabs(dest_file) else os.path.abspath(dest_file)
+    print('===> Rebuilt requirement file is available on path {0}'.format(dest_file_abs))
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        destination_file = sys.argv[1]
+    except IndexError:
+        raise AttributeError('No destination requirement file provided to the command line.')
+    main(destination_file)
