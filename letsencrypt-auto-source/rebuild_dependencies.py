a89083ba3d43df2a06b485179cc47d60ba13dd98
Adrien Ferrand <ferrand.ad@gmail.com>
2019-03-10 23:40:21 +0100

    Add constraints. Clean code.

--- letsencrypt-auto-source/rebuild_dependencies.py
+++ letsencrypt-auto-source/rebuild_dependencies.py
@@ -22,10 +22,7 @@ import tempfile
 import os
 import sys
 
-# Library hashin is not used as a module in this script, but this import is
-# useful to fail fast the script if it is not installed.
-#import hashin
-
+# The list of docker distributions to test dependencies against with.
 DISTRIBUTION_LIST = [
     'ubuntu:18.04', 'ubuntu:14.04',
     'debian:stretch', 'debian:jessie',
@@ -34,11 +31,25 @@ DISTRIBUTION_LIST = [
     'fedora:29',
 ]
 
+# Theses dependencies will be added unconditionally on the output requirement files,
+# and are also used as constraints while gathering dependencies on each distribution.
+AUTHORITATIVE_REQUIREMENTS = {
+    # Using an older version of mock here prevents regressions of #5276.
+    'mock': '1.3.0'
+}
+
 
 # ./certbot/letsencrypt-auto-source/rebuild_dependencies.py (2 levels from certbot root path)
 CERTBOT_REPO_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(CERTBOT_REPO_PATH)
 
+# The script will be used to gathering dependencies for a given distribution.
+#   - certbot-auto is used to install relevant OS packages, and setup an initial venv
+#   - then this venv is used to consistently construct an empty new venv
+#   - once pipstraped, this new venv pip install certbot runtime (including apache/nginx),
+#     without pinned dependencies, and in respect with input authoritative requirements
+#   - certbot --version is called to check we have an healthy environment
+#   - finally current set of dependencies is extracted out of the docker using pip freeze
 SCRIPT = """\
 #!/bin/sh
 set -e
@@ -50,48 +61,49 @@ PYVER=`/opt/eff.org/certbot/venv/bin/python --version 2>&1 | cut -d" " -f 2 | cu
 /opt/eff.org/certbot/venv/bin/python letsencrypt-auto-source/pieces/create_venv.py /tmp/venv "$PYVER" "1"
 
 /tmp/venv/bin/python letsencrypt-auto-source/pieces/pipstrap.py
-/tmp/venv/bin/pip install -e acme -e . -e certbot-apache -e certbot-nginx
+/tmp/venv/bin/pip install -e acme -e . -e certbot-apache -e certbot-nginx -c /tmp/requirements.txt
 /tmp/venv/bin/certbot --version
 /tmp/venv/bin/pip freeze >> /tmp/workspace/results
 """
 
-OUTPUT_HEADER = """\
-# This is the flattened list of packages certbot-auto installs.
-# To generate this, do (with docker and package hashin installed):
-# ```
-# letsencrypt-auto-source/rebuild_dependencies.py \
-#   letsencrypt-auto-sources/pieces/dependency-requirements.txt
-# ```
-"""
 
-DEPENDENCY_PATTERN = r'(.*)==(.*)'
-
-
-def process_one_distribution(distribution):
+def _process_one_distribution(distribution):
     print('===> Gathering dependencies for {0}.'.format(distribution))
     workspace = tempfile.mkdtemp()
     script = os.path.join(workspace, 'script.sh')
+    authoritative_requirements = os.path.join(workspace, 'requirements.txt')
     try:
-        with open(script, 'w') as file_handler:
-            file_handler.write(SCRIPT)
+        with open(script, 'w') as file_h:
+            file_h.write(SCRIPT)
         os.chmod(script, 0o755)
-        command = ['docker', 'run', '--rm', '-v', '{0}:/tmp/certbot'.format(CERTBOT_REPO_PATH),
-                   '-v', '{0}:/tmp/workspace'.format(workspace), distribution, '/tmp/workspace/script.sh']
+
+        with open(authoritative_requirements, 'w') as file_h:
+            file_h.write('\n'.join(['{0}=={1}'.format(package, version)
+                                    for package, version in AUTHORITATIVE_REQUIREMENTS.items()]))
+
+        command = ['docker', 'run', '--rm',
+                   '-v', '{0}:/tmp/certbot'.format(CERTBOT_REPO_PATH),
+                   '-v', '{0}:/tmp/workspace'.format(workspace),
+                   '-v', '{0}:/tmp/requirements.txt'.format(authoritative_requirements),
+                   distribution, '/tmp/workspace/script.sh']
         subprocess.check_output(command, stderr=subprocess.STDOUT)
-        with open(os.path.join(workspace, 'results'), 'r') as file_handler:
-            return file_handler.read()
+
+        with open(os.path.join(workspace, 'results'), 'r') as file_h:
+            return file_h.read()
     except subprocess.CalledProcessError as error:
-        sys.stderr.write('Error while gathering dependencies for {0}.\n'.format(distribution))
-        sys.stderr.write('Output was:\n')
-        sys.stderr.write(error.output)
+        sys.stderr.write('''\
+Error while gathering dependencies for {0}.
+Output was:
+{1}
+'''.format(distribution, error.output))
     finally:
         shutil.rmtree(workspace)
 
 
-def insert_results(dependencies_map, results, distribution):
+def _insert_results(dependencies_map, results, distribution):
     refined_results = []
     for result in results.split(os.linesep):
-        match = re.match(DEPENDENCY_PATTERN, result)
+        match = re.match(r'(.*)==(.*)', result)
         if match:
             package = match.group(1)
             version = match.group(2)
@@ -101,16 +113,18 @@ def insert_results(dependencies_map, results, distribution):
     return refined_results
 
 
-def process_dependency_map(dependency_map):
+def _process_dependency_map(dependency_map):
     print('===> Processing the dependency map.')
     requirements = []
     conflicts = []
     for package, versions in dependency_map.items():
-        reduced_versions = reduce_versions(versions)
+        reduced_versions = _reduce_versions(versions)
 
         if len(reduced_versions) > 1:
-            version_list = ['{0} ({1})'.format(version, ','.join(distributions)) for version, distributions in reduced_versions.items()]
-            conflict = 'package {0} is declared with several versions: {1}'.format(package, ', '.join(version_list))
+            version_list = ['{0} ({1})'.format(version, ','.join(distributions))
+                            for version, distributions in reduced_versions.items()]
+            conflict = ('package {0} is declared with several versions: {1}'
+                        .format(package, ', '.join(version_list)))
             conflicts.append(conflict)
             sys.stderr.write('ERROR: {0}\n'.format(conflict))
         else:
@@ -120,7 +134,7 @@ def process_dependency_map(dependency_map):
     return requirements, conflicts
 
 
-def reduce_versions(versions):
+def _reduce_versions(versions):
     version_map = {}
     for version in versions:
         version_map.setdefault(version[0], []).append(version[1])
@@ -128,36 +142,45 @@ def reduce_versions(versions):
     return version_map
 
 
-def write_requirements(dest_file, requirements, conflicts):
+def _write_requirements(dest_file, requirements, conflicts):
     print('===> Calculating hashes for the requirement file.')
     if os.path.exists(dest_file):
         os.remove(dest_file)
 
-    with open(dest_file, 'w') as file_handler:
-        file_handler.write(OUTPUT_HEADER)
+    with open(dest_file, 'w') as file_h:
+        file_h.write('''\
+# This is the flattened list of packages certbot-auto installs.
+# To generate this, do (with docker and package hashin installed):
+# ```
+# letsencrypt-auto-source/rebuild_dependencies.py \
+#   letsencrypt-auto-sources/pieces/dependency-requirements.txt
+# ```
+''')
     for req in requirements:
-        subprocess.check_call(['hashin', '{0}=={1}'.format(req[0], req[1]), '--requirements-file', dest_file])
+        subprocess.check_call(['hashin', '{0}=={1}'.format(req[0], req[1]),
+                               '--requirements-file', dest_file])
 
     if conflicts:
-        with open(dest_file, 'a') as file_handler:
-            file_handler.write('\n## ! SOME ERRORS OCCURRED WHILE REBUILDING THE REQUIREMENT FILE ! ##\n')
-            file_handler.write('\n'.join('# {0}'.format(conflict) for conflict in conflicts))
-            file_handler.write('\n')
+        with open(dest_file, 'a') as file_h:
+            file_h.write('\n## ! SOME ERRORS OCCURRED ! ##\n')
+            file_h.write('\n'.join('# {0}'.format(conflict) for conflict in conflicts))
+            file_h.write('\n')
 
-    with open(dest_file, 'r') as file_handler:
-        print(file_handler.read())
+    with open(dest_file, 'r') as file_h:
+        print(file_h.read())
 
 
-def main(dest_file):
-    dependencies_map = {}
+def _gather_dependencies(dest_file):
+    dependencies_map = {package: (version, 'authoritative_requirement')
+                        for package, version in AUTHORITATIVE_REQUIREMENTS.items()}
 
     for distribution in DISTRIBUTION_LIST:
-        results = process_one_distribution(distribution)
-        insert_results(dependencies_map, results, distribution)
+        results = _process_one_distribution(distribution)
+        _insert_results(dependencies_map, results, distribution)
 
-    requirements, conflicts = process_dependency_map(dependencies_map)
+    requirements, conflicts = _process_dependency_map(dependencies_map)
 
-    write_requirements(dest_file, requirements, conflicts)
+    _write_requirements(dest_file, requirements, conflicts)
 
     dest_file_abs = dest_file if os.path.isabs(dest_file) else os.path.abspath(dest_file)
     print('===> Rebuilt requirement file is available on path {0}'.format(dest_file_abs))
@@ -168,4 +191,8 @@ if __name__ == '__main__':
         destination_file = sys.argv[1]
     except IndexError:
         raise AttributeError('No destination requirement file provided to the command line.')
-    main(destination_file)
+    try:
+        subprocess.check_call(['hashin', '--version'])
+    except subprocess.CalledProcessError:
+        raise RuntimeError('Hashin is not installed, please `pip install hashin`.')
+    _gather_dependencies(destination_file)
