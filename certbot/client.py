489c5f2f205502cc2cd5a0a81247c257a0b0c9f2
Adrien Ferrand <adferrand@users.noreply.github.com>
2019-03-28 23:18:09 +0100

    Update client.py

--- certbot/client.py
+++ certbot/client.py
@@ -16,7 +16,7 @@ from acme import client as acme_client
 from acme import crypto_util as acme_crypto_util
 from acme import errors as acme_errors
 from acme import messages
-from acme.magic_typing import Optional  # type: ignore  # pylint: disable=unused-import,no-name-in-module
+from acme.magic_typing import Optional  # pylint: disable=unused-import,no-name-in-module
 
 import certbot
 from certbot import account
