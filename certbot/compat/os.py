6e0f4288628db3d0ce53f471bd304814b184e553
Adrien Ferrand <ferrand.ad@gmail.com>
2019-03-29 01:23:45 +0100

    Import sys

--- certbot/compat/os.py
+++ certbot/compat/os.py
@@ -3,17 +3,18 @@ This compat modules extends native capabilities of core os module to handle corr
 specific operations (eg. chown, chmod, geuid).
 This module is intended to replace standard os module throughout certbot projects (except acme).
 """
-from __future__ import absolute_import
+from __future__ import absolute_import as std_absolute_import
 
 # Expose everything from standard os package to make current package a complete replacement of os.
 from os import *  # type: ignore  # pylint: disable=wildcard-import,unused-wildcard-import,redefined-builtin
 
 # Monkey patch ourselves to get os attributes that are not in __all__ (so not from os import *).
 import os as std_os
-ourselves = sys.modules[__name__]
+import sys as std_sys
+ourselves = std_sys.modules[__name__]
 for attribute in dir(std_os):
     if not hasattr(ourselves, attribute):
         setattr(ourselves, attribute, getattr(std_os, attribute))
 
 # Clean all remaining importables that are not from the core os module.
-del ourselves, std_os, absolute_import
+del ourselves, std_os, std_sys, std_absolute_import
