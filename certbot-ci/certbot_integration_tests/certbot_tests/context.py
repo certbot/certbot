53230146eb47f081c8eb096de7dc7103d8e207f9
ohemorange <ebportnoy@gmail.com>
2019-03-30 01:33:03 +0100

    Update certbot-ci/certbot_integration_tests/certbot_tests/context.py
    Co-Authored-By: adferrand <adferrand@users.noreply.github.com>

--- certbot-ci/certbot_integration_tests/certbot_tests/context.py
+++ certbot-ci/certbot_integration_tests/certbot_tests/context.py
@@ -130,7 +130,7 @@ class IntegrationTestsContext(object):
         Generate a certificate domain name suitable for distributed certbot integration tests.
         This is a requirement to let the distribution know how to redirect the challenge check
         from the ACME server to the relevant pytest-xdist worker. This resolution is done by
-        appending the pytest worker id to the domain, using this pattern:
+        appending the pytest worker id to the subdomain, using this pattern:
         {subdomain}.{worker_id}.wtf
         :param subdomain: the subdomain to use in the generated domain (default 'le')
         :return: the well-formed domain suitable for redirection on 
