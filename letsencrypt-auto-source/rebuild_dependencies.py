b1de228e7a42fbbf7dc4369279d81f6fcaae9473
Adrien Ferrand <ferrand.ad@gmail.com>
2019-04-03 00:36:19 +0200

    Add the environment marker back. Ensure that it will stay by adding an override to dependencies generator.

--- letsencrypt-auto-source/rebuild_dependencies.py
+++ letsencrypt-auto-source/rebuild_dependencies.py
@@ -42,6 +42,9 @@ AUTHORITATIVE_CONSTRAINTS = {
     # Too touchy to move to a new version. And will be removed soon
     # in favor of pure python parser for Apache.
     'python-augeas': '0.5.0',
+    # Package enum34 needs to be explicitly limited to Python2.x, in order to avoid
+    # certbot-auto to fail on Centos 6 + Python 3.6, see #5456.
+    'enum34': '1.1.6 ; python_version < \'3.4\''
 }
 
 
