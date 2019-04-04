c32b57607f79db1aab35ba6682d8bc947dc5a21e
Brad Warren <bmw@eff.org>
2019-04-04 15:24:43 -0700

    Release 0.33.1

--- certbot-dns-linode/setup.py
+++ certbot-dns-linode/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.33.0'
+version = '0.33.1'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
