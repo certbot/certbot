8d71c7819fa26b42e60917a50e5612d6aec59e7f
Joey Staehle <jwstaehle@gmail.com>
2019-04-08 13:15:46 -0400

    dnsimple 3.2.1
    In order to pick up https://github.com/AnalogJ/lexicon/pull/389 which fixes a url construction issue

--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -9,7 +9,7 @@ version = '0.34.0.dev0'
 install_requires = [
     'acme>=0.31.0',
     'certbot>=0.31.0',
-    'dns-lexicon>=2.2.1',  # Support for >1 TXT record per name
+    'dns-lexicon>=3.2.1',  # Support for >1 TXT record per name
     'mock',
     'setuptools',
     'zope.interface',
