dc475e0e9850239870bcd10b6a34a8df200b6831
Adrien Ferrand <adrien.ferrand@eff.org>
2019-04-09 22:05:14 +0200

    Implement the new linter_plugin

--- linter_plugin.py
+++ linter_plugin.py
@@ -1,31 +1,52 @@
-"""Certbot ACME PyLint plugin.
-
+"""Certbot PyLint plugin.
 http://docs.pylint.org/plugins.html
-
 """
-from astroid import MANAGER
-from astroid import nodes
+from pylint.checkers import BaseChecker
+from pylint.interfaces import IAstroidChecker
+from pylint.checkers.utils import check_messages
 
 
-def register(unused_linter):
-    """Register this module as PyLint plugin."""
-    pass
+# Module in theses packages can import os module.
+WHITELIST_PACKAGES = ['acme', 'certbot_compatibility_test', 'letshelp_certbot', 'lock_test']
 
 
-def _transform(cls):
-    # fix the "no-member" error on instances of
-    # letsencrypt.acme.util.ImmutableMap subclasses (instance
-    # attributes are initialized dynamically based on __slots__)
+class ForbidStandardOsModule(BaseChecker):
+    """
+    This checker ensures that standard os module is not imported by certbot classes.
+    Otherwise a 'os-module-forbidden' error will be registered for the faulty lines.
+    """
+    __implements__ = IAstroidChecker
 
-    # TODO: this is too broad and applies to any tested class...
+    name = 'forbid-os-module'
+    msgs = {
+        'E5001': (
+            'Forbidden use of os module, certbot.compat.os must be used instead',
+            'os-module-forbidden',
+            'Some methods from standard os modules cannot be used for security reasons on Windows: '
+            'the safe wrapper certbot.compat.os must be used instead in Certbot.'
+        )
+    }
+    priority = -1
 
-    #if cls.slots() is not None:
-    #    for slot in cls.slots():
-    #        cls.locals[slot.value] = [nodes.EmptyNode()]
+    # TODO: exclude acme module from the check
+    @check_messages('os-module-forbidden')
+    def visit_import(self, node):
+        if 'os' in [name[0] for name in node.names] and not _check_disabled(node):
+            self.add_message('os-module-forbidden', node=node)
 
-    if cls.name == 'JSONObjectWithFields':
-        # _fields is magically introduced by JSONObjectWithFieldsMeta
-        cls.locals['_fields'] = [nodes.EmptyNode()]
+    # TODO: exclude acme module from the check
+    @check_messages('os-module-forbidden')
+    def visit_importfrom(self, node):
+        if node.modname == 'os' and not _check_disabled(node):
+            self.add_message('os-module-forbidden', node=node)
 
 
-MANAGER.register_transform(nodes.Class, _transform)
+def register(linter):
+    """Register this module as a Pylint plugin."""
+    linter.register_checker(ForbidStandardOsModule(linter))
+
+
+def _check_disabled(node):
+    module = node.root()
+    return any([package for package in WHITELIST_PACKAGES
+                if module.name.startswith(package + '.') or module.name == package])
