3019f732f76984ba4d5170bfd892ac9d0cc54ef5
Adrien Ferrand <ferrand.ad@gmail.com>
2019-04-24 17:39:43 +0200

    Protect against import error

--- certbot-ci/certbot_integration_tests/utils/misc.py
+++ certbot-ci/certbot_integration_tests/utils/misc.py
@@ -36,8 +36,11 @@ def check_until_timeout(url):
     :param str url: the URL to test
     :raise ValueError: exception raised after 150 unsuccessful attempts to reach the URL
     """
-    import urllib3
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    try:
+        import urllib3
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    except ImportError:
+        pass
 
     for _ in range(0, 150):
         time.sleep(1)
