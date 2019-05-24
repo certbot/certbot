e10b96120b10c0659624a06cc355e93c4f13fbe1
Adrien Ferrand <ferrand.ad@gmail.com>
2019-05-24 14:35:34 +0200

    Add several cases of ocsp responder. More factories for the resilience tests.

--- certbot/tests/ocsp_test.py
+++ certbot/tests/ocsp_test.py
@@ -1,5 +1,6 @@
 """Tests for ocsp.py"""
 # pylint: disable=protected-access
+import contextlib
 import unittest
 from datetime import datetime, timedelta
 
@@ -139,116 +140,131 @@ class OSCPTestCryptography(unittest.TestCase):
 
         mock_revoke.assert_called_once_with(self.cert_path, self.chain_path, 'http://example.com')
 
-    @mock.patch('certbot.ocsp.requests.post')
-    @mock.patch('certbot.ocsp.ocsp.load_der_ocsp_response')
-    def test_revoke(self, mock_ocsp_response, mock_post):
-        with mock.patch('certbot.ocsp.crypto_util.verify_signed_payload'):
-            mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-                ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-            mock_post.return_value = mock.Mock(status_code=200)
+    def test_revoke(self):
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL):
             revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
         self.assertTrue(revoked)
 
-    @mock.patch('certbot.ocsp.crypto_util.verify_signed_payload')
-    @mock.patch('certbot.ocsp.requests.post')
-    @mock.patch('certbot.ocsp.ocsp.load_der_ocsp_response')
-    def test_responder_signing(self, mock_ocsp_response, mock_post, mock_check):
-        response = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-        mock_ocsp_response.return_value = response
-        mock_post.return_value = mock.Mock(status_code=200)
+    def test_responder_is_issuer(self):
+        issuer = x509.load_pem_x509_certificate(
+            test_util.load_vector('ocsp_issuer_certificate.pem'), default_backend())
 
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED,
+                        ocsp_lib.OCSPResponseStatus.SUCCESSFUL) as mocks:
+            mocks['mock_response'].return_value.responder_name = issuer.subject
+            self.checker.ocsp_revoked(self.cert_path, self.chain_path)
+        # Here responder and issuer are the same. So only the signature of the OCSP
+        # response is checked (using the issuer/responder public key).
+        self.assertEqual(mocks['mock_check'].call_count, 1)
+        self.assertEqual(mocks['mock_check'].call_args[0][0].public_numbers(),
+                         issuer.public_key().public_numbers())
+
+    def test_responder_is_authorized_delegate(self):
         issuer = x509.load_pem_x509_certificate(
             test_util.load_vector('ocsp_issuer_certificate.pem'), default_backend())
         responder = x509.load_pem_x509_certificate(
             test_util.load_vector('ocsp_responder_certificate.pem'), default_backend())
 
-        self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-        # On first call, responder and issuer are not the same. Two signatures will be checked
-        # then, first to verify the responder cert (using the issuer public key), second to
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED,
+                        ocsp_lib.OCSPResponseStatus.SUCCESSFUL) as mocks:
+            self.checker.ocsp_revoked(self.cert_path, self.chain_path)
+        # Here responder and issuer are not the same. Two signatures will be checked then,
+        # first to verify the responder cert (using the issuer public key), second to
         # to verify the OCSP response itself (using the responder public key).
-        self.assertEqual(mock_check.call_count, 2)
-        self.assertEqual(mock_check.call_args_list[0][0][0].public_numbers(),
+        self.assertEqual(mocks['mock_check'].call_count, 2)
+        self.assertEqual(mocks['mock_check'].call_args_list[0][0][0].public_numbers(),
                          issuer.public_key().public_numbers())
-        self.assertEqual(mock_check.call_args_list[1][0][0].public_numbers(),
+        self.assertEqual(mocks['mock_check'].call_args_list[1][0][0].public_numbers(),
                          responder.public_key().public_numbers())
 
-        response.responder_name = issuer.subject
-        self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-        # On second call, responder and issuer are the same. So only the signature of the OCSP
-        # response is checked (using the issuer/responder public key).
-        self.assertEqual(mock_check.call_count, 3)
-        self.assertEqual(mock_check.call_args_list[2][0][0].public_numbers(),
-                         issuer.public_key().public_numbers())
-
-    @mock.patch('certbot.ocsp.crypto_util.verify_signed_payload')
-    @mock.patch('certbot.ocsp.requests.post')
-    @mock.patch('certbot.ocsp.ocsp.load_der_ocsp_response')
-    def test_revoke_resiliency(self, mock_ocsp_response, mock_post, mock_check):
+    def test_revoke_resiliency(self):
         # Server return an invalid HTTP response
-        mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-        mock_post.return_value = mock.Mock(status_code=400)
-        revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.SUCCESSFUL,
+                        http_status_code=400):
+            revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
         self.assertFalse(revoked)
 
         # OCSP response in invalid
-        mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.UNAUTHORIZED)
-        mock_post.return_value = mock.Mock(status_code=200)
-        revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.UNAUTHORIZED):
+            revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
         self.assertFalse(revoked)
 
         # OCSP response is valid, but certificate status is unknown
-        mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-        mock_post.return_value = mock.Mock(status_code=200)
-        revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.SUCCESSFUL):
+            revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
         self.assertFalse(revoked)
 
         # The OCSP response says that the certificate is revoked, but certificate
         # does not contain the OCSP extension.
-        mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.UNKNOWN, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-        mock_post.return_value = mock.Mock(status_code=200)
-        with mock.patch('cryptography.x509.Extensions.get_extension_for_class',
-                        side_effect=x509.ExtensionNotFound(
-                            'Not found', x509.AuthorityInformationAccessOID.OCSP)):
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL):
+            with mock.patch('cryptography.x509.Extensions.get_extension_for_class',
+                            side_effect=x509.ExtensionNotFound(
+                                'Not found', x509.AuthorityInformationAccessOID.OCSP)):
+                revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
+        self.assertFalse(revoked)
+
+        # OCSP response uses an unsupported signature.
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL,
+                        check_signature_side_effect=UnsupportedAlgorithm('foo')):
             revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
         self.assertFalse(revoked)
 
-        # Valid response, OCSP extension is present,
-        # but OCSP response uses an unsupported signature.
-        mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-        mock_post.return_value = mock.Mock(status_code=200)
-        mock_check.side_effect = UnsupportedAlgorithm('foo')
-        revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
+        # OSCP signature response is invalid.
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL,
+                        check_signature_side_effect=InvalidSignature('foo')):
+            revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
         self.assertFalse(revoked)
 
-        # And now, the signature itself is invalid.
-        mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-        mock_post.return_value = mock.Mock(status_code=200)
-        mock_check.side_effect = InvalidSignature('foo')
-        revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
+        # Assertion error on OCSP response validity
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL,
+                        check_signature_side_effect=AssertionError('foo')):
+            revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
         self.assertFalse(revoked)
 
-        # Finally, assertion error on OCSP response validity
-        mock_ocsp_response.return_value = _construct_mock_ocsp_response(
-            ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL)
-        mock_post.return_value = mock.Mock(status_code=200)
-        mock_check.side_effect = AssertionError('foo')
-        revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
-
+        # No responder cert in OCSP response
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED,
+                        ocsp_lib.OCSPResponseStatus.SUCCESSFUL) as mocks:
+            mocks['mock_response'].return_value.certificates = []
+            revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
         self.assertFalse(revoked)
+
+        # Responder cert is not signed by certificate issuer
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED,
+                        ocsp_lib.OCSPResponseStatus.SUCCESSFUL) as mocks:
+            cert = mocks['mock_response'].return_value.certificates[0]
+            mocks['mock_response'].return_value.certificates[0] = mock.Mock(
+                issuer='fake', subject=cert.subject)
+            revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
+        self.assertFalse(revoked)
+
+        with _ocsp_mock(ocsp_lib.OCSPCertStatus.REVOKED, ocsp_lib.OCSPResponseStatus.SUCCESSFUL):
+            # This mock is necessary to avoid the first call contained in _determine_ocsp_server
+            # of the method cryptography.x509.Extensions.get_extension_for_class.
+            with mock.patch('certbot.ocsp._determine_ocsp_server') as mock_server:
+                mock_server.return_value = ('https://i.do.not.care', 'i.do.not.care')
+                with mock.patch('cryptography.x509.Extensions.get_extension_for_class',
+                                side_effect=x509.ExtensionNotFound(
+                                    'Not found', x509.AuthorityInformationAccessOID.OCSP)):
+                    revoked = self.checker.ocsp_revoked(self.cert_path, self.chain_path)
+        self.assertFalse(revoked)
+
+
+@contextlib.contextmanager
+def _ocsp_mock(certificate_status, response_status,
+               http_status_code=200, check_signature_side_effect=None):
+    with mock.patch('certbot.ocsp.ocsp.load_der_ocsp_response') as mock_response:
+        mock_response.return_value = _construct_mock_ocsp_response(
+            certificate_status, response_status)
+        with mock.patch('certbot.ocsp.requests.post') as mock_post:
+            mock_post.return_value = mock.Mock(status_code=http_status_code)
+            with mock.patch('certbot.ocsp.crypto_util.verify_signed_payload') as mock_check:
+                if check_signature_side_effect:
+                    mock_check.side_effect = check_signature_side_effect
+                yield {
+                    'mock_response': mock_response,
+                    'mock_post': mock_post,
+                    'mock_check': mock_check,
+                }
 
 
 def _construct_mock_ocsp_response(certificate_status, response_status):
