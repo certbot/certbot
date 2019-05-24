36c421926c2a07e204a17b0e728b7603ce03572a
Adrien Ferrand <ferrand.ad@gmail.com>
2019-05-24 16:22:13 +0200

    Update tests

--- certbot/tests/compat/filesystem_test.py
+++ certbot/tests/compat/filesystem_test.py
@@ -18,28 +18,35 @@ class FilesystemTest(TempDirTestCase):
 
         # Set ownership of target to authenticated user
         authenticated_user, _, _ = win32security.LookupAccountName("", win32api.GetUserName())
-        security_owner = win32security.GetFileSecurity(target, win32security.OWNER_SECURITY_INFORMATION)
+        security_owner = win32security.GetFileSecurity(
+            target, win32security.OWNER_SECURITY_INFORMATION)
         security_owner.SetSecurityDescriptorOwner(authenticated_user, False)
-        win32security.SetFileSecurity(target, win32security.OWNER_SECURITY_INFORMATION, security_owner)
+        win32security.SetFileSecurity(
+            target, win32security.OWNER_SECURITY_INFORMATION, security_owner)
 
         filesystem.chmod(target, 0o700)
 
-        security_dacl = win32security.GetFileSecurity(target, win32security.DACL_SECURITY_INFORMATION)
+        security_dacl = win32security.GetFileSecurity(
+            target, win32security.DACL_SECURITY_INFORMATION)
         dacl = security_dacl.GetSecurityDescriptorDacl()
         # We expect three ACE: one for admins, one for system, and one for the user
         self.assertEqual(dacl.GetAceCount(), 3)
 
         # Set ownership of target to Administrators user group
         admin_user = win32security.ConvertStringSidToSid('S-1-5-32-544')
-        security_owner = win32security.GetFileSecurity(target, win32security.OWNER_SECURITY_INFORMATION)
+        security_owner = win32security.GetFileSecurity(
+            target, win32security.OWNER_SECURITY_INFORMATION)
         security_owner.SetSecurityDescriptorOwner(admin_user, False)
-        win32security.SetFileSecurity(target, win32security.OWNER_SECURITY_INFORMATION, security_owner)
+        win32security.SetFileSecurity(
+            target, win32security.OWNER_SECURITY_INFORMATION, security_owner)
 
         filesystem.chmod(target, 0o700)
 
-        security_dacl = win32security.GetFileSecurity(target, win32security.DACL_SECURITY_INFORMATION)
+        security_dacl = win32security.GetFileSecurity(
+            target, win32security.DACL_SECURITY_INFORMATION)
         dacl = security_dacl.GetSecurityDescriptorDacl()
-        # We expect only two ACE: one for admins, one for system, since the user is also the admins group
+        # We expect only two ACE: one for admins, one for system,
+        # since the user is also the admins group
         self.assertEqual(dacl.GetAceCount(), 2)
 
 
