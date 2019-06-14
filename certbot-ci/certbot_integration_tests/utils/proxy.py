d60b1409861de43a95fa74f987fd8cc090d56a45
Adrien Ferrand <ferrand.ad@gmail.com>
2019-06-14 15:58:36 +0200

    Refactor proxy config

--- certbot-ci/certbot_integration_tests/utils/proxy.py
+++ certbot-ci/certbot_integration_tests/utils/proxy.py
@@ -3,39 +3,40 @@ import json
 import sys
 
 from six.moves import SimpleHTTPServer, socketserver
-from six.moves.urllib.request import urlopen
+from six.moves.urllib.request import Request, urlopen
 
 
 class _GracefulTCPServer(socketserver.TCPServer):
     allow_reuse_address = True
 
 
-def _create_proxy(ports_mapping):
+def _select_port(mapping, host):
+    fqdn = host.split(':')[0]
+    return [port for pattern, port in mapping.items()
+            if fqdn.endswith(pattern)][0]
+
+
+def _create_proxy(mapping):
     class ProxyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         def do_GET(self):
-            url = '{0}:{1}{2}'.format('http://127.0.0.1', self._select_port(), self.path)
+            host = self.headers.get('Host')
+            url = '{0}:{1}{2}'.format('http://127.0.0.1', _select_port(mapping, host), self.path)
+            req = Request(url, headers={'Host': host})
+            response = urlopen(req)
+
+            self.send_response(response.getcode())
+            for key, value in response.getinfo():
+                self.send_header(key, value)
+            self.end_headers()
             self.copyfile(urlopen(url), self.wfile)
-
-        def end_headers(self):
-            self.send_header('Host', self._get_host())
-            SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
-
-        def _select_port(self):
-            host = self._get_host()
-            return [port for pattern, port in ports_mapping.items()
-                    if host.endswith(pattern)][0]
-
-        def _get_host(self):
-            print(vars(self.headers))
-            return self.headers.getheader('Host').split(':')[0]
 
     return ProxyHandler
 
 
 if __name__ == '__main__':
     http_port = int(sys.argv[1])
-    mapping = json.loads(sys.argv[2])
-    httpd = _GracefulTCPServer(('', http_port), _create_proxy(mapping))
+    port_mapping = json.loads(sys.argv[2])
+    httpd = _GracefulTCPServer(('', http_port), _create_proxy(port_mapping))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
