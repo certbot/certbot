d445b96442d8dd9b1dbbc6f6ef868f2b0a80d74e
Adrien Ferrand <ferrand.ad@gmail.com>
2019-06-17 09:39:17 +0200

    Full object

--- certbot-ci/certbot_integration_tests/utils/acme_server.py
+++ certbot-ci/certbot_integration_tests/utils/acme_server.py
@@ -38,7 +38,7 @@ class ACMEServer(object):
         :param str[] nodes: list of node names that will be setup by pytest xdist
         :param bool proxy: set to False to not start the Traefik proxy
         """
-        self.acme_xdist = _construct_acme_xdist(acme_server, nodes)
+        self._construct_acme_xdist(acme_server, nodes)
 
         self._acme_type = 'pebble' if acme_server == 'pebble' else 'boulder'
         self._proxy = proxy
@@ -48,11 +48,11 @@ class ACMEServer(object):
     def start(self):
         """Start the test stack"""
         if self._proxy:
-            self._processes.extend(_prepare_http_proxy(self.acme_xdist))
+            self._prepare_http_proxy()
         if self._acme_type == 'pebble':
-            self._processes.extend(_prepare_pebble_server(self._workspace, self.acme_xdist))
+            self._prepare_pebble_server()
         if self._acme_type == 'boulder':
-            self._processes.extend(_prepare_boulder_server(self._workspace, self.acme_xdist))
+            self._prepare_boulder_server()
 
     def stop(self):
         """Stop the test stack, and clean its resources"""
@@ -68,9 +68,9 @@ class ACMEServer(object):
                 # If we started the acme server from a normal user that has access to the Docker
                 # daemon, this user will not be able to delete these artifacts from the host.
                 # We need to do it through a docker.
-                process = _launch_process(['docker', 'run', '--rm', '-v',
-                                           '{0}:/workspace'.format(self._workspace),
-                                            'alpine', 'rm', '-rf', '/workspace/boulder'])
+                process = self._launch_process(['docker', 'run', '--rm', '-v',
+                                                '{0}:/workspace'.format(self._workspace),
+                                                'alpine', 'rm', '-rf', '/workspace/boulder'])
                 process.wait()
         finally:
             shutil.rmtree(self._workspace)
@@ -83,104 +83,96 @@ class ACMEServer(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
 
+    def _construct_acme_xdist(self, acme_server, nodes):
+        """Generate and return the acme_xdist dict"""
+        acme_xdist = {'acme_server': acme_server, 'challtestsrv_port': CHALLTESTSRV_PORT}
 
-def _construct_acme_xdist(acme_server, nodes):
-    """Generate and return the acme_xdist dict"""
-    acme_xdist = {'acme_server': acme_server, 'challtestsrv_port': CHALLTESTSRV_PORT}
+        # Directory and ACME port are set implicitly in the docker-compose.yml files of Boulder/Pebble.
+        if acme_server == 'pebble':
+            acme_xdist['directory_url'] = PEBBLE_DIRECTORY_URL
+        else:  # boulder
+            acme_xdist['directory_url'] = BOULDER_V2_DIRECTORY_URL \
+                if acme_server == 'boulder-v2' else BOULDER_V1_DIRECTORY_URL
 
-    # Directory and ACME port are set implicitly in the docker-compose.yml files of Boulder/Pebble.
-    if acme_server == 'pebble':
-        acme_xdist['directory_url'] = PEBBLE_DIRECTORY_URL
-    else:  # boulder
-        acme_xdist['directory_url'] = BOULDER_V2_DIRECTORY_URL \
-            if acme_server == 'boulder-v2' else BOULDER_V1_DIRECTORY_URL
+        acme_xdist['http_port'] = {node: port for (node, port)
+                                   in zip(nodes, range(5200, 5200 + len(nodes)))}
+        acme_xdist['https_port'] = {node: port for (node, port)
+                                    in zip(nodes, range(5100, 5100 + len(nodes)))}
+        acme_xdist['other_port'] = {node: port for (node, port)
+                                    in zip(nodes, range(5300, 5300 + len(nodes)))}
 
-    acme_xdist['http_port'] = {node: port for (node, port)
-                               in zip(nodes, range(5200, 5200 + len(nodes)))}
-    acme_xdist['https_port'] = {node: port for (node, port)
-                                in zip(nodes, range(5100, 5100 + len(nodes)))}
-    acme_xdist['other_port'] = {node: port for (node, port)
-                                in zip(nodes, range(5300, 5300 + len(nodes)))}
+        self.acme_xdist = acme_xdist
 
-    return acme_xdist
+    def _prepare_pebble_server(self):
+        """Configure and launch the Pebble server"""
+        print('=> Starting pebble instance deployment...')
+        pebble_path, challtestsrv_path, pebble_config_path = pebble_artifacts.fetch(self._workspace)
 
+        # Configure Pebble at full speed (PEBBLE_VA_NOSLEEP=1) and not randomly refusing valid
+        # nonce (PEBBLE_WFE_NONCEREJECT=0) to have a stable test environment.
+        environ = os.environ.copy()
+        environ['PEBBLE_VA_NOSLEEP'] = '1'
+        environ['PEBBLE_WFE_NONCEREJECT'] = '0'
 
-def _prepare_pebble_server(workspace, acme_xdist):
-    print('=> Starting pebble instance deployment...')
-    pebble_path, challtestsrv_path, pebble_config_path = pebble_artifacts.fetch(workspace)
+        self._launch_process(
+            [pebble_path, '-config', pebble_config_path, '-strict', '-dnsserver', '127.0.0.1:8053'],
+            env=environ)
 
-    # Configure Pebble at full speed (PEBBLE_VA_NOSLEEP=1) and not randomly refusing valid
-    # nonce (PEBBLE_WFE_NONCEREJECT=0) to have a stable test environment.
-    environ = os.environ.copy()
-    environ['PEBBLE_VA_NOSLEEP'] = '1'
-    environ['PEBBLE_WFE_NONCEREJECT'] = '0'
+        self._launch_process(
+            [challtestsrv_path, '-management', ':{0}'.format(CHALLTESTSRV_PORT), '-defaultIPv6', '""',
+             '-defaultIPv4', '127.0.0.1', '-http01', '""', '-tlsalpn01', '""', '-https01', '""'])
 
-    process_pebble = _launch_process(
-        [pebble_path, '-config', pebble_config_path, '-strict', '-dnsserver', '127.0.0.1:8053'],
-        env=environ)
+        # Wait for the ACME CA server to be up.
+        print('=> Waiting for pebble instance to respond...')
+        misc.check_until_timeout(self.acme_xdist['directory_url'])
 
-    process_challtestsrv = _launch_process(
-        [challtestsrv_path, '-management', ':{0}'.format(CHALLTESTSRV_PORT), '-defaultIPv6', '""',
-         '-defaultIPv4', '127.0.0.1', '-http01', '""', '-tlsalpn01', '""', '-https01', '""'])
+        print('=> Finished pebble instance deployment.')
 
-    # Wait for the ACME CA server to be up.
-    print('=> Waiting for pebble instance to respond...')
-    misc.check_until_timeout(acme_xdist['directory_url'])
+    def _prepare_boulder_server(self):
+        """Configure and launch the Boulder server"""
+        print('=> Starting boulder instance deployment...')
+        instance_path = join(self._workspace, 'boulder')
 
-    print('=> Finished pebble instance deployment.')
+        # Load Boulder from git, that includes a docker-compose.yml ready for production.
+        process = self._launch_process(['git', 'clone', 'https://github.com/letsencrypt/boulder',
+                                   '--single-branch', '--depth=1', instance_path])
+        process.wait()
 
-    return [process_pebble, process_challtestsrv]
+        # Allow Boulder to ignore usual limit rate policies, useful for tests.
+        os.rename(join(instance_path, 'test/rate-limit-policies-b.yml'),
+                  join(instance_path, 'test/rate-limit-policies.yml'))
 
+        # Launch the Boulder server
+        self._launch_process(['docker-compose', 'up', '--force-recreate'], cwd=instance_path)
 
-def _prepare_boulder_server(workspace, acme_xdist):
-    """Configure and launch the ACME server, Boulder or Pebble"""
-    print('=> Starting boulder instance deployment...')
-    instance_path = join(workspace, 'boulder')
+        # Wait for the ACME CA server to be up.
+        print('=> Waiting for boulder instance to respond...')
+        misc.check_until_timeout(self.acme_xdist['directory_url'])
 
-    # Load Boulder from git, that includes a docker-compose.yml ready for production.
-    process = _launch_process(['git', 'clone', 'https://github.com/letsencrypt/boulder',
-                               '--single-branch', '--depth=1', instance_path])
-    process.wait()
+        # Configure challtestsrv to answer any A record request with ip of the docker host.
+        response = requests.post('http://localhost:{0}/set-default-ipv4'.format(CHALLTESTSRV_PORT),
+                                 json={'ip': '10.77.77.1'})
+        response.raise_for_status()
 
-    # Allow Boulder to ignore usual limit rate policies, useful for tests.
-    os.rename(join(instance_path, 'test/rate-limit-policies-b.yml'),
-              join(instance_path, 'test/rate-limit-policies.yml'))
+        print('=> Finished boulder instance deployment.')
 
-    # Launch the Boulder server
-    process = _launch_process(['docker-compose', 'up', '--force-recreate'], cwd=instance_path)
+    def _prepare_http_proxy(self):
+        """Configure and launch an HTTP proxy"""
+        print('=> Configuring the HTTP proxy...')
+        mapping = {r'.+\.{0}\.wtf'.format(node): 'http://127.0.0.1:{0}'.format(port)
+                   for node, port in self.acme_xdist['http_port'].items()}
+        command = [sys.executable, proxy.__file__, str(HTTP_01_PORT), json.dumps(mapping)]
+        self._launch_process(command)
+        print('=> Finished configuring the HTTP proxy.')
 
-    # Wait for the ACME CA server to be up.
-    print('=> Waiting for boulder instance to respond...')
-    misc.check_until_timeout(acme_xdist['directory_url'])
-
-    # Configure challtestsrv to answer any A record request with ip of the docker host.
-    response = requests.post('http://localhost:{0}/set-default-ipv4'.format(CHALLTESTSRV_PORT),
-                             json={'ip': '10.77.77.1'})
-    response.raise_for_status()
-
-    print('=> Finished boulder instance deployment.')
-
-    return [process]
-
-
-def _prepare_http_proxy(acme_xdist):
-    """Configure and launch an HTTP proxy"""
-    print('=> Configuring the HTTP proxy...')
-    mapping = {r'.+\.{0}\.wtf'.format(node): 'http://127.0.0.1:{0}'.format(port)
-               for node, port in acme_xdist['http_port'].items()}
-    command = [sys.executable, proxy.__file__, str(HTTP_01_PORT), json.dumps(mapping)]
-    process = _launch_process(command)
-    print('=> Finished configuring the HTTP proxy.')
-
-    return [process]
-
-
-def _launch_process(command, cwd=os.getcwd(), env=None):
-    """Launch silently an subprocess OS command"""
-    if not env:
-        env = os.environ
-    with open(os.devnull, 'w') as null:
-        return subprocess.Popen(command, stdout=null, stderr=subprocess.STDOUT, cwd=cwd, env=env)
+    def _launch_process(self, command, cwd=os.getcwd(), env=None):
+        """Launch silently an subprocess OS command"""
+        if not env:
+            env = os.environ
+        with open(os.devnull, 'w') as null:
+            process = subprocess.Popen(command, stdout=null, stderr=subprocess.STDOUT, cwd=cwd, env=env)
+            self._processes.append(process)
+            return process
 
 
 def main():
