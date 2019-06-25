b0fcb0f5901e2b348d6e2f5a8ffdda3b098313f1
Brad Warren <bmw@eff.org>
2019-06-25 10:30:06 -0700

    Drop support for quay.io.

--- tools/docker-warning.sh
+++ tools/docker-warning.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
-echo "Warning: This Docker image will soon be switching to Alpine Linux." >&2
-echo "You can switch now using the certbot/certbot repo on Docker Hub." >&2
+echo 'Warning: This Docker image is no longer receiving updates!' >&2
+echo 'You should switch to the Docker images on Docker Hub at:' >&2
+echo 'https://hub.docker.com/u/certbot' >&2
 exec /opt/certbot/venv/bin/certbot $@
