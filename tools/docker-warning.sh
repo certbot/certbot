f2ab6a338cb2177697b671e4381e8d08a50b5c06
Brad Warren <bmw@users.noreply.github.com>
2019-06-26 02:54:02 -0700

    Remove files for old Docker image. (#7188)

--- tools/docker-warning.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-echo 'Warning: This Docker image is no longer receiving updates!' >&2
-echo 'You should switch to the Docker images on Docker Hub at:' >&2
-echo 'https://hub.docker.com/u/certbot' >&2
-exec /opt/certbot/venv/bin/certbot $@
