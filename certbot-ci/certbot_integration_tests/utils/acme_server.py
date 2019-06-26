16c95765709de0e85eecc53d6fb2a3e5057817b3
Adrien Ferrand <ferrand.ad@gmail.com>
2019-06-26 12:45:54 +0200

    Update docstring

--- certbot-ci/certbot_integration_tests/utils/acme_server.py
+++ certbot-ci/certbot_integration_tests/utils/acme_server.py
@@ -35,7 +35,7 @@ class ACMEServer(object):
         """
         Create an ACMEServer instance.
         :param str acme_server: the type of acme server used (boulder-v1, boulder-v2 or pebble)
-        :param str[] nodes: list of node names that will be setup by pytest xdist
+        :param list nodes: list of node names that will be setup by pytest xdist
         :param bool proxy: set to False to not start the Traefik proxy
         """
         self._construct_acme_xdist(acme_server, nodes)
