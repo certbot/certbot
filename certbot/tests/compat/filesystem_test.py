27147502cf6d56b6f091a95aae9edc9813f3d6cf
Adrien Ferrand <ferrand.ad@gmail.com>
2019-06-28 22:02:07 +0200

    Added test cases

--- certbot/tests/compat/filesystem_test.py
+++ certbot/tests/compat/filesystem_test.py
@@ -1,15 +1,20 @@
 """Tests for certbot.compat.filesystem"""
+import errno
 import unittest
 
 try:
-    import win32api  # pylint: disable=import-error
-    import win32security  # pylint: disable=import-error
-    import ntsecuritycon  # pylint: disable=import-error
+    # pylint: disable=import-error
+    import win32api
+    import win32security
+    import ntsecuritycon
+    import msvcrt
+    # pylint: enable=import-error
     POSIX_MODE = False
 except ImportError:
     POSIX_MODE = True
 
 import certbot.tests.util as test_util
+from certbot import lock
 from certbot.compat import os
 from certbot.compat import filesystem
 from certbot.tests.util import TempDirTestCase
@@ -180,6 +185,49 @@ class WindowsOpenTest(TempDirTestCase):
 
         self.assertFalse([dacl.GetAce(index) for index in range(0, dacl.GetAceCount())
                           if dacl.GetAce(index)[2] == everybody])
+
+    def test_create_file_on_open(self):
+        # os.O_CREAT | os.O_EXCL + file not exists = OK
+        self._test_one_creation(1, file_exist=False, flags=(os.O_CREAT | os.O_EXCL))
+
+        # os.O_CREAT | os.O_EXCL + file exists = EEXIST OS exception
+        with self.assertRaises(OSError) as raised:
+            self._test_one_creation(2, file_exist=True, flags=(os.O_CREAT | os.O_EXCL))
+        self.assertEqual(raised.exception.errno, errno.EEXIST)
+
+        # os.O_CREAT + file not exists = OK
+        self._test_one_creation(3, file_exist=False, flags=os.O_CREAT)
+
+        # os.O_CREAT + file exists = OK
+        self._test_one_creation(4, file_exist=True, flags=os.O_CREAT)
+
+        # os.O_CREAT + file exists (locked) = EACCES OS exception
+        path = os.path.join(self.tempdir, '5')
+        open(path, 'w').close()
+        filelock = lock.LockFile(path)
+        try:
+            with self.assertRaises(OSError) as raised:
+                self._test_one_creation(5, file_exist=True, flags=os.O_CREAT)
+            self.assertEqual(raised.exception.errno, errno.EACCES)
+        finally:
+            filelock.release()
+
+        # os.O_CREAT not set + file not exists = OS exception
+        with self.assertRaises(OSError):
+            self._test_one_creation(6, file_exist=False, flags=os.O_RDONLY)
+
+    def _test_one_creation(self, num, file_exist, flags):
+        one_file = os.path.join(self.tempdir, str(num))
+        if file_exist and not os.path.exists(one_file):
+            open(one_file, 'w').close()
+
+        handler = None
+        try:
+            handler = filesystem.open(one_file, flags)
+        except BaseException as err:
+            if handler:
+                os.close(handler)
+            raise err
 
 
 class OsReplaceTest(test_util.TempDirTestCase):
