192e4c3a2703e95b639918cc1ebf99f87af92dd9
Adrien Ferrand <ferrand.ad@gmail.com>
2019-06-28 22:07:54 +0200

    Improve docstring

--- certbot/compat/filesystem.py
+++ certbot/compat/filesystem.py
@@ -55,7 +55,9 @@ def open(file_path, flags, mode=0o777):  # pylint: disable=redefined-builtin
         Python defaults will be applied if ``None``
     :returns: the file descriptor to the opened file
     :rtype: int
-    :raise: OSError(errno.EEXIST) if the file already exists and os.O_CREAT & os.O_EXCL are set.
+    :raise: OSError(errno.EEXIST) if the file already exists and os.O_CREAT & os.O_EXCL are set,
+            OSError(errno.EACCES) on Windows if the file already exists and is a directory, and
+                os.O_CREAT is set.
     """
     if POSIX_MODE:
         # On Linux, invoke os.open directly.
