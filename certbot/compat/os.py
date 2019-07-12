70ca7f04d6dbdbaec25e8c8583405a72d8ac7dde
Adrien Ferrand <ferrand.ad@gmail.com>
2019-07-12 23:54:18 +0200

    Use same approach of wrapping than certbot.compat.os

--- certbot/compat/os.py
+++ certbot/compat/os.py
@@ -17,7 +17,6 @@ from os import *  # type: ignore  # pylint: disable=wildcard-import,unused-wildc
 # and so not in `from os import *`.
 import os as std_os  # pylint: disable=os-module-forbidden
 import sys as std_sys
-import importlib as std_importlib
 
 ourselves = std_sys.modules[__name__]
 for attribute in dir(std_os):
@@ -27,12 +26,12 @@ for attribute in dir(std_os):
     if not hasattr(ourselves, attribute):
         setattr(ourselves, attribute, getattr(std_os, attribute))
 
-# Similar to os.path, allow certbot.compat.os.path to behave as a module
-path = std_importlib.import_module('os.path')
+# Import our path module, then allow certbot.compat.os.path to behave as a module (like os.path)
+from certbot.compat import path
 std_sys.modules[__name__ + '.path'] = path
 
 # Clean all remaining importables that are not from the core os module.
-del ourselves, std_os, std_sys, std_importlib
+del ourselves, std_os, std_sys
 
 
 # Chmod is the root of all evil for our security model on Windows. With the default implementation
@@ -107,14 +106,3 @@ def replace(*unused_args, **unused_kwargs):
     """Method os.replace() is forbidden"""
     raise RuntimeError('Usage of os.replace() is forbidden. '
                        'Use certbot.compat.filesystem.replace() instead.')
-
-
-def realpath(*unused_args, **unused_kwargs):
-    """Method os.path.realpath() is forbidden"""
-    raise RuntimeError('Usage of os.path.realpath() is forbidden. '
-                       'Use certbot.compat.filesystem.realpath() instead.')
-
-
-# Associate new realpath method to current os.path module
-setattr(path, 'realpath', realpath)
-del realpath
