20e7b5c503ba89f192634ad98e1ddeb562244879
Adrien Ferrand <ferrand.ad@gmail.com>
2019-08-02 12:14:16 +0200

    Clean debug code

--- certbot-ci/certbot_integration_tests/assets/hook.py
+++ certbot-ci/certbot_integration_tests/assets/hook.py
@@ -3,7 +3,6 @@ import sys
 import os
 
 hook_script_type = os.path.basename(os.path.dirname(sys.argv[1]))
-sys.stderr.write(str(os.environ))
 if hook_script_type == 'deploy' and ('RENEWED_DOMAINS' not in os.environ or 'RENEWED_LINEAGE' not in os.environ):
     sys.stderr.write('Environment variables not properly set!\n')
     sys.exit(1)
