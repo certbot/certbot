e0ce3f9d6e444ea9023b8c8081c9ea4de578784a
Joona Hoikkala <joohoi@users.noreply.github.com>
2019-08-16 09:43:54 +0300

    Update certbot-apache/certbot_apache/interfaces.py
    Co-Authored-By: Brad Warren <bmw@users.noreply.github.com>

--- certbot-apache/certbot_apache/interfaces.py
+++ certbot-apache/certbot_apache/interfaces.py
@@ -252,7 +252,7 @@ class BlockNode(ParserNode):
     name: str
 
     # Tuple of parameters of this ParserNode object, excluding whitespaces.
-    parameters: Tuple[str]
+    parameters: Tuple[str, ...]
 
     # Tuple of ParserNode objects that are the children for this node. The order
     # of the children is the same s that of the parsed configuration block.
