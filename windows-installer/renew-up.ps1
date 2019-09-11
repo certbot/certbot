aa2e158cf302581346f33c04e1ff469251920333
Adrien Ferrand <ferrand.ad@gmail.com>
2019-09-12 01:11:58 +0200

    Improve scripts

--- windows-installer/renew-up.ps1
+++ windows-installer/renew-up.ps1
@@ -1,12 +1,11 @@
-$taskName = "Certbot Renew Task"
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$down = Join-Path (Get-ScriptDirectory) 'renew-down.ps1'
+& $down
 
-$exists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName}
-if ($exists) {
-    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
-}
+$taskName = "Certbot Renew Task"
 
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -Command "certbot renew"'
 $triggerAM = New-ScheduledTaskTrigger -Daily -At 12am
 $triggerPM = New-ScheduledTaskTrigger -Daily -At 12pm
-$principal = New-ScheduledTaskPrincipal -UserId $env:UserName -LogonType S4U -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserId 'System' -LogonType S4U -RunLevel Highest
 Register-ScheduledTask -Action $action -Trigger $triggerAM,$triggerPM -TaskName $taskName -Description "Execute twice a day the 'certbot renew' command, to renew managed certificates if needed." -Principal $principal
