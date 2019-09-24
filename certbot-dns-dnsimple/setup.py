4e3d90dfe02ddfe61f71acf646855bffe9197cfd
Brad Warren <bmw@eff.org>
2019-09-24 11:24:05 -0700

    List support for Python 3.8.

--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -55,6 +55,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Security',
         'Topic :: System :: Installation/Setup',
