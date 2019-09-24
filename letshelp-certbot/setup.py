8cb57566c0be9fbed6d86316a0afb18e86d9f481
Brad Warren <bmw@users.noreply.github.com>
2019-09-24 11:38:38 -0700

    List support for Python 3.8 (#7392)
    Fixes #7368.
    
    When updating the changelog, I replaced the line about running tests on Python 3.8 because I personally think that support for Python 3.8 is the most relevant information for our users/packagers about our changes in this area.
    
    * List support for Python 3.8.
    
    * Update changelog.

--- letshelp-certbot/setup.py
+++ letshelp-certbot/setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Security',
         'Topic :: System :: Installation/Setup',
