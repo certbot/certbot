84646a918ee0684e5167a586ec3155f79f6bfe1f
Adrien Ferrand <ferrand.ad@gmail.com>
2019-10-21 23:53:05 +0200

    Fix test message

--- letsencrypt-auto-source/tests/oraclelinux6_tests.sh
+++ letsencrypt-auto-source/tests/oraclelinux6_tests.sh
@@ -27,7 +27,7 @@ fi
 echo "PASSED: Bootstrap from old letsencrypt-auto succeeded and installed Python 3.4"
 
 # Expect letsencrypt-auto to skip rebootstrapping with a warning since SCL is not installed.
-if ! "$LE_AUTO" --non-interactive --version 2>&1 | grep -q "Enable the SCL repository and try running Certbot again."; then
+if ! "$LE_AUTO" --non-interactive --version 2>&1 | grep -q "Skipping upgrade because new OS dependencies may need to be installed."; then
   echo "FAILED: Script letsencrypt-auto managed to start Certbot in interactive shell while SCL is not enabled!"
   exit 1
 fi
