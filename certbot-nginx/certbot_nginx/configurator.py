e4fd25912227b0734abf6e32ada5af73521c9db1
Tomo.M <tomoyuki@pobox.com>
2019-10-31 06:23:14 +0000

    Fix tox error

--- certbot-nginx/certbot_nginx/configurator.py
+++ certbot-nginx/certbot_nginx/configurator.py
@@ -629,8 +629,9 @@ class NginxConfigurator(common.Installer):
         """Generate invalid certs that let us create ssl directives for Nginx"""
         # TODO: generate only once
         tmp_dir = os.path.join(self.config.work_dir, "snakeoil")
+        key_prop = { "key_type": "rsa", "key_size": 1024 }
         le_key = crypto_util.init_save_key(
-            key_size=1024, key_dir=tmp_dir, keyname="key.pem")
+            key_prop, key_dir=tmp_dir, keyname="key.pem")
         key = OpenSSL.crypto.load_privatekey(
             OpenSSL.crypto.FILETYPE_PEM, le_key.pem)
         cert = acme_crypto_util.gen_ss_cert(key, domains=[socket.gethostname()])
