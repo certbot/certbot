72458ef9b74bf65e3a242bda9b0a03ffee9e789e
Joona Hoikkala <joona@kuori.org>
2019-11-01 09:37:02 +0200

    Better test state handling

--- certbot/tests/util_test.py
+++ certbot/tests/util_test.py
@@ -478,7 +478,6 @@ class OsInfoTest(unittest.TestCase):
     @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
     def test_systemd_os_release_like(self, m_distro):
         import certbot.util as cbutil
-        cbutil._USE_DISTRO = True  # pylint: disable=protected-access
         m_distro.like.return_value = "first debian third"
         id_likes = cbutil.get_systemd_os_like()
         self.assertEqual(len(id_likes), 3)
@@ -488,7 +487,6 @@ class OsInfoTest(unittest.TestCase):
     @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
     def test_get_os_info_ua(self, m_distro):
         import certbot.util as cbutil
-        cbutil._USE_DISTRO = True  # pylint: disable=protected-access
         with mock.patch('platform.system_alias',
                         return_value=('linux', '42', '42')):
             m_distro.name.return_value = ""
@@ -504,7 +502,6 @@ class OsInfoTest(unittest.TestCase):
     @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
     def test_get_os_info(self, m_distro):
         import certbot.util as cbutil
-        cbutil._USE_DISTRO = True  # pylint: disable=protected-access
         with mock.patch("platform.system") as mock_platform:
             m_distro.linux_distribution.return_value = ("name", "version", 'x')
             mock_platform.return_value = "linux"
@@ -518,44 +515,42 @@ class OsInfoTest(unittest.TestCase):
     @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
     def test_get_systemd_os_info_deprecation(self, _, mock_warn):
         import certbot.util as cbutil
-        cbutil._USE_DISTRO = True  # pylint: disable=protected-access
         cbutil.get_systemd_os_info()
         self.assertTrue(mock_warn.called)
 
     @mock.patch("certbot.util.subprocess.Popen")
     def test_non_systemd_os_info(self, popen_mock):
         import certbot.util as cbutil
-        cbutil._USE_DISTRO = False  # pylint: disable=protected-access
-        with mock.patch('platform.system_alias',
-                        return_value=('NonSystemD', '42', '42')):
-            self.assertEqual(cbutil.get_python_os_info()[0], 'nonsystemd')
+        with mock.patch('certbot.util._USE_DISTRO', False):
+            with mock.patch('platform.system_alias',
+                            return_value=('NonSystemD', '42', '42')):
+                self.assertEqual(cbutil.get_python_os_info()[0], 'nonsystemd')
 
-        with mock.patch('platform.system_alias',
-                        return_value=('darwin', '', '')):
-            comm_mock = mock.Mock()
-            comm_attrs = {'communicate.return_value':
-                        ('42.42.42', 'error')}
-            comm_mock.configure_mock(**comm_attrs)
-            popen_mock.return_value = comm_mock
-            self.assertEqual(cbutil.get_python_os_info()[0], 'darwin')
-            self.assertEqual(cbutil.get_python_os_info()[1], '42.42.42')
+            with mock.patch('platform.system_alias',
+                            return_value=('darwin', '', '')):
+                comm_mock = mock.Mock()
+                comm_attrs = {'communicate.return_value':
+                            ('42.42.42', 'error')}
+                comm_mock.configure_mock(**comm_attrs)
+                popen_mock.return_value = comm_mock
+                self.assertEqual(cbutil.get_python_os_info()[0], 'darwin')
+                self.assertEqual(cbutil.get_python_os_info()[1], '42.42.42')
 
-        with mock.patch('platform.system_alias',
-                        return_value=('freebsd', '9.3-RC3-p1', '')):
-            self.assertEqual(cbutil.get_python_os_info(), ("freebsd", "9"))
+            with mock.patch('platform.system_alias',
+                            return_value=('freebsd', '9.3-RC3-p1', '')):
+                self.assertEqual(cbutil.get_python_os_info(), ("freebsd", "9"))
 
-        with mock.patch('platform.system_alias',
-                        return_value=('windows', '', '')):
-            with mock.patch('platform.win32_ver',
-                            return_value=('4242', '95', '2', '')):
-                self.assertEqual(cbutil.get_python_os_info(),
-                                ("windows", "95"))
+            with mock.patch('platform.system_alias',
+                            return_value=('windows', '', '')):
+                with mock.patch('platform.win32_ver',
+                                return_value=('4242', '95', '2', '')):
+                    self.assertEqual(cbutil.get_python_os_info(),
+                                    ("windows", "95"))
 
     @mock.patch("certbot.util.distro")
     @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
     def test_python_os_info_notfound(self, m_distro):
         import certbot.util as cbutil
-        cbutil._USE_DISTRO = True  # pylint: disable=protected-access
         m_distro.linux_distribution.return_value = ('', '', '')
         self.assertEqual(cbutil.get_python_os_info()[0], "linux")
 
@@ -563,7 +558,6 @@ class OsInfoTest(unittest.TestCase):
     @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
     def test_python_os_info_custom(self, m_distro):
         import certbot.util as cbutil
-        cbutil._USE_DISTRO = True  # pylint: disable=protected-access
         m_distro.linux_distribution.return_value = ('testdist', '42', '')
         self.assertEqual(cbutil.get_python_os_info(), ("testdist", "42"))
 
