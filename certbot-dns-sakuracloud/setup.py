61f77c35c06fc27b0df3497576b1d2d9b630d6a5
Erica Portnoy <ebportnoy@gmail.com>
2019-11-05 18:32:22 -0800

    Bump version to 1.0.0

--- certbot-dns-sakuracloud/setup.py
+++ certbot-dns-sakuracloud/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '0.40.1'
+version = '1.0.0.dev0'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
