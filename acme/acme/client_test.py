39e3918477b4ecefa1f9fcc6d9f4349f01e80100
Adrien Ferrand <ferrand.ad@gmail.com>
2019-11-13 00:33:05 +0100

    Fix error

--- acme/acme/client_test.py
+++ acme/acme/client_test.py
@@ -392,7 +392,7 @@ class ClientTest(ClientTestBase):
     def test_agree_to_tos(self):
         self.client.update_registration = mock.Mock()
         self.client.agree_to_tos(self.regr)
-        regr = self.client.update_registration.call_args.args[0]
+        regr = self.client.update_registration.call_args[0][0]
         self.assertEqual(self.regr.terms_of_service, regr.body.agreement)
 
     def _prepare_response_for_request_challenges(self):
