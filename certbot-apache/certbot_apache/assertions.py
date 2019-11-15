2b78c32cf8a1ed6c55dd67bf0b4440f6103416d8
Joona Hoikkala <joona@kuori.org>
2019-11-15 12:50:24 +0200

    Add parsed_files stub to ApacheParserNodes and fix assertions

--- certbot-apache/certbot_apache/assertions.py
+++ certbot-apache/certbot_apache/assertions.py
@@ -105,11 +105,15 @@ def assertEqualSimple(first, second):
     if not isPass(first) and not isPass(second):
         assert first == second
 
-def assertEqualPathsList(first, second):
+def assertEqualPathsList(first, second):  # pragma: no cover
     """
     Checks that the two lists of file paths match. This assertion allows for wildcard
     paths.
     """
+    if any([isPass(path) for path in first]):
+        return
+    if any([isPass(path) for path in second]):
+        return
     for fpath in first:
         assert any([fnmatch.fnmatch(fpath, spath) for spath in second])
     for spath in second:
