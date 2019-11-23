e8857149e12a22e88052ebd0b53fa6a0e477f93c
Erica Portnoy <ebportnoy@gmail.com>
2019-11-22 18:16:29 -0800

    Remove unused sys and logging from main shim

--- certbot/certbot/main.py
+++ certbot/certbot/main.py
@@ -1,11 +1,5 @@
 """Certbot main public entry point."""
-import logging.handlers
-import sys
-
 from certbot._internal import main as internal_main
-
-
-logger = logging.getLogger(__name__)
 
 
 def main(cli_args=None):
