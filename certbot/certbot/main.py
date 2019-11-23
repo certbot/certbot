ee37260df17a0940b09e0e5d2ca20dfa33045cc6
Erica Portnoy <ebportnoy@gmail.com>
2019-11-22 17:40:35 -0800

    Remove cruft for running certbot directly from main

--- certbot/certbot/main.py
+++ certbot/certbot/main.py
@@ -11,10 +11,3 @@ logger = logging.getLogger(__name__)
 def main(*args, **kwargs):
     """Shim around internal main function"""
     return internal_main.main(*args, **kwargs)
-
-
-if __name__ == "__main__":
-    err_string = main()
-    if err_string:
-        logger.warning("Exiting with message %s", err_string)
-    sys.exit(err_string)  # pragma: no cover
