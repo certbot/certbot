e4d1121cc478760e254a9d66b1062bc0c04572e6
Erica Portnoy <ebportnoy@gmail.com>
2019-11-22 17:42:32 -0800

    Have main shim take real arg

--- certbot/certbot/main.py
+++ certbot/certbot/main.py
@@ -8,6 +8,13 @@ from certbot._internal import main as internal_main
 logger = logging.getLogger(__name__)
 
 
-def main(*args, **kwargs):
-    """Shim around internal main function"""
-    return internal_main.main(*args, **kwargs)
+def main(cli_args=None):
+    """Shim around internal main script execution.
+
+    :returns: result of requested command
+
+    :raises errors.Error: OS errors triggered by wrong permissions
+    :raises errors.Error: error if plugin command is not supported
+
+    """
+    return internal_main.main(cli_args)
