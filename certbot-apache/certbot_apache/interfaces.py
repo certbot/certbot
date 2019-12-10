a7de05718868a5e0983d42b9907bfe857f0c4b5f
Joona Hoikkala <joohoi@users.noreply.github.com>
2019-12-10 09:32:38 +0200

    Update certbot-apache/certbot_apache/interfaces.py
    Co-Authored-By: ohemorange <ebportnoy@gmail.com>

--- certbot-apache/certbot_apache/interfaces.py
+++ certbot-apache/certbot_apache/interfaces.py
@@ -507,7 +507,7 @@ class BlockNode(DirectiveNode):
     def parsed_paths(self):
         """
         Returns a list of file paths that have currently been parsed into the parser
-        tree. The returned list  may include paths with wildcard characters, for
+        tree. The returned list may include paths with wildcard characters, for
         example: ['/etc/apache2/conf.d/*.load']
 
         This is typically called on the root node of the ParserNode tree.
