1b55099059e254beb9e0cf7ef7f986634a59dc1e
Adrien Ferrand <ferrand.ad@gmail.com>
2019-12-11 21:30:15 +0100

    Update scripts

--- windows-installer/tasks-down.ps1
+++ windows-installer/tasks-down.ps1
@@ -1,15 +1,11 @@
 #Requires -RunAsAdministrator
-[CmdletBinding()]
-param()
-begin {}
-process {
-    $tasks = "Certbot Renew & Auto-Update Task", "Certbot Renew Task"
 
-    foreach ($task in $tasks) {
-        $exists = Get-ScheduledTask | Where-Object { $_.TaskName -like $task }
-        if ($exists)
-        {
-            Unregister-ScheduledTask -TaskName $task -Confirm:$false
-        }
+$tasks = "Certbot Renew & Auto-Update Task", "Certbot Renew Task"
+
+foreach ($task in $tasks) {
+    $exists = Get-ScheduledTask | Where-Object { $_.TaskName -like $task }
+    if ($exists)
+    {
+        Unregister-ScheduledTask -TaskName $task -Confirm:$false
     }
 }
--- windows-installer/tasks-up.ps1
+++ windows-installer/tasks-up.ps1
@@ -1,29 +1,27 @@
 #Requires -RunAsAdministrator
-[CmdletBinding()]
+
 param(
     [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
     [string]
     $InstallDir
 )
-begin {}
-process {
-    function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
-    $down = Join-Path (Get-ScriptDirectory) 'tasks-down.ps1'
-    & $down
 
-    $taskName = "Certbot Renew & Auto-Update Task"
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$down = Join-Path (Get-ScriptDirectory) 'tasks-down.ps1'
+& $down
 
-    $actionRenew = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -Command "certbot renew"'
-    $actionUpgrade = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -WindowStyle Hidden -File ""$InstallDir\auto-update.ps1"""
+$taskName = "Certbot Renew & Auto-Update Task"
 
-    $delay = New-TimeSpan -Hours 12
-    $triggerAM = New-ScheduledTaskTrigger -Daily -At 12am -RandomDelay $delay
-    $triggerPM = New-ScheduledTaskTrigger -Daily -At 12pm -RandomDelay $delay
-    # NB: For now scheduled task is set up under Administrators account because Certbot Installer installs Certbot for all users.
-    # If in the future we allow the Installer to install Certbot for one specific user, the scheduled task will need to
-    # switch to this user, since Certbot will be available only for him.
-    $adminSID = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-544")
-    $adminGroupID = $adminSID.Translate([System.Security.Principal.NTAccount]).Value
-    $principal = New-ScheduledTaskPrincipal -GroupId $adminGroupID -RunLevel Highest
-    Register-ScheduledTask -Action $actionRenew, $actionUpgrade -Trigger $triggerAM, $triggerPM -TaskName $taskName -Description "Execute twice a day the 'certbot renew' command, to renew managed certificates if needed." -Principal $principal
-}
+$actionRenew = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -Command "certbot renew"'
+$actionUpgrade = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -WindowStyle Hidden -File ""$InstallDir\auto-update.ps1"""
+
+$delay = New-TimeSpan -Hours 12
+$triggerAM = New-ScheduledTaskTrigger -Daily -At 12am -RandomDelay $delay
+$triggerPM = New-ScheduledTaskTrigger -Daily -At 12pm -RandomDelay $delay
+# NB: For now scheduled task is set up under Administrators account because Certbot Installer installs Certbot for all users.
+# If in the future we allow the Installer to install Certbot for one specific user, the scheduled task will need to
+# switch to this user, since Certbot will be available only for him.
+$adminSID = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-544")
+$adminGroupID = $adminSID.Translate([System.Security.Principal.NTAccount]).Value
+$principal = New-ScheduledTaskPrincipal -GroupId $adminGroupID -RunLevel Highest
+Register-ScheduledTask -Action $actionRenew, $actionUpgrade -Trigger $triggerAM, $triggerPM -TaskName $taskName -Description "Execute twice a day the 'certbot renew' command, to renew managed certificates if needed." -Principal $principal
