2d259baa47dc2fa145be6b319fe2406e54583408
Brad Warren <bmw@eff.org>
2019-12-10 17:29:39 -0800

    Release 1.1.0

--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -5,7 +5,7 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 
-version = '1.1.0.dev0'
+version = '1.1.0'
 
 # Remember to update local-oldest-requirements.txt when changing the minimum
 # acme/certbot version.
