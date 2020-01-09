c12baf7d8c873a1492088f844fe885a16bf19cd0
Brad Warren <bmw@eff.org>
2020-01-09 10:26:37 -0800

    Fix path to apache-conf-test

--- snap/test.sh
+++ snap/test.sh
@@ -7,5 +7,5 @@ venv/bin/python -m pytest certbot/certbot-ci/certbot_integration_tests -n 4
 
 # DO NOT RUN `apache-conf-test` LOCALLY, IT WILL BREAK YOUR APACHE CONFIGURATION!
 if [ -n "$TRAVIS" ]; then
-    venv/bin/python certbot/certbot-apache/certbot_apache/tests/apache-conf-files/apache-conf-test-pebble.py --debian-modules
+    venv/bin/python certbot/certbot-apache/tests/apache-conf-files/apache-conf-test-pebble.py --debian-modules
 fi
