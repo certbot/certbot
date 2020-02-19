83b73aeb93d664aaa8f1a081cae805ae5d9bcc4b
Joona Hoikkala <joohoi@users.noreply.github.com>
2020-02-19 17:14:07 +0200

    Update certbot-apache/setup.py
    Co-Authored-By: Brad Warren <bmw@users.noreply.github.com>

--- certbot-apache/setup.py
+++ certbot-apache/setup.py
@@ -10,7 +10,7 @@ version = '1.3.0.dev0'
 # acme/certbot version.
 install_requires = [
     'acme>=0.29.0',
-    'certbot>=1.2.0.dev0',
+    'certbot>=1.3.0.dev0',
     'mock',
     'python-augeas',
     'setuptools',
