59931672e2d6f4d5f35581ab11643d582e2446e1
Erica Portnoy <ebportnoy@gmail.com>
2020-03-19 19:03:36 -0700

    just use LooseVersion in test

--- tests/letstest/scripts/test_openssl_version.py
+++ tests/letstest/scripts/test_openssl_version.py
@@ -8,7 +8,7 @@ def main(openssl_version, apache_version):
     conf_file_location = "/etc/letsencrypt/options-ssl-apache.conf"
     with open(conf_file_location) as f:
         contents = f.read()
-    if tuple(apache_version.split(".")) < (2, 4, 11) or \
+    if LooseVersion(apache_version.strip()) < LooseVersion('2.4.11') or \
         LooseVersion(openssl_version.strip()) < LooseVersion('1.0.2l'):
         # should be old version
         # assert SSLSessionTickets not in conf file
