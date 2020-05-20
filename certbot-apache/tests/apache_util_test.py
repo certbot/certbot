47ba8ad09eb1a891b7af181747f84820b20b8691
Brad Warren <bmw@eff.org>
2020-05-20 14:50:05 -0700

    Create apache_util_test.py.

---
+++ certbot-apache/tests/apache_util_test.py
@@ -0,0 +1,15 @@
+"""Tests for certbot_apache._internal.apache_util."""
+import certbot.tests.util as test_util
+
+
+class CertFingerprintTest(unittest.TestCase):
+    """Tests for certbot_apache._internal.apache_util.cert_sha1_fingerprint"""
+
+    def test_cert_sha1_fingerprint(self):
+        from certbot_apache._internal.apache_util import cert_sha1_fingerprint
+
+        cert_path = test_util.vector_path('cert_512.pem')
+        self.assertEqual(
+            cert_sha1_fingerprint(cert_path),
+            b'\t\xf8\xce\x01E\r(\x84g\xc32j\xc0E~5\x199\xc7.'
+        )
--- certbot-apache/tests/ocsp_prefetch_test.py
+++ certbot-apache/tests/ocsp_prefetch_test.py
@@ -488,20 +488,6 @@ class OCSPPrefetchTest(util.ApacheTest):
         self.assertTrue(mock_rest.called)
 
 
-class CertFingerprintTest(unittest.TestCase):
-    """Tests for certbot_apache._internal.apache_util.cert_sha1_fingerprint"""
-
-    def test_cert_sha1_fingerprint(self):
-        import certbot.tests.util as test_util
-        from certbot_apache._internal.apache_util import cert_sha1_fingerprint
-
-        cert_path = test_util.vector_path('cert_512.pem')
-        self.assertEqual(
-            cert_sha1_fingerprint(cert_path),
-            b'\t\xf8\xce\x01E\r(\x84g\xc32j\xc0E~5\x199\xc7.'
-        )
-
-
 def _read_dbm(filename):
 
     """Helper method for reading the dbm using context manager.
