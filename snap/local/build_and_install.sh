b501ec0f3c6274e3f5fd8ddb41b64211f0433e05
Adrien Ferrand <ferrand.ad@gmail.com>
2020-05-21 23:27:00 +0200

    Backport https://github.com/basak/certbot-snap-build/pull/27 into Certbot project

--- snap/local/build_and_install.sh
+++ snap/local/build_and_install.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
-set -ex
+set -emx
 
-if [[ -z "$TRAVIS" ]]; then
+if [[ -z "${TRAVIS}" ]]; then
     echo "This script makes global changes to the system it is run on so should only be run in CI."
     exit 1
 fi
 
-sudo /snap/bin/lxd.migrate -yes
-sudo /snap/bin/lxd waitready
-sudo /snap/bin/lxd init --auto
-tools/strip_hashes.py letsencrypt-auto-source/pieces/dependency-requirements.txt > constraints.txt
-sudo snapcraft --use-lxd
-sudo snap install --dangerous --classic *.snap
+SNAP_ARCH=$1
+
+if [[ -z "${SNAP_ARCH}" ]]; then
+    echo "You need to specify the target architecture"
+    exit 1
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+"${SCRIPT_DIR}/builder/prepare.sh" "${SNAP_ARCH}"
+
+docker run --net=host -d --rm -v "${SCRIPT_DIR}/packages:/data/packages" --name pypiserver pypiserver/pypiserver
+tools/strip_hashes.py letsencrypt-auto-source/pieces/dependency-requirements.txt > snap-constraints.txt
+
+function cleanup() {
+    docker rm --force pypiserver
+}
+
+trap cleanup EXIT
+
+docker run \
+  --rm \
+  --net=host \
+  -v "$(pwd):$(pwd)" \
+  -w "$(pwd)" \
+  -e "PIP_EXTRA_INDEX_URL=http://localhost:8080/simple" \
+  -t "builder:${SNAP_ARCH}" \
+  snapcraft
