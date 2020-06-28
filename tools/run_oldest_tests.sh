4dcb7ad7b773b333f7b2afae20e86174ae7e7303
Adrien Ferrand <ferrand.ad@gmail.com>
2020-06-28 22:01:33 +0200

    Smarter build of oldest tests

--- tools/run_oldest_tests.sh
+++ tools/run_oldest_tests.sh
@@ -12,29 +12,28 @@ function cleanup() {
 
 trap cleanup EXIT
 
-SCRIPT=$(mktemp /tmp/test-script.XXXXXX)
-chmod +x "${SCRIPT}"
+DOCKERFILE=$(mktemp /tmp/Dockerfile.XXXXXX)
 
-cat << "EOF" >> "${SCRIPT}"
-#!/bin/bash
-set -e
-apt-get update
-apt-get install -y --no-install-recommends \
-    python-dev python-pip git gcc libaugeas0 libssl-dev libffi-dev \
-    ca-certificates nginx-light openssl curl software-properties-common
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BB92C09DB82666C
-add-apt-repository ppa:fkrull/deadsnakes-python2.7
-apt-get update
-apt-get upgrade -y
-sh <(curl -fsSL https://get.docker.com)
-python -m pip install --upgrade pip virtualenv wheel
-python -m pip install tox
-python -m tox
+cat << "EOF" >> "${DOCKERFILE}"
+FROM ubuntu:14.04
+RUN apt-get update \
+ && apt-get install -y --no-install-recommends \
+        python-dev python-pip git gcc libaugeas0 libssl-dev libffi-dev \
+        ca-certificates nginx-light openssl curl software-properties-common \
+ && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BB92C09DB82666C \
+ && add-apt-repository ppa:fkrull/deadsnakes-python2.7 \
+ && apt-get update \
+ && apt-get upgrade -y \
+ && curl -fsSL https://get.docker.com | bash /dev/stdin \
+ && python -m pip install --upgrade pip virtualenv wheel \
+ && python -m pip install tox \
+ && rm -rf /var/lib/apt/lists/*
 EOF
 
+docker build -f "${DOCKERFILE}" -t oldest-worker "${DIR}"
 docker run --rm --network=host -w "${PWD}" \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "${PWD}:${PWD}" -v "${SCRIPT}:/script.sh" \
   -v /tmp:/tmp \
   -e TOXENV -e ACME_SERVER -e PYTEST_ADDOPTS \
-  ubuntu:14.04 /script.sh
+  oldest-worker python -m tox
