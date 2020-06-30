36d6bc6c8a54fe33a23c5aa315dbc0e9a0598cd4
Adrien Ferrand <ferrand.ad@gmail.com>
2020-06-30 11:50:57 +0200

    Fixes

--- snap/local/build.sh
+++ snap/local/build.sh
@@ -2,7 +2,7 @@
 # Cross-compile the Certbot snap from local sources for the specified architecture,
 # and install it if this architecture is also the the current machine one.
 # This script is designed for CI tests purpose.
-# Usage: build_and_install.sh [amd64,arm64,armhf]
+# Usage: build.sh [amd64,arm64,armhf]
 set -ex
 
 SNAP_ARCH=$1
