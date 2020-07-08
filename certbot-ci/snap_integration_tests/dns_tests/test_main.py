a0da6a5025daccae005234b307fceee7b114cc6b
Adrien Ferrand <ferrand.ad@gmail.com>
2020-07-08 22:38:29 +0200

    Integrate DNS snap tests into certbot-ci

--- certbot-ci/snap_integration_tests/dns_tests/test_main.py
+++ certbot-ci/snap_integration_tests/dns_tests/test_main.py
@@ -6,33 +6,12 @@ import os
 import re
 
 
-def _get_snap_directory():
-    snap_directory = os.environ.get('SNAP_FOLDER')
-    if not snap_directory:
-        raise ValueError('Error, SNAP_FOLDER environment variable is not set.')
-
-    return snap_directory
-
-
-def _list_dns_snaps_paths():
-    snap_directory = _get_snap_directory()
-    if not snap_directory:
-        raise ValueError('Error, SNAP_FOLDER environment variable is not set.')
-
-    return glob.glob(os.path.join(snap_directory, 'certbot-dns-*_*.snap'))
-
-
-def _extract_plugin_name(dns_snap_path):
-    return re.match(r'^certbot-(dns-\w+)_.*\.snap$', os.path.basename(dns_snap_path)).group(1)
-
-
 @pytest.fixture(autouse=True, scope="module")
-def install_certbot_snap():
+def install_certbot_snap(request):
     with pytest.raises(Exception):
         subprocess.check_call(['certbot', '--version'])
     try:
-        snap_directory = _get_snap_directory()
-        snap_path = glob.glob(os.path.join(snap_directory, 'certbot_*.snap'))[0]
+        snap_path = glob.glob(os.path.join(request.config.snap_folder, 'certbot_*.snap'))[0]
         subprocess.check_call(['snap', 'install', '--classic', '--dangerous', snap_path])
         subprocess.check_call(['certbot', '--version'])
         yield
@@ -40,13 +19,13 @@ def install_certbot_snap():
         subprocess.call(['snap', 'remove', 'certbot'])
 
 
-@pytest.mark.parametrize('dns_snap_path', _list_dns_snaps_paths())
-def test_it(dns_snap_path):
+def test_dns_plugin_install(dns_snap_path):
     """
     Test that each DNS plugin Certbot snap available in SNAP_FOLDER
     can be installed and is usable with the Certbot snap.
     """
-    plugin_name = _extract_plugin_name(dns_snap_path)
+    plugin_name = re.match(r'^certbot-(dns-\w+)_.*\.snap$',
+                           os.path.basename(dns_snap_path)).group(1)
     snap_name = 'certbot-{0}'.format(plugin_name)
     assert plugin_name not in subprocess.check_output(['certbot', 'plugins', '--prepare'],
                                                       universal_newlines=True)
