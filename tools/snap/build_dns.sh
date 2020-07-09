d434b92945f04f84b10fee1048cb45a15d3a36ba
Adrien Ferrand <adferrand@users.noreply.github.com>
2020-07-09 20:33:25 +0200

    Build the DNS plugins snaps (#8129)
    Fixes #8041
    
    This PR makes Azure Pipeline build the DNS plugins snaps for the 3 architectures during the CI.
    
    It leverages the existing logic for building the Certbot snap in order to deploy a QEMU environment with Docker, and leverages the local PyPI index to speed up the build when installing `cffi` and `cryptography`.
    
    All DNS plugins snaps are constructed in one unique docker container, in order to save the time required to install the system dependencies upon first start of `snapcraft`, and so speed up significantly the build.
    
    Finally, all `amd64` DNS plugins snaps are built within 6 minutes. For `arm64` and `armhf`, it is around 40 mins: this is quite fast in fact, considering that 14 DNS plugins snaps are built.
    
    However, this is still an extremely heavy task to make the full 3 architectures builds, even for Azure Pipelines and its 10 parallel jobs capability. That is why I make the `arm64` and `armhf` builds be skipped for the `full-test-suite`, and let them run only for `nightly` and `release`. This means however that these builds will not be done for the release branches. If this is a problem, I can put a more elaborate suspend condition to triggers the builds in this case.
    
    All snaps are stored in the pipeline artifacts storage, making them available for publication during a `release` pipeline.
    
    The PR is set as Draft for now, because I use temporarily `pr_test-suite` to validate the packaging jobs when commits are pushed. Once the PR is ready, I will revert it back to the normal configuration (run the standard tests).
    
    * Configure a script to build DNS snaps
    
    * Focus on packaging
    
    * Trigger all architectures
    
    * Add extra index
    
    * Prepare conditional suspend
    
    * Set final suspend logic
    
    * Set final suspend value
    
    * Loop for publication
    
    * Use python3
    
    * Clean before build
    
    * Add a test
    
    * Add test job in Azure
    
    * Preserve env
    
    * Apply normal config for pipelines
    
    * Skip QEMU jobs only for test branches
    
    * Makes snap run tests depends also on the Certbot snap build
    
    * Update .azure-pipelines/templates/jobs/packaging-jobs.yml
    
    Co-authored-by: Brad Warren <bmw@users.noreply.github.com>
    
    * Update .azure-pipelines/templates/stages/deploy-stage.yml
    
    Co-authored-by: Brad Warren <bmw@users.noreply.github.com>
    
    * More accurate way to get the plugin snap name
    
    * Integrate DNS snap tests into certbot-ci
    
    * Fixes
    
    * Update certbot-ci/snap_integration_tests/conftest.py
    
    Co-authored-by: Brad Warren <bmw@users.noreply.github.com>
    
    * Update certbot-ci/snap_integration_tests/conftest.py
    
    Co-authored-by: Brad Warren <bmw@users.noreply.github.com>
    
    * Clean an _init_.py file
    
    Co-authored-by: Brad Warren <bmw@users.noreply.github.com>

---
+++ tools/snap/build_dns.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Cross-compile the specified Certbot DNS plugins snaps from local sources for the specified architecture.
+# This script is designed for CI tests purpose.
+# Usage: build.sh [amd64,arm64,armhf] [DNS_PLUGIN1,DNS_PLUGIN2 or ALL]
+set -ex
+
+SNAP_ARCH=$1
+DNS_PLUGINS=$2
+
+if [[ -z "${SNAP_ARCH}" ]]; then
+    echo "You need to specify the target architecture"
+    exit 1
+fi
+
+if [[ -z "${DNS_PLUGINS}" ]]; then
+    echo "You need to specify the DNS plugins"
+    exit 1
+fi
+
+if [[ "${DNS_PLUGINS}" = "ALL" ]]; then
+    DNS_PLUGINS=$(find . -maxdepth 1 -type d -name "certbot-dns-*" -exec basename {} \; | paste -sd "," -)
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CERTBOT_DIR="$(dirname "$(dirname "${DIR}")")"
+
+# shellcheck source=common.sh
+source "${DIR}/common.sh"
+
+RegisterQemuHandlers
+ResolveArch "${SNAP_ARCH}"
+
+pushd "${DIR}/packages"
+"${CERTBOT_DIR}/tools/simple_http_server.py" 8080 >/dev/null 2>&1 &
+HTTP_SERVER_PID="$!"
+popd
+
+function cleanup() {
+    kill "${HTTP_SERVER_PID}"
+}
+
+trap cleanup EXIT
+
+SCRIPT=$(mktemp /tmp/script.XXXXXX.sh)
+chmod +x "${SCRIPT}"
+
+SNAP_CONSTRAINTS=$(mktemp /tmp/snap-constraints.XXXXXX.txt)
+python3 tools/strip_hashes.py letsencrypt-auto-source/pieces/dependency-requirements.txt | grep -v python-augeas > "${SNAP_CONSTRAINTS}"
+
+cat << "EOF" >> "${SCRIPT}"
+#!/bin/bash
+set -ex
+IFS=","
+for DNS_PLUGIN in ${DNS_PLUGINS}; do
+  pushd "${DNS_PLUGIN}"
+  cp /snap-constraints.txt .
+  snapcraft clean
+  snapcraft
+  popd
+done
+EOF
+
+docker run \
+  --rm \
+  --net=host \
+  -v "${CERTBOT_DIR}:/certbot" \
+  -v "${SCRIPT}:/script.sh" \
+  -v "${SNAP_CONSTRAINTS}:/snap-constraints.txt" \
+  -w "/certbot" \
+  -e "DNS_PLUGINS=${DNS_PLUGINS}" \
+  -e "PIP_EXTRA_INDEX_URL=http://localhost:8080" \
+  "adferrand/snapcraft:${DOCKER_ARCH}-stable" \
+  /script.sh
