e42f782525e9d5ef1cae947efcbd65a27eafdd23
Adrien Ferrand <adferrand@users.noreply.github.com>
2020-07-27 19:29:38 +0200

    Update tools/snap/build_remote.py
    Co-authored-by: Brad Warren <bmw@users.noreply.github.com>

--- tools/snap/build_remote.py
+++ tools/snap/build_remote.py
@@ -58,7 +58,7 @@ def _build_snap(target, archs, status, lock):
                     break
 
             if failed_archs:
-                # We expect for each failed builds to have a build output, or something bad happened.
+                # We expect each failed build to have a log file, or something bad happened.
                 missing_outputs = False
                 for arch in failed_archs:
                     if not exists(join(workspace, f'{target}_{arch}.txt')):
