f52008804375e4acc6020a70c0c910e6f83594ca
Brad Warren <bmw@eff.org>
2020-08-12 17:19:46 -0700

    update python_requires

--- certbot-compatibility-test/setup.py
+++ certbot-compatibility-test/setup.py
@@ -38,7 +38,7 @@ setup(
     author="Certbot Project",
     author_email='client-dev@letsencrypt.org',
     license='Apache License 2.0',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
