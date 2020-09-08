ec147740eef886cef98de90e058e08215f60f015
Brad Warren <bmw@eff.org>
2020-09-08 09:59:33 -0700

    Bump version to 1.9.0

--- certbot-dns-cloudxns/setup.py
+++ certbot-dns-cloudxns/setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-version = '1.8.0'
+version = '1.9.0.dev0'
 
 # Remember to update local-oldest-requirements.txt when changing the minimum
 # acme/certbot version.
