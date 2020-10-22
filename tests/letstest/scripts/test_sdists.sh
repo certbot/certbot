50ef97bb236dd4a4d5ff0cb426f7c7c8fa56353d
Brad Warren <bmw@eff.org>
2020-10-22 16:21:32 -0700

    Strip enum34 dependency.

--- tests/letstest/scripts/test_sdists.sh
+++ tests/letstest/scripts/test_sdists.sh
@@ -18,8 +18,13 @@ fi
 # We strip the hashes because the venv creation script includes unhashed
 # constraints in the commands given to pip and the mix of hashed and unhashed
 # packages makes pip error out.
-python3 tools/strip_hashes.py letsencrypt-auto-source/pieces/dependency-requirements.txt > unhashed-requirements.txt
-CERTBOT_PIP_NO_BINARY=:all: tools/venv3.py --requirement unhashed-requirements.txt
+python3 tools/strip_hashes.py letsencrypt-auto-source/pieces/dependency-requirements.txt > requirements.txt
+# We also strip out the requirement for enum34 because it cannot be installed
+# in newer versions of Python 3, tools/strip_hashes.py removes the environment
+# marker that'd normally prevent it from being installed, and this package is
+# not needed for any OS tested here.
+sed -i '/enum34/d' requirements.txt
+CERTBOT_PIP_NO_BINARY=:all: tools/venv3.py --requirement requirements.txt
 . "$VENV_PATH/bin/activate"
 # pytest is needed to run tests on some of our packages so we install a pinned version here.
 tools/pip_install.py pytest
