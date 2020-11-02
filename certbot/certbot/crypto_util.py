713c861c73e88691692597c412dea64c9c57dc79
Brad Warren <bmw@eff.org>
2020-11-02 15:17:32 -0800

    Remove unused import.

--- certbot/certbot/crypto_util.py
+++ certbot/certbot/crypto_util.py
@@ -25,7 +25,6 @@ import zope.component
 
 from acme import crypto_util as acme_crypto_util
 from acme.magic_typing import IO  # pylint: disable=unused-import
-from acme.magic_typing import Union
 from certbot import errors
 from certbot import interfaces
 from certbot import util
