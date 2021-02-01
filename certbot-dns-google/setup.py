1a53cfbb7807411a5f5b7e8af522ac2904577e7c
Brad Warren <bmw@eff.org>
2021-02-01 15:10:54 -0800

    remove mock dependency in plugins

--- certbot-dns-google/setup.py
+++ certbot-dns-google/setup.py
@@ -1,8 +1,6 @@
-from distutils.version import LooseVersion
 import os
 import sys
 
-from setuptools import __version__ as setuptools_version
 from setuptools import find_packages
 from setuptools import setup
 
@@ -29,15 +27,6 @@ elif 'bdist_wheel' in sys.argv[1:]:
                        'to include certbot dependencies.')
 if os.environ.get('SNAP_BUILD'):
     install_requires.append('packaging')
-
-setuptools_known_environment_markers = (LooseVersion(setuptools_version) >= LooseVersion('36.2'))
-if setuptools_known_environment_markers:
-    install_requires.append('mock ; python_version < "3.3"')
-elif 'bdist_wheel' in sys.argv[1:]:
-    raise RuntimeError('Error, you are trying to build certbot wheels using an old version '
-                       'of setuptools. Version 36.2+ of setuptools is required.')
-elif sys.version_info < (3,3):
-    install_requires.append('mock')
 
 docs_extras = [
     'Sphinx>=1.0',  # autodoc_member_order = 'bysource', autodoc_default_flags
