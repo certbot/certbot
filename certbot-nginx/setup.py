d3d9a05826af027b31de5cb60948dafbf6597873
Brad Warren <bmw@users.noreply.github.com>
2021-05-03 12:38:54 -0700

    fix client email address (#8817)
    client-dev@letsencrypt.org is no longer used by the Certbot team so this PR updates the email address in our packages to our current mailing list.

--- certbot-nginx/setup.py
+++ certbot-nginx/setup.py
@@ -20,7 +20,7 @@ setup(
     description="Nginx plugin for Certbot",
     url='https://github.com/letsencrypt/letsencrypt',
     author="Certbot Project",
-    author_email='client-dev@letsencrypt.org',
+    author_email='certbot-dev@eff.org',
     license='Apache License 2.0',
     python_requires='>=3.6',
     classifiers=[
