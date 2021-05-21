95ae5c31baad16f1a7b36cb9d57632f0b92b1f16
Brad Warren <bmw@eff.org>
2021-05-21 13:44:27 -0700

    remove unused import

--- certbot/certbot/_internal/renewal.py
+++ certbot/certbot/_internal/renewal.py
@@ -14,7 +14,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
-import OpenSSL
 import zope.component
 
 from certbot import crypto_util
