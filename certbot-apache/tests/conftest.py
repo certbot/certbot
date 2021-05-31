00d66d5229364df830f78659edcf5654f471983e
Adrien Ferrand <ferrand.ad@gmail.com>
2021-05-31 21:31:28 +0200

    Skip coverage on conftest

--- certbot-apache/tests/conftest.py
+++ certbot-apache/tests/conftest.py
@@ -1,6 +1,6 @@
 import sys
 
-def pytest_ignore_collect(path, config):
+def pytest_ignore_collect(path, config):  # pragma: no cover
     # Do not run any test for certbot-apache on Windows.
     if sys.platform == 'win32':
         return True
