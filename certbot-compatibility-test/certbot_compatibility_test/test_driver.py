a4959d4f7e37f1a1ca2108802ecfec8eda6f2303
Adrien Ferrand <ferrand.ad@gmail.com>
2021-06-05 00:16:46 +0200

    Fixing circular dependencies

--- certbot-compatibility-test/certbot_compatibility_test/test_driver.py
+++ certbot-compatibility-test/certbot_compatibility_test/test_driver.py
@@ -19,6 +19,7 @@ from acme import crypto_util
 from acme import messages
 from certbot import achallenges
 from certbot import errors as le_errors
+from certbot.display import util as display_util
 from certbot._internal.display import obj as display_obj
 from certbot.tests import acme_util
 from certbot_compatibility_test import errors
@@ -330,7 +331,7 @@ def setup_logging(args):
 
 def setup_display():
     """"Prepares IDisplay for the Certbot plugins """
-    displayer = display_obj.NoninteractiveDisplay(sys.stdout)
+    displayer = display_util.NoninteractiveDisplay(sys.stdout)
     display_obj.set_display(displayer)
 
 
