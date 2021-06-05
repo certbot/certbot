f48fa3809397b826f63ece482763980eeaaefe01
OsirisInferi <github@flut.demon.nl>
2021-06-05 11:32:04 +0200

    Pleasing mypy

--- certbot/certbot/_internal/plugins/disco.py
+++ certbot/certbot/_internal/plugins/disco.py
@@ -48,9 +48,11 @@ class PluginEntryPoint:
 
     def __init__(self, entry_point: pkg_resources.EntryPoint, with_prefix=False):
         self.name = self.entry_point_to_plugin_name(entry_point, with_prefix)
-        self.version = entry_point.dist.version
         self.plugin_cls: interfaces.IPluginFactory = entry_point.load()
         self.entry_point = entry_point
+        self.version: Optional[str] = None
+        if entry_point.dist is not None:
+            self.version = entry_point.dist.version
         self.warning_message: Optional[str] = None
         self._initialized: Optional[interfaces.IPlugin] = None
         self._prepared: Optional[Union[bool, Error]] = None
