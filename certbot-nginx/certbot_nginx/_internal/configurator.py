0c637860cd2193d1b988d3752c8abef328a08e65
alexzorin <alex@zorin.id.au>
2021-06-11 08:58:11 +1000

    cli: improve error messages for enhance errors (#8884)
    * cli: improve error messages for enhance errors
    
    * remove status message after enhance config revert

--- certbot-nginx/certbot_nginx/_internal/configurator.py
+++ certbot-nginx/certbot_nginx/_internal/configurator.py
@@ -769,9 +769,6 @@ class NginxConfigurator(common.Installer):
         except (KeyError, ValueError):
             raise errors.PluginError(
                 "Unsupported enhancement: {0}".format(enhancement))
-        except errors.PluginError:
-            logger.error("Failed %s for %s", enhancement, domain)
-            raise
 
     def _has_certbot_redirect(self, vhost, domain):
         test_redirect_block = _test_block_from_block(_redirect_block_for_domain(domain))
@@ -791,6 +788,10 @@ class NginxConfigurator(common.Installer):
         :raises .errors.PluginError: If no viable HTTPS host can be created or
             set with header header_substring.
         """
+        if not header_substring in constants.HEADER_ARGS:
+            raise errors.NotSupportedError(
+                f"{header_substring} is not supported by the nginx plugin.")
+
         vhosts = self.choose_vhosts(domain)
         if not vhosts:
             raise errors.PluginError(
