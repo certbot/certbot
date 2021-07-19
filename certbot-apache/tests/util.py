f57cd85d5e62e8f8a850b77a4e3fd6ce4d605f15
Adrien Ferrand <ferrand.ad@gmail.com>
2021-07-20 01:07:34 +0200

    Avoid relying on internal certbot packages from certbot-apache

--- certbot-apache/tests/util.py
+++ certbot-apache/tests/util.py
@@ -15,7 +15,6 @@ from certbot.compat import os
 from certbot.plugins import common
 from certbot.tests import util as test_util
 from certbot.display import util as display_util
-from certbot._internal.display import obj as display_obj
 from certbot_apache._internal import configurator
 from certbot_apache._internal import entrypoint
 from certbot_apache._internal import obj
@@ -69,8 +68,6 @@ class ParserTest(ApacheTest):
               config_root="debian_apache_2_4/multiple_vhosts/apache2",
               vhost_root="debian_apache_2_4/multiple_vhosts/apache2/sites-available"):
         super().setUp(test_dir, config_root, vhost_root)
-
-        display_obj.set_display(display_util.FileDisplay(sys.stdout, False))
 
         from certbot_apache._internal.parser import ApacheParser
         self.aug = augeas.Augeas(
