4b8ad7ddee5a4b2d76df578ba8e7bf93f1db2e5f
Brad Warren <bmw@eff.org>
2021-09-09 14:50:00 -0700

    update min josepy dep

--- certbot/setup.py
+++ certbot/setup.py
@@ -52,9 +52,9 @@ install_requires = [
     'configobj>=5.0.6',
     'cryptography>=2.1.4',
     'distro>=1.0.1',
-    # 1.1.0+ is required to avoid the warnings described at
-    # https://github.com/certbot/josepy/issues/13.
-    'josepy>=1.1.0',
+    # 1.9.0+ is needed to avoid problems with EC keys. See
+    # https://github.com/certbot/josepy/issues/109.
+    'josepy>=1.9.0',
     'parsedatetime>=2.4',
     'pyrfc3339',
     'pytz',
