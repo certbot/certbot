6bd97197ab3ffd7d27aaf161b0e778b7a86dd530
Adrien Ferrand <ferrand.ad@gmail.com>
2021-09-10 21:53:49 +0200

    Increase minimum of josepy version to use and update the oldest contraints.

--- acme/setup.py
+++ acme/setup.py
@@ -7,10 +7,7 @@ version = '1.20.0.dev0'
 
 install_requires = [
     'cryptography>=2.1.4',
-    # formerly known as acme.jose:
-    # 1.1.0+ is required to avoid the warnings described at
-    # https://github.com/certbot/josepy/issues/13.
-    'josepy>=1.1.0',
+    'josepy>=1.9.0',
     'PyOpenSSL>=17.3.0',
     'pyrfc3339',
     'pytz',
