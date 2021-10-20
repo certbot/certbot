c6924d8a15ad81d947fa935704589262dbcbddcd
Chris Swan <478926+cpswan@users.noreply.github.com>
2021-10-20 17:21:18 +0100

    Use {datetime.now().year} vs hard coded 2021

--- acme/docs/conf.py
+++ acme/docs/conf.py
@@ -14,6 +14,7 @@
 
 import os
 import sys
+from datetime import datetime
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -58,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'acme-python'
-copyright = u'2015-2021, Let\'s Encrypt Project'
+copyright = f"2015-{datetime.now().year}, Let's Encrypt Project"
 author = u'Let\'s Encrypt Project'
 
 # The version info for the project you're documenting, acts as replacement for
