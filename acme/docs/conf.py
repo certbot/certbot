cdbc264bb62ddcc1d4810584919f21ef714ad13e
Chris Swan <478926+cpswan@users.noreply.github.com>
2021-10-25 20:20:57 +0100

    Fix copyright date s/2015-2015/2015/ (#9070)

--- acme/docs/conf.py
+++ acme/docs/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'acme-python'
-copyright = u'2015-2015, Let\'s Encrypt Project'
+copyright = u'2015, Let\'s Encrypt Project'
 author = u'Let\'s Encrypt Project'
 
 # The version info for the project you're documenting, acts as replacement for
