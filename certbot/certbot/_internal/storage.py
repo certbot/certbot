df21ce7d091c1ea916158ac11b2c265dd0400c3e
Brad Warren <bmw@eff.org>
2021-11-01 10:22:02 -0700

    add missing import

--- certbot/certbot/_internal/storage.py
+++ certbot/certbot/_internal/storage.py
@@ -12,6 +12,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 import parsedatetime
+import pkg_resources
 import pytz
 
 import certbot
