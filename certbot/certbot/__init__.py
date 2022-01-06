0e247a7bb731e445d2cdd1402644e7e3b8765aa8
Adrien Ferrand <ferrand.ad@gmail.com>
2022-01-06 21:49:10 +0100

    Deprecate Python 3.6 support.

--- certbot/certbot/__init__.py
+++ certbot/certbot/__init__.py
@@ -1,3 +1,13 @@
 """Certbot client."""
 # version number like 1.2.3a0, must have at least 2 parts, like 1.2
+import sys
+import warnings
+
 __version__ = '1.23.0.dev0'
+
+if sys.version_info[:2] == (3, 6):
+    warnings.warn(
+            "Python 3.6 support will be dropped in the next release of "
+            "certbot. Please upgrade your Python version.",
+            PendingDeprecationWarning,
+    )  # pragma: no cover
