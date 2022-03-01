3d3077e1f17942d0b77453480efd6e16a71133bb
Erica Portnoy <ebportnoy@gmail.com>
2022-03-01 12:34:21 -0800

    Release 1.24.0

--- certbot-apache/setup.py
+++ certbot-apache/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.24.0.dev0'
+version = '1.24.0'
 
 install_requires = [
     # We specify the minimum acme and certbot version as the current plugin
