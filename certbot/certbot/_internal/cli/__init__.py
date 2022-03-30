ce13317a432022b647e007568a726283d1b28d60
Amir Omidi <amir@aaomidi.com>
2022-03-30 14:28:20 -0400

    Oops, commit this file too

--- certbot/certbot/_internal/cli/__init__.py
+++ certbot/certbot/_internal/cli/__init__.py
@@ -372,10 +372,10 @@ def prepare_and_parse_args(plugins: plugins_disco.PluginsRegistry, args: List[st
         default=flag_default("issuance_timeout"),
         help=config_help("issuance_timeout"))
     helpful.add(
-        [None, "certonly", "run"], "--certificate-expiration", type=nonnegative_int,
-        dest="certificate_expiration",
-        default=flag_default("certificate_expiration"),
-        help=config_help("certificate_expiration"))
+        [None, "certonly", "run"], "--certificate-validity", type=nonnegative_int,
+        dest="certificate_validity",
+        default=flag_default("certificate_validity"),
+        help=config_help("certificate_validity"))
     helpful.add(
         "renew", "--pre-hook",
         help="Command to be run in a shell before obtaining any certificates."
