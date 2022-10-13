8b1b389cbb612a53ae346729b0ed0982d7b02f90
Erica Portnoy <ebportnoy@gmail.com>
2022-10-12 18:21:09 -0700

    only load cert once

--- certbot/certbot/_internal/main.py
+++ certbot/certbot/_internal/main.py
@@ -1692,18 +1692,13 @@ def reconfigure(config: configuration.NamespaceConfig,
             custom_prompt=certname_question)[0]
 
     certname = config.certname
-    lineage = cert_manager.lineage_for_certname(config, certname)
 
-    if not lineage:
-        raise errors.ConfigurationError("An existing certificate for the given name could not "
+    try:
+        renewal_file = storage.renewal_file_for_certname(config, certname)
+    except errors.CertStorageError:
+        raise errors.ConfigurationError(f"An existing certificate with name {certname} could not "
             "be found. Run `certbot certificates` to list available certificates.")
 
-    # If auth plugin wasn't set in config, get it from the existing lineage
-    # req_auth, _ = plug_sel.cli_plugin_requests(config)
-    # installer = auth = None
-
-    #if not req_auth:
-    renewal_file = storage.renewal_file_for_certname(config, certname)
     lineage_config = copy.deepcopy(config)
     try:
         renewal_candidate = renewal._reconstitute(lineage_config, renewal_file)
@@ -1712,15 +1707,12 @@ def reconfigure(config: configuration.NamespaceConfig,
     if not renewal_candidate:
         raise errors.ConfigurationError("asfdads")
     installer, auth = plug_sel.choose_configurator_plugins(lineage_config, plugins, "certonly")
-    # else:
-    #     # if it was set, get it from the cli config
-    #     installer, auth = plug_sel.choose_configurator_plugins(config, plugins, "certonly")
-
 
     le_client = _init_le_client(lineage_config, auth, installer)
 
     # renews cert as dry run
-    lineage = _get_and_save_cert(le_client, lineage_config, certname=certname, lineage=lineage)
+    lineage = _get_and_save_cert(le_client, lineage_config, certname=certname,
+        lineage=renewal_candidate)
 
     lineage.save_new_config_values(lineage_config)
 
