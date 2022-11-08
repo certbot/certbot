fd22bd0f6615c25dbb657319e3f13a8fc60dade0
Erica Portnoy <ebportnoy@gmail.com>
2022-11-08 15:23:35 -0800

    Bump version to 1.33.0

--- acme/setup.py
+++ acme/setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.32.0'
+version = '1.33.0.dev0'
 
 install_requires = [
     'cryptography>=2.5.0',
--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.32.0'
+version = '1.33.0.dev0'
 
 install_requires = [
     # This version of lexicon is required to address the problem described in
