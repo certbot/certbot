1e615138595916f21c5d91f9d1b9843af2b9502c
Will Greenberg <willg@eff.org>
2022-11-21 09:59:06 -0800

    Bump version to 2.1.0

--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.0.0'
+version = '2.1.0.dev0'
 
 install_requires = [
     # This version of lexicon is required to address the problem described in
