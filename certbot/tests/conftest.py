d792d398134f4f8b67debaf82e6e75635409c627
Brad Warren <bmw@users.noreply.github.com>
2023-02-08 14:24:07 -0800

    reset set_by_cli between each test (#9567)

---
+++ certbot/tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+from certbot._internal import cli
+
+@pytest.fixture(autouse=True)
+def reset_cli_global():
+    cli.set_by_cli.detector = None
--- certbot/tests/main_test.py
+++ certbot/tests/main_test.py
@@ -422,7 +422,6 @@ class RevokeTest(test_util.TempDirTestCase):
         if not args:
             args = 'revoke --cert-path={0} '
             args = args.format(self.tmp_cert_path).split()
-        cli.set_by_cli.detector = None # required to reset set_by_cli state
         plugins = disco.PluginsRegistry.find_all()
         config = configuration.NamespaceConfig(
             cli.prepare_and_parse_args(plugins, args))
@@ -588,7 +587,6 @@ class ReconfigureTest(test_util.TempDirTestCase):
 
     def _call(self, passed_args):
         full_args = passed_args + ['--config-dir', self.config_dir]
-        cli.set_by_cli.detector = None # required to reset set_by_cli state
         plugins = disco.PluginsRegistry.find_all()
         config = configuration.NamespaceConfig(
             cli.prepare_and_parse_args(plugins, full_args))
