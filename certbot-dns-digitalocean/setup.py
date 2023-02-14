d4b2d3202bb7cb58d3e24bcc95b104b102600005
Will Greenberg <willg@eff.org>
2023-02-14 12:44:31 -0800

    Release 2.3.0

--- certbot-dns-digitalocean/setup.py
+++ certbot-dns-digitalocean/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.3.0.dev0'
+version = '2.3.0'
 
 install_requires = [
     'python-digitalocean>=1.11', # 1.15.0 or newer is recommended for TTL support
