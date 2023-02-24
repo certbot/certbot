7c840a7dfdef2500f45999cc33c4544a4a822385
Alex Zorin <alex@zorin.au>
2023-02-25 08:45:19 +1100

    Revert "nginx: on encountering lua directives, produce a better warning (#9475)"
    This reverts commit c178fa8c0be8266a6edd9be5973bc1c135734c38.

--- certbot-nginx/tests/nginxparser_test.py
+++ certbot-nginx/tests/nginxparser_test.py
@@ -14,7 +14,6 @@ from certbot_nginx._internal.nginxparser import load
 from certbot_nginx._internal.nginxparser import loads
 from certbot_nginx._internal.nginxparser import RawNginxParser
 from certbot_nginx._internal.nginxparser import UnspacedList
-from certbot_nginx._internal.nginxparser import UnsupportedDirectiveException
 import test_util as util
 
 FIRST = operator.itemgetter(0)
@@ -357,68 +356,6 @@ class TestRawNginxParser(unittest.TestCase):
         parsed = loads("")
         assert parsed == []
 
-    def test_lua(self):
-        # https://github.com/certbot/certbot/issues/9066
-        with pytest.raises(UnsupportedDirectiveException):
-            loads("""
-        location /foo {
-            content_by_lua_block {
-                ngx.say('Hello World')
-            }
-        }
-        """)
-
-        # Without leading whitespace
-        with pytest.raises(UnsupportedDirectiveException):
-            loads("""
-        location /foo {content_by_lua_block {
-                ngx.say('Hello World')
-            }
-        }
-        """)
-
-        # Doesn't trigger if it's commented or not in the right position.
-        parsed = loads("""
-        location /foo {server_name content_by_lua_block;
-            #content_by_lua_block {
-            # ngx.say('Hello World')
-            # }
-        }
-        """)
-        assert parsed == \
-            [
-                [['location', '/foo'],
-                [['server_name', 'content_by_lua_block'],
-                 ['#', 'content_by_lua_block {'],
-                 ['#', " ngx.say('Hello World')"],
-                 ['#', ' }']
-                ]]
-            ]
-
-        # *_by_lua should parse successfully.
-        parsed = loads("""
-            location / {
-                set $a 32;
-                set $b 56;
-                set_by_lua $sum
-                    'return tonumber(ngx.arg[1]) + tonumber(ngx.arg[2])'
-                    $a $b;
-                content_by_lua '
-                    ngx.say("foo");
-                ';
-            }
-        """)
-        assert parsed == \
-            [
-                [['location', '/'],
-                 [['set', '$a', '32'],
-                  ['set', '$b', '56'],
-                  ['set_by_lua', '$sum',
-                   "'return tonumber(ngx.arg[1]) + tonumber(ngx.arg[2])'", '$a', '$b'
-                  ],
-                  ['content_by_lua', '\'\n                    ngx.say("foo");\n                \'']
-                 ]]
-            ]
 
 class TestUnspacedList(unittest.TestCase):
     """Test the UnspacedList data structure"""
