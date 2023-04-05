b28259526c03aac89c92d1222e81fd55c689deab
Brad Warren <bmw@eff.org>
2023-04-05 09:43:36 -0700

    remove unused var

--- tools/pip_install.py
+++ tools/pip_install.py
@@ -4,11 +4,15 @@
 # is used. Before installing the requested packages, core Python packaging
 # tools like pip, setuptools, and wheel are updated to pinned versions to
 # increase stability of the install.
+#
+# cryptography is currently using this script in their CI at
+# https://github.com/pyca/cryptography/blob/14d45c2259b01f1459eeab8bb7d85ce4cfb0841b/.github/downstream.d/certbot.sh#L8-L9.
+# We should try to remember to keep their repo updated if we make any changes
+# to this script which may break things for them.
 
 from __future__ import absolute_import
 from __future__ import print_function
 
-import contextlib
 import os
 import subprocess
 import sys
@@ -30,40 +34,35 @@ def pip_install_with_print(args_str, env):
     call_with_print(''.join(command), env=env)
 
 
-@contextlib.contextmanager
-def modified_environ():
+def pip_constrained_environ():
     tools_path = find_tools_path()
 
-    with tempfile.TemporaryDirectory() as working_dir:
-        repo_path = os.path.dirname(tools_path)
-        if os.environ.get('CERTBOT_OLDEST') == '1':
-            constraints_path = os.path.normpath(os.path.join(
-                repo_path, 'tools', 'oldest_constraints.txt'))
-        else:
-            constraints_path = os.path.normpath(os.path.join(
-                repo_path, 'tools', 'requirements.txt'))
+    repo_path = os.path.dirname(tools_path)
+    if os.environ.get('CERTBOT_OLDEST') == '1':
+        constraints_path = os.path.normpath(os.path.join(
+            repo_path, 'tools', 'oldest_constraints.txt'))
+    else:
+        constraints_path = os.path.normpath(os.path.join(
+            repo_path, 'tools', 'requirements.txt'))
 
-        env = os.environ.copy()
-        # We set constraints for pip using an environment variable so that they
-        # are also used when installing build dependencies. See
-        # https://github.com/certbot/certbot/pull/8443 for more info.
-        env["PIP_CONSTRAINT"] = constraints_path
-        yield env
+    env = os.environ.copy()
+    # We set constraints for pip using an environment variable so that they
+    # are also used when installing build dependencies. See
+    # https://github.com/certbot/certbot/pull/8443 for more info.
+    env["PIP_CONSTRAINT"] = constraints_path
+    return env
 
 
 def pipstrap(env=None):
     if env is None:
-        context_manager = modified_environ()
-    else:
-        context_manager = contextlib.nullcontext(env)
-    with context_manager as env:
-        pip_install_with_print('pip setuptools wheel', env=env)
+        env = pip_constrained_environ()
+    pip_install_with_print('pip setuptools wheel', env=env)
 
 
 def main(args):
-    with modified_environ() as env:
-        pipstrap(env)
-        pip_install_with_print(' '.join(args), env=env)
+    env = pip_constrained_environ()
+    pipstrap(env)
+    pip_install_with_print(' '.join(args), env=env)
 
 
 if __name__ == '__main__':
