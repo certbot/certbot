2468c2686cbfbbb34f8bb7af3c8ce1e8269c3da2
Brad Warren <bmw@eff.org>
2023-08-25 10:04:53 -0700

    simplify theme use

--- certbot-dns-google/docs/conf.py
+++ certbot-dns-google/docs/conf.py
@@ -16,7 +16,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 import sys
 
 sys.path.insert(0, os.path.abspath('_ext'))
@@ -93,14 +93,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 
-# https://docs.readthedocs.io/en/stable/faq.html#i-want-to-use-the-read-the-docs-theme-locally
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
