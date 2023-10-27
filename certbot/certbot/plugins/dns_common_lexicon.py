certbot/certbot

commit 732a3ac962ce5d1143fd0a962777301c1eef093c

Refactor Lexicon-based DNS plugins (#9746)
* Refactor Lexicon-based DNS plugins and upgrade minimal version of Lexicon

* Relax filterwarning to comply with envs where boto3 is not installed

* Update pinned dependencies

* Use our previous method to deprecate part of modules

* Safe import internally

* Add changelog

Co-authored-by: Brad Warren <bmw@users.noreply.github.com>

certbot/certbot/plugins/dns_common_lexicon.py
@@ -1,14 +1,22 @@
 """Common code for DNS Authenticator Plugins built on Lexicon."""
+import abc
 import logging
+import sys
+from types import ModuleType
 from typing import Any
+from typing import cast
 from typing import Dict
+from typing import List
 from typing import Mapping
 from typing import Optional
+from typing import Tuple
 from typing import Union
+import warnings
 
 from requests.exceptions import HTTPError
 from requests.exceptions import RequestException
 
+from certbot import configuration
 from certbot import errors
 from certbot.plugins import dns_common
 
@@ -18,18 +26,23 @@ from certbot.plugins import dns_common
 # always importable, even if it does not make sense to use it
 # if Lexicon is not available, obviously.
 try:
+    from lexicon.client import Client
     from lexicon.config import ConfigResolver
-    from lexicon.providers.base import Provider
-except ImportError:
+    from lexicon.interfaces import Provider
+except ImportError:  # pragma: no cover
+    Client = None
     ConfigResolver = None
     Provider = None
 
 logger = logging.getLogger(__name__)
 
 
-class LexiconClient:
+class LexiconClient:  # pragma: no cover
     """
     Encapsulates all communication with a DNS provider via Lexicon.
+
+    .. deprecated:: 2.7.0
+       Please use certbot.plugins.dns_common_lexicon.LexiconDNSAuthenticator instead.
     """
 
     def __init__(self) -> None:
@@ -122,14 +135,18 @@ class LexiconClient:
 
 def build_lexicon_config(lexicon_provider_name: str,
                          lexicon_options: Mapping[str, Any], provider_options: Mapping[str, Any]
-                         ) -> Union[ConfigResolver, Dict[str, Any]]:
+                         ) -> Union[ConfigResolver, Dict[str, Any]]:  # pragma: no cover
     """
     Convenient function to build a Lexicon 2.x/3.x config object.
+
     :param str lexicon_provider_name: the name of the lexicon provider to use
     :param dict lexicon_options: options specific to lexicon
     :param dict provider_options: options specific to provider
     :return: configuration to apply to the provider
     :rtype: ConfigurationResolver or dict
+
+    .. deprecated:: 2.7.0
+       Please use certbot.plugins.dns_common_lexicon.LexiconDNSAuthenticator instead.
     """
     config: Union[ConfigResolver, Dict[str, Any]] = {'provider_name': lexicon_provider_name}
     config.update(lexicon_options)
@@ -144,3 +161,143 @@ def build_lexicon_config(lexicon_provider_name: str,
         config = ConfigResolver().with_dict(config).with_env()
 
     return config
+
+
+class LexiconDNSAuthenticator(dns_common.DNSAuthenticator):
+    """
+    Base class for a DNS authenticator that uses Lexicon client
+    as backend to execute DNS record updates
+    """
+
+    def __init__(self, config: configuration.NamespaceConfig, name: str):
+        super().__init__(config, name)
+        self._provider_options: List[Tuple[str, str, str]] = []
+        self._credentials: dns_common.CredentialsConfiguration
+
+    @property
+    @abc.abstractmethod
+    def _provider_name(self) -> str:
+        """
+        The name of the Lexicon provider to use
+        """
+
+    @property
+    def _ttl(self) -> int:
+        """
+        Time to live to apply to the DNS records created by this Authenticator
+        """
+        return 60
+
+    def _add_provider_option(self, creds_var_name: str, creds_var_label: str,
+                             lexicon_provider_option_name: str) -> None:
+        self._provider_options.append(
+            (creds_var_name, creds_var_label, lexicon_provider_option_name))
+
+    def _build_lexicon_config(self, domain: str) -> ConfigResolver:
+        if not hasattr(self, '_credentials'):  # pragma: no cover
+            self._setup_credentials()
+
+        dict_config = {
+            'domain': domain,
+            'provider_name': self._provider_name,
+            'ttl': self._ttl,
+            self._provider_name: {item[2]: self._credentials.conf(item[0])
+                                  for item in self._provider_options}
+        }
+        return ConfigResolver().with_dict(dict_config).with_env()
+
+    def _setup_credentials(self) -> None:
+        self._credentials = self._configure_credentials(
+            key='credentials',
+            label=f'Credentials INI file for {self._provider_name} DNS authenticator',
+            required_variables={item[0]: item[1] for item in self._provider_options},
+        )
+
+    def _perform(self, domain: str, validation_name: str, validation: str) -> None:
+        resolved_domain = self._resolve_domain(domain)
+
+        try:
+            with Client(self._build_lexicon_config(resolved_domain)) as operations:
+                operations.create_record(rtype='TXT', name=validation_name, content=validation)
+        except RequestException as e:
+            logger.debug('Encountered error adding TXT record: %s', e, exc_info=True)
+            raise errors.PluginError('Error adding TXT record: {0}'.format(e))
+
+    def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
+        try:
+            resolved_domain = self._resolve_domain(domain)
+        except errors.PluginError as e:
+            logger.debug('Encountered error finding domain_id during deletion: %s', e,
+                         exc_info=True)
+            return
+
+        try:
+            with Client(self._build_lexicon_config(resolved_domain)) as operations:
+                operations.delete_record(rtype='TXT', name=validation_name, content=validation)
+        except RequestException as e:
+            logger.debug('Encountered error deleting TXT record: %s', e, exc_info=True)
+
+    def _resolve_domain(self, domain: str) -> str:
+        domain_name_guesses = dns_common.base_domain_name_guesses(domain)
+
+        for domain_name in domain_name_guesses:
+            try:
+                # Using client as a context manager requires `dns-lexicon>=3.14` and we may want to
+                # provide better checks and error handling around this in the future.
+                with Client(self._build_lexicon_config(domain_name)):
+                    return domain_name
+            except HTTPError as e:
+                result1 = self._handle_http_error(e, domain_name)
+
+                if result1:
+                    raise result1
+            except Exception as e:  # pylint: disable=broad-except
+                result2 = self._handle_general_error(e, domain_name)
+
+                if result2:
+                    raise result2  # pylint: disable=raising-bad-type
+
+        raise errors.PluginError('Unable to determine zone identifier for {0} using zone names: {1}'
+                                 .format(domain, domain_name_guesses))
+
+    def _handle_http_error(self, e: HTTPError, domain_name: str) -> Optional[errors.PluginError]:
+        return errors.PluginError('Error determining zone identifier for {0}: {1}.'
+                                  .format(domain_name, e))
+
+    def _handle_general_error(self, e: Exception, domain_name: str) -> Optional[errors.PluginError]:
+        if not str(e).startswith('No domain found'):
+            return errors.PluginError('Unexpected error determining zone identifier for {0}: {1}'
+                                      .format(domain_name, e))
+        return None
+
+
+# This class takes a similar approach to the cryptography project to deprecate attributes
+# in public modules. See the _ModuleWithDeprecation class here:
+# https://github.com/pyca/cryptography/blob/91105952739442a74582d3e62b3d2111365b0dc7/src/cryptography/utils.py#L129
+class _DeprecationModule:
+    """
+    Internal class delegating to a module, and displaying warnings when attributes
+    related to deprecated attributes in the current module.
+    """
+    def __init__(self, module: ModuleType):
+        self.__dict__['_module'] = module
+
+    def __getattr__(self, attr: str) -> Any:
+        if attr in ('LexiconClient', 'build_lexicon_config'):
+            warnings.warn(f'{attr} attribute in {__name__} module is deprecated '
+                          'and will be removed soon.',
+                          DeprecationWarning, stacklevel=2)
+        return getattr(self._module, attr)
+
+    def __setattr__(self, attr: str, value: Any) -> None:  # pragma: no cover
+        setattr(self._module, attr, value)
+
+    def __delattr__(self, attr: str) -> Any:  # pragma: no cover
+        delattr(self._module, attr)
+
+    def __dir__(self) -> List[str]:  # pragma: no cover
+        return ['_module'] + dir(self._module)
+
+
+# Patching ourselves to warn about deprecation and planned removal of some elements in the module.
+sys.modules[__name__] = cast(ModuleType, _DeprecationModule(sys.modules[__name__]))

certbot-dns-dnsimple/certbot_dns_dnsimple/_internal/dns_dnsimple.py
@@ -2,34 +2,30 @@
 import logging
 from typing import Any
 from typing import Callable
-from typing import cast
-from typing import Optional
 
-from lexicon.providers import dnsimple
 from requests import HTTPError
 
 from certbot import errors
-from certbot.plugins import dns_common
 from certbot.plugins import dns_common_lexicon
-from certbot.plugins.dns_common import CredentialsConfiguration
 
 logger = logging.getLogger(__name__)
 
 ACCOUNT_URL = 'https://dnsimple.com/user'
 
 
-class Authenticator(dns_common.DNSAuthenticator):
+class Authenticator(dns_common_lexicon.LexiconDNSAuthenticator):
     """DNS Authenticator for DNSimple
 
     This Authenticator uses the DNSimple v2 API to fulfill a dns-01 challenge.
     """
 
     description = 'Obtain certificates using a DNS TXT record (if you are using DNSimple for DNS).'
-    ttl = 60
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.credentials: Optional[CredentialsConfiguration] = None
+        self._add_provider_option('token',
+                                  f'User access token for DNSimple v2 API. (See {ACCOUNT_URL}.)',
+                                  'auth_token')
 
     @classmethod
     def add_parser_arguments(cls, add: Callable[..., None],
@@ -41,42 +37,9 @@ class Authenticator(dns_common.DNSAuthenticator):
         return 'This plugin configures a DNS TXT record to respond to a dns-01 challenge using ' + \
                'the DNSimple API.'
 
-    def _setup_credentials(self) -> None:
-        self.credentials = self._configure_credentials(
-            'credentials',
-            'DNSimple credentials INI file',
-            {
-                'token': 'User access token for DNSimple v2 API. (See {0}.)'.format(ACCOUNT_URL)
-            }
-        )
-
-    def _perform(self, domain: str, validation_name: str, validation: str) -> None:
-        self._get_dnsimple_client().add_txt_record(domain, validation_name, validation)
-
-    def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
-        self._get_dnsimple_client().del_txt_record(domain, validation_name, validation)
-
-    def _get_dnsimple_client(self) -> "_DNSimpleLexiconClient":
-        if not self.credentials:  # pragma: no cover
-            raise errors.Error("Plugin has not been prepared.")
-        return _DNSimpleLexiconClient(cast(str, self.credentials.conf('token')), self.ttl)
-
-
-class _DNSimpleLexiconClient(dns_common_lexicon.LexiconClient):
-    """
-    Encapsulates all communication with the DNSimple via Lexicon.
-    """
-
-    def __init__(self, token: str, ttl: int) -> None:
-        super().__init__()
-
-        config = dns_common_lexicon.build_lexicon_config('dnssimple', {
-            'ttl': ttl,
-        }, {
-            'auth_token': token,
-        })
-
-        self.provider = dnsimple.Provider(config)
+    @property
+    def _provider_name(self) -> str:
+        return 'dnssimple'
 
     def _handle_http_error(self, e: HTTPError, domain_name: str) -> errors.PluginError:
         hint = None

certbot-dns-dnsimple/certbot_dns_dnsimple/_internal/tests/dns_dnsimple_test.py
@@ -1,8 +1,6 @@
 """Tests for certbot_dns_dnsimple._internal.dns_dnsimple."""
-
-import sys
-import unittest
 from unittest import mock
+import sys
 
 import pytest
 from requests.exceptions import HTTPError
@@ -16,7 +14,9 @@ TOKEN = 'foo'
 
 
 class AuthenticatorTest(test_util.TempDirTestCase,
-                        dns_test_common_lexicon.BaseLexiconAuthenticatorTest):
+                        dns_test_common_lexicon.BaseLexiconDNSAuthenticatorTest):
+
+    LOGIN_ERROR = HTTPError('401 Client Error: Unauthorized for url: ...')
 
     def setUp(self):
         super().setUp()
@@ -30,23 +30,6 @@ class AuthenticatorTest(test_util.TempDirTestCase,
                                      dnsimple_propagation_seconds=0)  # don't wait during tests
 
         self.auth = Authenticator(self.config, "dnsimple")
-
-        self.mock_client = mock.MagicMock()
-        # _get_dnsimple_client | pylint: disable=protected-access
-        self.auth._get_dnsimple_client = mock.MagicMock(return_value=self.mock_client)
-
-
-class DNSimpleLexiconClientTest(unittest.TestCase, dns_test_common_lexicon.BaseLexiconClientTest):
-
-    LOGIN_ERROR = HTTPError('401 Client Error: Unauthorized for url: ...')
-
-    def setUp(self):
-        from certbot_dns_dnsimple._internal.dns_dnsimple import _DNSimpleLexiconClient
-
-        self.client = _DNSimpleLexiconClient(TOKEN, 0)
-
-        self.provider_mock = mock.MagicMock()
-        self.client.provider = self.provider_mock
 
 
 if __name__ == "__main__":

certbot-dns-dnsimple/setup.py
@@ -9,7 +9,7 @@ version = '2.7.0.dev0'
 install_requires = [
     # This version of lexicon is required to address the problem described in
     # https://github.com/AnalogJ/lexicon/issues/387.
-    'dns-lexicon>=3.2.1',
+    'dns-lexicon>=3.14.1',
     'setuptools>=41.6.0',
 ]
 

certbot-dns-dnsmadeeasy/certbot_dns_dnsmadeeasy/_internal/dns_dnsmadeeasy.py
@@ -2,23 +2,19 @@
 import logging
 from typing import Any
 from typing import Callable
-from typing import cast
 from typing import Optional
 
-from lexicon.providers import dnsmadeeasy
 from requests import HTTPError
 
 from certbot import errors
-from certbot.plugins import dns_common
 from certbot.plugins import dns_common_lexicon
-from certbot.plugins.dns_common import CredentialsConfiguration
 
 logger = logging.getLogger(__name__)
 
 ACCOUNT_URL = 'https://cp.dnsmadeeasy.com/account/info'
 
 
-class Authenticator(dns_common.DNSAuthenticator):
+class Authenticator(dns_common_lexicon.LexiconDNSAuthenticator):
     """DNS Authenticator for DNS Made Easy
 
     This Authenticator uses the DNS Made Easy API to fulfill a dns-01 challenge.
@@ -26,11 +22,17 @@ class Authenticator(dns_common.DNSAuthenticator):
 
     description = ('Obtain certificates using a DNS TXT record (if you are using DNS Made Easy for '
                    'DNS).')
-    ttl = 60
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.credentials: Optional[CredentialsConfiguration] = None
+        self._add_provider_option('api-key',
+                                  'API key for DNS Made Easy account, '
+                                  f'obtained from {ACCOUNT_URL}',
+                                  'auth_username')
+        self._add_provider_option('secret-key',
+                                  'Secret key for DNS Made Easy account, '
+                                  f'obtained from {ACCOUNT_URL}',
+                                  'auth_token')
 
     @classmethod
     def add_parser_arguments(cls, add: Callable[..., None],
@@ -42,48 +44,9 @@ class Authenticator(dns_common.DNSAuthenticator):
         return 'This plugin configures a DNS TXT record to respond to a dns-01 challenge using ' + \
                'the DNS Made Easy API.'
 
-    def _setup_credentials(self) -> None:
-        self.credentials = self._configure_credentials(
-            'credentials',
-            'DNS Made Easy credentials INI file',
-            {
-                'api-key': 'API key for DNS Made Easy account, obtained from {0}'
-                           .format(ACCOUNT_URL),
-                'secret-key': 'Secret key for DNS Made Easy account, obtained from {0}'
-                              .format(ACCOUNT_URL)
-            }
-        )
-
-    def _perform(self, domain: str, validation_name: str, validation: str) -> None:
-        self._get_dnsmadeeasy_client().add_txt_record(domain, validation_name, validation)
-
-    def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
-        self._get_dnsmadeeasy_client().del_txt_record(domain, validation_name, validation)
-
-    def _get_dnsmadeeasy_client(self) -> "_DNSMadeEasyLexiconClient":
-        if not self.credentials:  # pragma: no cover
-            raise errors.Error("Plugin has not been prepared.")
-        return _DNSMadeEasyLexiconClient(cast(str, self.credentials.conf('api-key')),
-                                         cast(str, self.credentials.conf('secret-key')),
-                                         self.ttl)
-
-
-class _DNSMadeEasyLexiconClient(dns_common_lexicon.LexiconClient):
-    """
-    Encapsulates all communication with the DNS Made Easy via Lexicon.
-    """
-
-    def __init__(self, api_key: str, secret_key: str, ttl: int) -> None:
-        super().__init__()
-
-        config = dns_common_lexicon.build_lexicon_config('dnsmadeeasy', {
-            'ttl': ttl,
-        }, {
-            'auth_username': api_key,
-            'auth_token': secret_key,
-        })
-
-        self.provider = dnsmadeeasy.Provider(config)
+    @property
+    def _provider_name(self) -> str:
+        return 'dnsmadeeasy'
 
     def _handle_http_error(self, e: HTTPError, domain_name: str) -> Optional[errors.PluginError]:
         if domain_name in str(e) and str(e).startswith('404 Client Error: Not Found for url:'):

certbot-dns-dnsmadeeasy/certbot_dns_dnsmadeeasy/_internal/tests/dns_dnsmadeeasy_test.py
@@ -1,7 +1,6 @@
 """Tests for certbot_dns_dnsmadeeasy._internal.dns_dnsmadeeasy."""
 
 import sys
-import unittest
 from unittest import mock
 
 import pytest
@@ -18,7 +17,10 @@ SECRET_KEY = 'bar'
 
 
 class AuthenticatorTest(test_util.TempDirTestCase,
-                        dns_test_common_lexicon.BaseLexiconAuthenticatorTest):
+                        dns_test_common_lexicon.BaseLexiconDNSAuthenticatorTest):
+
+    DOMAIN_NOT_FOUND = HTTPError(f'404 Client Error: Not Found for url: {DOMAIN}.')
+    LOGIN_ERROR = HTTPError(f'403 Client Error: Forbidden for url: {DOMAIN}.')
 
     def setUp(self):
         super().setUp()
@@ -34,24 +36,6 @@ class AuthenticatorTest(test_util.TempDirTestCase,
                                      dnsmadeeasy_propagation_seconds=0)  # don't wait during tests
 
         self.auth = Authenticator(self.config, "dnsmadeeasy")
-
-        self.mock_client = mock.MagicMock()
-        # _get_dnsmadeeasy_client | pylint: disable=protected-access
-        self.auth._get_dnsmadeeasy_client = mock.MagicMock(return_value=self.mock_client)
-
-
-class DNSMadeEasyLexiconClientTest(unittest.TestCase,
-                                   dns_test_common_lexicon.BaseLexiconClientTest):
-    DOMAIN_NOT_FOUND = HTTPError('404 Client Error: Not Found for url: {0}.'.format(DOMAIN))
-    LOGIN_ERROR = HTTPError('403 Client Error: Forbidden for url: {0}.'.format(DOMAIN))
-
-    def setUp(self):
-        from certbot_dns_dnsmadeeasy._internal.dns_dnsmadeeasy import _DNSMadeEasyLexiconClient
-
-        self.client = _DNSMadeEasyLexiconClient(API_KEY, SECRET_KEY, 0)
-
-        self.provider_mock = mock.MagicMock()
-        self.client.provider = self.provider_mock
 
 
 if __name__ == "__main__":

certbot-dns-dnsmadeeasy/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 version = '2.7.0.dev0'
 
 install_requires = [
-    'dns-lexicon>=3.2.1',
+    'dns-lexicon>=3.14.1',
     'setuptools>=41.6.0',
 ]
 

certbot-dns-gehirn/certbot_dns_gehirn/_internal/dns_gehirn.py
@@ -2,23 +2,19 @@
 import logging
 from typing import Any
 from typing import Callable
-from typing import cast
 from typing import Optional
 
-from lexicon.providers import gehirn
 from requests import HTTPError
 
 from certbot import errors
-from certbot.plugins import dns_common
 from certbot.plugins import dns_common_lexicon
-from certbot.plugins.dns_common import CredentialsConfiguration
 
 logger = logging.getLogger(__name__)
 
 DASHBOARD_URL = "https://gis.gehirn.jp/"
 
 
-class Authenticator(dns_common.DNSAuthenticator):
+class Authenticator(dns_common_lexicon.LexiconDNSAuthenticator):
     """DNS Authenticator for Gehirn Infrastructure Service DNS
 
     This Authenticator uses the Gehirn Infrastructure Service API to fulfill
@@ -27,11 +23,17 @@ class Authenticator(dns_common.DNSAuthenticator):
 
     description = 'Obtain certificates using a DNS TXT record ' + \
                   '(if you are using Gehirn Infrastructure Service for DNS).'
-    ttl = 60
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.credentials: Optional[CredentialsConfiguration] = None
+        self._add_provider_option('api-token',
+                                  'API token for Gehirn Infrastructure Service '
+                                  f'API obtained from {DASHBOARD_URL}',
+                                  'auth_token')
+        self._add_provider_option('api-secret',
+                                  'API secret for Gehirn Infrastructure Service '
+                                  f'API obtained from {DASHBOARD_URL}',
+                                  'auth_secret')
 
     @classmethod
     def add_parser_arguments(cls, add: Callable[..., None],
@@ -43,50 +45,9 @@ class Authenticator(dns_common.DNSAuthenticator):
         return 'This plugin configures a DNS TXT record to respond to a dns-01 challenge using ' + \
                'the Gehirn Infrastructure Service API.'
 
-    def _setup_credentials(self) -> None:
-        self.credentials = self._configure_credentials(
-            'credentials',
-            'Gehirn Infrastructure Service credentials file',
-            {
-                'api-token': 'API token for Gehirn Infrastructure Service ' + \
-                             'API obtained from {0}'.format(DASHBOARD_URL),
-                'api-secret': 'API secret for Gehirn Infrastructure Service ' + \
-                              'API obtained from {0}'.format(DASHBOARD_URL),
-            }
-        )
-
-    def _perform(self, domain: str, validation_name: str, validation: str) -> None:
-        self._get_gehirn_client().add_txt_record(domain, validation_name, validation)
-
-    def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
-        self._get_gehirn_client().del_txt_record(domain, validation_name, validation)
-
-    def _get_gehirn_client(self) -> "_GehirnLexiconClient":
-        if not self.credentials:  # pragma: no cover
-            raise errors.Error("Plugin has not been prepared.")
-        return _GehirnLexiconClient(
-            cast(str, self.credentials.conf('api-token')),
-            cast(str, self.credentials.conf('api-secret')),
-            self.ttl
-        )
-
-
-class _GehirnLexiconClient(dns_common_lexicon.LexiconClient):
-    """
-    Encapsulates all communication with the Gehirn Infrastructure Service via Lexicon.
-    """
-
-    def __init__(self, api_token: str, api_secret: str, ttl: int) -> None:
-        super().__init__()
-
-        config = dns_common_lexicon.build_lexicon_config('gehirn', {
-            'ttl': ttl,
-        }, {
-            'auth_token': api_token,
-            'auth_secret': api_secret,
-        })
-
-        self.provider = gehirn.Provider(config)
+    @property
+    def _provider_name(self) -> str:
+        return 'gehirn'
 
     def _handle_http_error(self, e: HTTPError, domain_name: str) -> Optional[errors.PluginError]:
         if domain_name in str(e) and (str(e).startswith('404 Client Error: Not Found for url:')):

commit cf4f07d17e22924f7d7d3f41a09136df22981765

Set the delegated field in Lexicon config to bypass subdomain resolution (#9821)
The Lexicon-based DNS plugins use a mechanism to determine which actual segment of the input domain is actually the DNS zone in which the DNS-01 challenge has to be initiated (eg. `subdomain.domain.com` or `domain.com` for input `subdomain.domain.com`): they tries recursively to configure Lexicon and initiate authentication from the most specific to most generic domain segment, and select the first segment where Lexicon stop erroring out.

This mechanism broke with #9746 because now the plugins call Lexicon client instead of the underlying providers, and the client makes guess on the actual domain requested. Typically for `subdomain.domain.com` it will actually try to authenticate against `domain.com`, and so the mechanism above does not work anymore.

This PR fixes the issue by using the `delegated` field in Lexicon config each time the plugin needs it. This field is designed for this kind of purpose: it will instruct Lexicon what is the actual DNS zone domain instead of guessing it.

I tested the change with one of my OVH account. The expected behavior is re-established and the plugin is able to test `subdomain.domain.com` then `domain.com` as before.

Fixes #9791
Fixes #9818

certbot/certbot/plugins/dns_common_lexicon.py
@@ -198,6 +198,10 @@ class LexiconDNSAuthenticator(dns_common.DNSAuthenticator):
 
         dict_config = {
             'domain': domain,
+            # We bypass Lexicon subdomain resolution by setting the 'delegated' field in the config
+            # to the value of the 'domain' field itself. Here we consider that the domain passed to
+            # _build_lexicon_config() is already the exact subdomain of the actual DNS zone to use.
+            'delegated': domain,
             'provider_name': self._provider_name,
             'ttl': self._ttl,
             self._provider_name: {item[2]: self._credentials.conf(item[0])