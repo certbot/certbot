4d7d0d6d048ed67d78fa56f440bada443e857f11
Will Greenberg <willg@eff.org>
2024-04-02 14:20:24 -0700

    Release 2.10.0

--- certbot-apache/setup.py
+++ certbot-apache/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.10.0.dev0'
+version = '2.10.0'
 
 install_requires = [
     # We specify the minimum acme and certbot version as the current plugin
