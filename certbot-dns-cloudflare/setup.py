9213154e4428bee58ddcb9790b76c3b4f8f7740b
Will Greenberg <willg@eff.org>
2024-06-05 14:34:41 -0700

    Bump version to 2.12.0

--- certbot-dns-cloudflare/setup.py
+++ certbot-dns-cloudflare/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.11.0'
+version = '2.12.0.dev0'
 
 install_requires = [
     # for now, do not upgrade to cloudflare>=2.20 to avoid deprecation warnings and the breaking
--- certbot-dns-ovh/setup.py
+++ certbot-dns-ovh/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.11.0'
+version = '2.12.0.dev0'
 
 install_requires = [
     'dns-lexicon>=3.15.1',
