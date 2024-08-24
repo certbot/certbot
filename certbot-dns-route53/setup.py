7174394d7d1134c4b0105a3f21eac9ba9f94fe0c
cx <1249843194@qq.com>
2024-08-24 22:46:34 +0800

    Remove parameters '--manual-public-ip-logging-ok' and remove 'certbot-route53:auth'

--- certbot-dns-route53/setup.py
+++ certbot-dns-route53/setup.py
@@ -70,8 +70,7 @@ setup(
     },
     entry_points={
         'certbot.plugins': [
-            'dns-route53 = certbot_dns_route53._internal.dns_route53:Authenticator',
-            'certbot-route53:auth = certbot_dns_route53.authenticator:Authenticator'
+            'dns-route53 = certbot_dns_route53._internal.dns_route53:Authenticator'
         ],
     },
 )
