e2b2d6c645fd4877f5edbbe3c0734d841d378e40
Adrien Ferrand <adferrand@users.noreply.github.com>
2024-08-28 09:42:01 +0200

    Update certbot/setup.py
    Co-authored-by: Brad Warren <bmw@users.noreply.github.com>

--- certbot/setup.py
+++ certbot/setup.py
@@ -34,7 +34,7 @@ install_requires = [
     'distro>=1.0.1',
     'importlib_resources>=1.3.1; python_version < "3.9"',
     'importlib_metadata>=4.6; python_version < "3.10"',
-    # Josepy 2+ introduce backward incompatible changes by droping usage of
+    # Josepy 2+ may introduce backward incompatible changes by droping usage of
     # deprecated PyOpenSSL APIs.
     'josepy>=1.13.0, <2',
     'parsedatetime>=2.4',
