5fc168e1f750bf0e3606e8c68dc7a767e7fa9aef
Brad Warren <bmw@eff.org>
2024-11-25 11:11:31 -0800

    update setup.py files

--- certbot-apache/setup.py
+++ certbot-apache/setup.py
@@ -29,7 +29,7 @@ setup(
     author="Certbot Project",
     author_email='certbot-dev@eff.org',
     license='Apache License 2.0',
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -38,7 +38,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
