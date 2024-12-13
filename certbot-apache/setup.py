325919f37e2e32e980ba2391ae1ed6ecfc17d90a
Brad Warren <bmw@eff.org>
2024-12-13 08:54:47 -0800

    sed -i "s/\( *'Pro.*3\.1\)2\(',\)/\12\2\n\13\2/" */setup.py

--- certbot-apache/setup.py
+++ certbot-apache/setup.py
@@ -41,6 +41,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Security',
         'Topic :: System :: Installation/Setup',
