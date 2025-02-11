ad29d5fbcf419300d3662ce62bec465f827b491a
Trinopoty Biswas <trinopoty@users.noreply.github.com>
2025-02-11 19:25:28 +0100

    Remove support for Linode API v3 (#10062)
    The v3 api was sunset at the end of July 2023. This PR cleans up code
    related to api v3.
    
    ## Pull Request Checklist
    
    - [ ] The Certbot team has recently expressed interest in reviewing a PR
    for this. If not, this PR may be closed due our limited resources and
    need to prioritize how we spend them.
    - [ ] If the change being made is to a [distributed
    component](https://certbot.eff.org/docs/contributing.html#code-components-and-layout),
    edit the `main` section of `certbot/CHANGELOG.md` to include a
    description of the change being made.
    - [ ] Add or update any documentation as needed to support the changes
    in this PR.
    - [ ] Include your name in `AUTHORS.md` if you like.

--- certbot-dns-linode/certbot_dns_linode/_internal/tests/dns_linode_test.py
+++ certbot-dns-linode/certbot_dns_linode/_internal/tests/dns_linode_test.py
@@ -14,7 +14,6 @@ from certbot.tests import util as test_util
 from certbot_dns_linode._internal.dns_linode import Authenticator
 
 TOKEN = 'a-token'
-TOKEN_V3 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ64'
 TOKEN_V4 = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
 
 
@@ -35,17 +34,6 @@ class AuthenticatorTest(test_util.TempDirTestCase,
         self.auth = Authenticator(self.config, "linode")
 
     # pylint: disable=protected-access
-    def test_api_version_3_detection(self):
-        path = os.path.join(self.tempdir, 'file_3_auto.ini')
-        dns_test_common.write({"linode_key": TOKEN_V3}, path)
-
-        config = mock.MagicMock(linode_credentials=path,
-                                linode_propagation_seconds=0)
-        auth = Authenticator(config, "linode")
-
-        assert auth._provider_name == "linode"
-
-    # pylint: disable=protected-access
     def test_api_version_4_detection(self):
         path = os.path.join(self.tempdir, 'file_4_auto.ini')
         dns_test_common.write({"linode_key": TOKEN_V4}, path)
@@ -55,62 +43,6 @@ class AuthenticatorTest(test_util.TempDirTestCase,
         auth = Authenticator(config, "linode")
 
         assert auth._provider_name == "linode4"
-
-    # pylint: disable=protected-access
-    def test_api_version_3_detection_empty_version(self):
-        path = os.path.join(self.tempdir, 'file_3_auto_empty.ini')
-        dns_test_common.write({"linode_key": TOKEN_V3, "linode_version": ""}, path)
-
-        config = mock.MagicMock(linode_credentials=path,
-                                linode_propagation_seconds=0)
-        auth = Authenticator(config, "linode")
-
-        assert auth._provider_name == "linode"
-
-    # pylint: disable=protected-access
-    def test_api_version_4_detection_empty_version(self):
-        path = os.path.join(self.tempdir, 'file_4_auto_empty.ini')
-        dns_test_common.write({"linode_key": TOKEN_V4, "linode_version": ""}, path)
-
-        config = mock.MagicMock(linode_credentials=path,
-                                linode_propagation_seconds=0)
-        auth = Authenticator(config, "linode")
-
-        assert auth._provider_name == "linode4"
-
-    # pylint: disable=protected-access
-    def test_api_version_3_manual(self):
-        path = os.path.join(self.tempdir, 'file_3_manual.ini')
-        dns_test_common.write({"linode_key": TOKEN_V4, "linode_version": 3}, path)
-
-        config = mock.MagicMock(linode_credentials=path,
-                                linode_propagation_seconds=0)
-        auth = Authenticator(config, "linode")
-
-        assert auth._provider_name == "linode"
-
-    # pylint: disable=protected-access
-    def test_api_version_4_manual(self):
-        path = os.path.join(self.tempdir, 'file_4_manual.ini')
-        dns_test_common.write({"linode_key": TOKEN_V3, "linode_version": 4}, path)
-
-        config = mock.MagicMock(linode_credentials=path,
-                                linode_propagation_seconds=0)
-        auth = Authenticator(config, "linode")
-
-        assert auth._provider_name == "linode4"
-
-    # pylint: disable=protected-access
-    def test_api_version_error(self):
-        path = os.path.join(self.tempdir, 'file_version_error.ini')
-        dns_test_common.write({"linode_key": TOKEN_V3, "linode_version": 5}, path)
-
-        config = mock.MagicMock(linode_credentials=path,
-                                linode_propagation_seconds=0)
-        auth = Authenticator(config, "linode")
-
-        with pytest.raises(errors.PluginError):
-            assert auth._provider_name == "linode4"
 
 
 if __name__ == "__main__":
