dcdfdacf75d8904504dded2ea800813ff623fe1a
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2025-05-07 16:32:48 -0700

    store preferred/required_profile in renewal config (#10280)
    This ensures that renewals of certificates will use the same profile
    settings.
    
    Fixes #10271

--- certbot/src/certbot/_internal/renewal.py
+++ certbot/src/certbot/_internal/renewal.py
@@ -45,7 +45,8 @@ logger = logging.getLogger(__name__)
 STR_CONFIG_ITEMS = ["config_dir", "logs_dir", "work_dir", "user_agent",
                     "server", "account", "authenticator", "installer",
                     "renew_hook", "pre_hook", "post_hook", "http01_address",
-                    "preferred_chain", "key_type", "elliptic_curve"]
+                    "preferred_chain", "key_type", "elliptic_curve",
+                    "preferred_profile", "required_profile"]
 INT_CONFIG_ITEMS = ["rsa_key_size", "http01_port"]
 BOOL_CONFIG_ITEMS = ["must_staple", "allow_subset_of_names", "reuse_key",
                      "autorenew"]
