ffb2196e2121162f2964a42aabf30a10edaffc8b
Erica Portnoy <erica@eff.org>
2025-08-05 08:58:17 -0700

    Bump version to 5.0.0

--- acme/setup.py
+++ acme/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '4.2.0'
+version = '5.0.0.dev0'
 
 install_requires = [
     'cryptography>=43.0.0',
--- certbot-dns-ovh/setup.py
+++ certbot-dns-ovh/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '4.2.0'
+version = '5.0.0.dev0'
 
 install_requires = [
     'dns-lexicon>=3.15.1',
