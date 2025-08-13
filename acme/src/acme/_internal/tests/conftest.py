1dddad8a343b339457160f73a01d564d27ca48ee
Erica Portnoy <erica@eff.org>
2025-08-13 14:33:38 -0700

    add comments

--- acme/src/acme/_internal/tests/conftest.py
+++ acme/src/acme/_internal/tests/conftest.py
@@ -2,6 +2,9 @@ from unittest import mock
 
 import pytest
 
+# This avoids a bug on mac where getfqdn errors after a long timeout.
+# See https://bugs.python.org/issue35164
+# and discussion at https://github.com/certbot/certbot/pull/10408
 @pytest.fixture(autouse=True)
 def mock_getfqdn():
     with mock.patch("socket.getfqdn", return_value="server_name") as mocked:
