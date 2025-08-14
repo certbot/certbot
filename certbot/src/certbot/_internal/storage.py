d794d77a1a773734b3bf40e3b5dbabffa888e3af
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2025-08-14 15:56:29 -0700

    Clean up delete loop

--- certbot/src/certbot/_internal/storage.py
+++ certbot/src/certbot/_internal/storage.py
@@ -151,10 +151,9 @@ def atomic_rewrite(config_filename: str, new_config: configobj.ConfigObj) -> Non
     #
     # This only applies when "renewalparams" is actually being updated, because sometimes we
     # update other sections independently (like "acme_renewal_info").
-    if "renewalparams" in new_config:
-        for k in merged_config["renewalparams"]:
-            if k not in new_config["renewalparams"]:
-                del merged_config["renewalparams"][k]
+    for k in merged_config.get("renewalparams", {}):
+        if k not in new_config.get("renewalparams", {}):
+            del merged_config["renewalparams"][k]
 
     current_permissions = stat.S_IMODE(os.lstat(config_filename).st_mode)
 
