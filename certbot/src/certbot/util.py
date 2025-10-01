b7f1f1f8c8b0e5ed23fad716c5f9c0e3f25fdb09
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2025-10-01 16:48:32 -0700

    Fix error message.

--- certbot/src/certbot/util.py
+++ certbot/src/certbot/util.py
@@ -637,9 +637,8 @@ def enforce_domain_sanity(domain: Union[str, bytes]) -> str:
 
     if is_ipaddress(domain):
         raise errors.ConfigurationError(
-            "Requested name {0} is an IP address. The Let's Encrypt "
-            "certificate authority will not issue certificates for a "
-            "bare IP address.".format(domain))
+            f"Requested domain name {domain} is an IP address. Use the --ip-address flag "
+            "instead of -d or --domain")
 
     # FQDN checks according to RFC 2181: domain name should be less than 255
     # octets (inclusive). And each label is 1 - 63 octets (inclusive).
