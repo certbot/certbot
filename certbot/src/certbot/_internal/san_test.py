552005d8c63752c610c944187d6371b03ddfbba2
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2025-10-16 15:35:07 -0700

    Incorporate validation in DNSName.__init__()
    This is the same validation as enforce_domain_sanity(), and the tests are copied
    as well.

--- certbot/src/certbot/_internal/san_test.py
+++ certbot/src/certbot/_internal/san_test.py
@@ -2,6 +2,7 @@
 import pytest
 import unittest
 
+from certbot import errors
 from certbot._internal import san
 
 class SanTest(unittest.TestCase):
@@ -11,6 +12,7 @@ class SanTest(unittest.TestCase):
 
     def test_hash(self) -> None:
         assert hash(san.DNSName("example.com")) == hash(san.DNSName("example.com"))
+        assert hash(san.DNSName("EXAMPLE.COM.")) == hash(san.DNSName("example.com"))
         assert hash(san.IPAddress("192.168.1.1")) == hash(san.IPAddress("192.168.1.1"))
 
     def test_repr(self) -> None:
@@ -26,6 +28,11 @@ class SanTest(unittest.TestCase):
             san.IPAddress("192.168.1.1") == "192.168.1.1" # pylint: disable=expression-not-assigned
         with pytest.raises(TypeError):
             "192.168.1.1" == san.IPAddress("192.168.1.1") # pylint: disable=expression-not-assigned
+        assert san.DNSName("example.com") == san.DNSName("example.com")
+        assert san.DNSName("Example.com") == san.DNSName("example.com")
+        assert san.DNSName("example.com") == san.DNSName("Example.com")
+        assert san.DNSName("EXAMPLE.COM") == san.DNSName("Example.com")
+        assert san.DNSName("EXAMPLE.COM.") == san.DNSName("example.com")
 
     def test_is_wildcard(self) -> None:
         assert not san.DNSName("example.com").is_wildcard()
@@ -50,3 +57,55 @@ class SanTest(unittest.TestCase):
     def test_ip_address(self) -> None:
         with pytest.raises(ValueError):
             san.IPAddress("example.com")
+
+class EnforceDomainSyntaxTest(unittest.TestCase):
+    """Test validation of domain names."""
+    def _call(self, dns_name: str) -> None:
+        san.DNSName(dns_name)
+
+    def test_nonascii_str(self) -> None:
+        with pytest.raises(errors.ConfigurationError):
+            self._call("eichh\u00f6rnchen.example.com")
+
+    def test_too_long(self) -> None:
+        long_domain = "a"*256
+        with pytest.raises(errors.ConfigurationError):
+            self._call(long_domain)
+
+    def test_not_too_long(self) -> None:
+        not_too_long_domain = "{0}.{1}.{2}.{3}".format("a"*63, "b"*63, "c"*63, "d"*63)
+        self._call(not_too_long_domain)
+
+    def test_empty_label(self) -> None:
+        empty_label_domain = "fizz..example.com"
+        with pytest.raises(errors.ConfigurationError):
+            self._call(empty_label_domain)
+
+    def test_empty_trailing_label(self) -> None:
+        empty_trailing_label_domain = "example.com.."
+        with pytest.raises(errors.ConfigurationError):
+            self._call(empty_trailing_label_domain)
+
+    def test_long_label_1(self) -> None:
+        long_label_domain = "a"*64
+        with pytest.raises(errors.ConfigurationError):
+            self._call(long_label_domain)
+
+    def test_long_label_2(self) -> None:
+        long_label_domain = "{0}.{1}.com".format("a"*64, "b"*63)
+        with pytest.raises(errors.ConfigurationError):
+            self._call(long_label_domain)
+
+    def test_not_long_label(self) -> None:
+        not_too_long_label_domain = "{0}.{1}.com".format("a"*63, "b"*63)
+        self._call(not_too_long_label_domain)
+
+    def test_empty_domain(self) -> None:
+        empty_domain = ""
+        with pytest.raises(errors.ConfigurationError):
+            self._call(empty_domain)
+
+    def test_punycode_ok(self) -> None:
+        # Punycode is now legal, so no longer an error; instead check
+        # that it's _not_ an error (at the initial sanity check stage)
+        self._call('this.is.xn--ls8h.tld')
