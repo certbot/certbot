1e10b1484372570cfd2d3cb0f3db8402f6151855
Will Greenberg <willg@eff.org>
2025-12-11 12:24:23 -0800

    Use certbot extra deps for apache/nginx shims

--- certbot-nginx/setup.py
+++ certbot-nginx/setup.py
@@ -3,15 +3,7 @@ from setuptools import setup
 version = '5.4.0.dev0'
 
 install_requires = [
-    # We specify the minimum acme and certbot version as the current plugin
-    # version for simplicity. See
-    # https://github.com/certbot/certbot/issues/8761 for more info.
-    f'acme>={version}',
-    f'certbot>={version}',
-    # PyOpenSSL>=25.0.0 is just needed to satisfy mypy right now so this dependency can probably be
-    # relaxed to >=24.0.0 if needed.
-    'PyOpenSSL>=25.0.0',
-    'pyparsing>=3.0.0',
+    f'certbot[nginx]>={version}',
 ]
 
 setup(
