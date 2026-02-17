a17385047ad4dfc5c79dc4b706442f88d6b4011b
Will Greenberg <willg@eff.org>
2026-02-17 15:07:15 -0800

    Update certbot-apache/setup.py
    Co-authored-by: ohemorange <erica@eff.org>

--- certbot-apache/setup.py
+++ certbot-apache/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 version = '5.4.0.dev0'
 
 install_requires = [
-    # We specify the minimum acme and certbot version as the current plugin
+    # We specify the minimum certbot version as the current plugin
     # version for simplicity. See
     # https://github.com/certbot/certbot/issues/8761 for more info.
     f'certbot[apache]>={version}',
