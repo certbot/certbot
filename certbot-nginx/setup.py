e4b68aae5789cc01c4d60a2c6e724792b9e9470c
Will Greenberg <willg@eff.org>
2026-02-17 15:11:26 -0800

    re-add comment about minimum certbot version

--- certbot-nginx/setup.py
+++ certbot-nginx/setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 version = '5.4.0.dev0'
 
 install_requires = [
+    # We specify the minimum certbot version as the current plugin
+    # version for simplicity. See
+    # https://github.com/certbot/certbot/issues/8761 for more info.
     f'certbot[nginx]>={version}',
 ]
 
