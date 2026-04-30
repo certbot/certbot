f9a75f854ba347146893bd4b36d74a0e44a97397
Will Greenberg <willg@eff.org>
2026-04-30 11:41:56 -0700

    acme: copy over is_wildcard_domain verbatim
    This is just wholesale copied from certbot's utils. Maybe we could
    DRY it up by having certbot rely on acme's, but IMO it's not a complex
    enough function to justify that.

--- acme/src/acme/_internal/tests/util_test.py
+++ acme/src/acme/_internal/tests/util_test.py
@@ -1,5 +1,6 @@
 """Tests for acme.util."""
 import sys
+import unittest
 
 import pytest
 
@@ -9,6 +10,26 @@ def test_it():
     assert {'a': 'b', 'c': 'd'} == \
                      map_keys({'a': 'b', 'c': 'd'}, lambda key: key)
     assert {2: 2, 4: 4} == map_keys({1: 2, 3: 4}, lambda x: x + 1)
+
+
+class IsWildcardDomainTest(unittest.TestCase):
+    """Tests for is_wildcard_domain."""
+
+    def setUp(self):
+        self.wildcard = u"*.example.org"
+        self.no_wildcard = u"example.org"
+
+    def _call(self, domain):
+        from acme.util import is_wildcard_domain
+        return is_wildcard_domain(domain)
+
+    def test_no_wildcard(self):
+        assert not self._call(self.no_wildcard)
+        assert not self._call(self.no_wildcard.encode())
+
+    def test_wildcard(self):
+        assert self._call(self.wildcard)
+        assert self._call(self.wildcard.encode())
 
 
 if __name__ == '__main__':
--- acme/src/acme/util.py
+++ acme/src/acme/util.py
@@ -7,3 +7,18 @@ from typing import Mapping
 def map_keys(dikt: Mapping[Any, Any], func: Callable[[Any], Any]) -> dict[Any, Any]:
     """Map dictionary keys."""
     return {func(key): value for key, value in dikt.items()}
+
+
+def is_wildcard_domain(domain: str) -> bool:
+    """"Is domain a wildcard domain?
+
+    :param domain: domain to check
+    :type domain: `bytes` or `str`
+
+    :returns: True if domain is a wildcard, otherwise, False
+    :rtype: bool
+
+    """
+    if isinstance(domain, str):
+        return domain.startswith("*.")
+    return domain.startswith(b"*.")
