cbf7dce0acfe9094efd949e3466a5072529bce30
Erica Portnoy <erica@eff.org>
2026-06-16 15:35:28 -0700

    Release 4.26.1

--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup
 
-version = '4.26.0'
+version = '4.26.1'
 
 install_requires = [
     # This version of lexicon is required to address the problem described in
--- certbot-dns-route53/setup.py
+++ certbot-dns-route53/setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup
 
-version = '4.26.0'
+version = '4.26.1'
 
 install_requires = [
     'boto3>=1.20.34',
