bb31517ac1ad81841344ceba6edf9ad471315f23
Erica Portnoy <erica@eff.org>
2026-06-23 10:31:17 -0700

    the new variable is called pr_opts, not create_pr_opts

--- tools/finish_release.py
+++ tools/finish_release.py
@@ -264,7 +264,7 @@ def _create_release_pr_to_minor_branch(
     body = 'this PR only needs 1 review and should be merged, not squashed'
     pr_opts = [ '--head', branch_name,
                 '--base', point_x_branch_name]
-    result = _create_pr(title, body, 'PR to merge release changes into .x branch', create_pr_opts)
+    result = _create_pr(title, body, 'PR to merge release changes into .x branch', pr_opts)
     print(f'PR location: {result}')
 
 
