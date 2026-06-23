fdc13c657eb0f0368bd68e0a3fd8ec365964d682
Erica Portnoy <erica@eff.org>
2026-06-23 10:34:58 -0700

    Bump version to 4.35.0

--- certbot-dns-dnsimple/setup.py
+++ certbot-dns-dnsimple/setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup
 
-version = '4.34.1'
+version = '4.35.0.dev0'
 
 install_requires = [
     # This version of lexicon is required to address the problem described in
